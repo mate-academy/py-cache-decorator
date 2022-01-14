5719562a7f1c5aade4b98ad89eae0cf98349a58d
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-14 23:35:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    result_arr = []
+
+    def inner(*args):
+        tmp_args = []
+        arr = [i for i in args]
+        if arr in result_arr:
+            print('Getting from cache')
+            return func(*tuple(arr))
+        else:
+            for i in args:
+                tmp_args.append(i)
+            result_arr.append(tmp_args)
+            print('Calculating new result')
+            return func(*tuple(tmp_args))
+
+    return inner
