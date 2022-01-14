b59982fb165fc750aade02be67d3b461523b61df
Artur Shavula <arthuromgnh@gmail.com>
2022-01-14 17:36:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 def cache(func):
-    # Write your code here
-    pass
+    stored_results = {}
+
+    def inner(*args):
+        if args in stored_results:
+            print("Getting from cache")
+            return stored_results[args]
+        else:
+            print('Calculating new result')
+            stored_results[args] = (result := func(*args))
+            return result
+
+    return inner
+
+
+
