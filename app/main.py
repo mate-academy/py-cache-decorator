335c2b2f67a787cb21db724fce6d2e2f3158817a
nikon4uk <nikonchukmax4@gmail.com>
2022-01-16 17:54:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    history = {}
+
+    def wrapper(*args, **kwargs):
+        nonlocal history
+        if history.get(args):
+            print("Getting from cache")
+            return history[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            history[args] = result
+            return result
+    return wrapper
