660f687e4e334775d4529cd811db49335b69e991
Alex Gergel <alexgergel@gmail.com>
2022-01-17 00:19:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def cache(func):
-    # Write your code here
-    pass
+    ls = []
+
+    def wrapper(*args, **kwargs):
+        if args in ls:
+            print("Getting from cache")
+        else:
+            ls.append(args)
+            print("Calculating new result")
+        return func(*args, **kwargs)
+    return wrapper
