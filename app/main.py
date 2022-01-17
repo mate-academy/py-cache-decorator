f57c8b0438534cbdb941993de2e063522ef93d23
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-01-17 12:40:33 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    unique = set()
+
+    def inner(*args):
+        if args not in unique:
+            unique.add(args)
+            print("Calculating new result")
+            return func(*args)
+        else:
+            print("Getting from cache")
+            return unique
+
+    return inner
