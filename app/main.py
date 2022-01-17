23551826a7d84e14fefa9e84bf948bb20210560f
NikitaZH1812 <zhukovna1812@gmail.com>
2022-01-17 14:26:29 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ def cache(func):
     cache_dict = {}
 
     def inner(*args):
-        if args in cache_dict:
+        if args in cache_dict.keys():
             print("Getting from cache")
         else:
             cache_dict[args] = func(*args)
             print("Calculating new result")
-            return cache_dict[args]
+        return cache_dict[args]
     return inner
