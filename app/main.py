4cd8bfdac9f0448c5b5dc6b4fa398314c8dcfe8c
Alexandr-Semenishin <97320430+Alexandr-Semenishin@users.noreply.github.com>
2022-01-18 01:11:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args):
+        if hash(tuple(args)) in cache_dict:
+            print("Getting from cache")
+            return cache_dict[hash(tuple(args))]
+        else:
+            print("Calculating new result")
+            some_data = func(*args)
+            cache_dict[hash(tuple(args))] = some_data
+            return some_data
+    return inner
