76144a3530957e355511fe0a491d8d04df61c200
alix makarova <alix.makarova@gmail.com>
2022-01-17 12:02:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
+
+    memory = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        nonlocal memory
+        if args in memory and memory[args][0] == func.__name__:
+            print("Getting from cache")
+            return memory[args][1]
+
+        else:
+            memory[args] = func.__name__, func(*args)
+            print("Calculating new result")
+            return func(*args)
+
+    return wrapper
     pass
