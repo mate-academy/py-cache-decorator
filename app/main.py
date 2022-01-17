23d42024a627dd447eb3ead98438695a6c802e24
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-01-17 15:16:44 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    caches = {}
+
+    def inner(*args):
+        if args in caches.keys():
+            print("Getting from cache")
+            return caches[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            caches[tuple(args)] = result
+            return result
+    return inner
