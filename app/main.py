67e294ceb99ec6c7f88b0174496a68163d9d62e6
Zikkolion <nmadaraosnova835@gmail.com>
2022-01-18 08:57:15 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    c_dict = {}
+
+    def inner(*args):
+        if args in c_dict:
+            print("Getting from cache")
+            return c_dict[args]
+        else:
+            print("Calculating new result")
+            s = func(*args)
+            c_dict[args] = s
+            return s
+
+    return inner
