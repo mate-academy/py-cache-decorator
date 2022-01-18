3f32815a5097311c3ad93ca4f59d4240b8ebf688
Anita Posledova <anitaposliedova@gmail.com>
2022-01-18 15:01:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dictionary = {}
+
+    def inner(*args):
+        if args in cache_dictionary:
+            print("Getting from cache")
+            return cache_dictionary[args]
+
+        else:
+            print('Calculating new result')
+            cache_dictionary[args] = (res := func(*args))
+            return res
+    return inner
