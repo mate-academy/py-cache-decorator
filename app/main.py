a0e6c4da2eea6560c79576227cf177597aa25857
Nazar-kob <kobrinnazar05@mail.com>
2022-01-19 13:41:59 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
-    # Write your code here
-    pass
+    result_old_task = {}
+
+    def inner(*args):
+        if args in result_old_task:
+            print("Getting from cache")
+            return result_old_task[args]
+
+        else:
+            result = func(*args)
+            result_old_task.update({args: result})
+            print("Calculating new result")
+            return result
+
+    return inner
