bd2690b50c937ee9df9425046cf8db219ddc4203
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-19 16:20:58 +0200

    Solution task about cache decorator. Katiukha

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args, **kwargs):
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[args] = result
+        return result
+
+    return inner
+
+
+@cache
+def sum(a, b):
+    return a + b
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
+
+sum(1, 2)
+sum(1, 2)
