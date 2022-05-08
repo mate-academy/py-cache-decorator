0d74679e8f383bf2ce7592aae21253bbea97fe25
Marko Khodan <khodanmarko@gmail.com>
2022-05-08 11:26:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = f"{func.__name__}{args}"
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            cache_dict.update({key: func(*args)})
+            print("Calculating new result")
+            return func(*args)
+    return wrapper
