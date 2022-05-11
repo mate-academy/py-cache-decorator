17fdc3803b3fd7eef2e3a050b5013be14ba10a1e
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-11 19:06:34 +0300

    Remove testing information

--- app/main.py
+++ app/main.py
@@ -27,14 +27,3 @@ def long_time_func(a, b, c):
 @cache
 def long_time_func_2(n_tuple, power):
     return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
-
-sum(1, 2)
-sum(1, 2)
