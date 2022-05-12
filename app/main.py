9f859cbc353a7386f29dd0a219f0b554e620ca8b
MykolaIF <nick.pasko.82@gmail.com>
2022-05-12 19:33:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    dict_with_args = {}
+
+    def wrapper(*args):
+        nonlocal dict_with_args
+        if args in dict_with_args:
+            print("Getting from cache")
+            return dict_with_args[args]
+
+        dict_with_args.update({args: func(*args)})
+        print("Calculating new result")
+        return func
+
+    return wrapper
