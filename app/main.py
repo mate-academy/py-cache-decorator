af4f5fb02db3e348e252d7a978c1fffc38d90fd4
Flashmobber <ufinaid@gmail.com>
2022-05-12 23:00:31 +0300

    Decorator done

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+results = {}
+
+
 def cache(func):
-    # Write your code here
-    pass
+    global results
+
+    def inner(*args):
+        if func in results:
+            if args in results[func] is not None:
+                print("Getting from cache")
+                return results[func][args]
+        else:
+            results[func] = {}
+
+        print("Calculating new result")
+        results[func][args] = func(*args)
+        return results[func][args]
+
+    return inner
