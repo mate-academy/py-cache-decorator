5cc90b7ef8e16f9949c1bd894aab18106d27d97d
MykolaIF <nick.pasko.82@gmail.com>
2022-05-13 11:21:29 +0300

    Return a calculated result. Nonlocal is removed.

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ def cache(func):
     dict_with_args = {}
 
     def wrapper(*args):
-        nonlocal dict_with_args
         if args in dict_with_args:
             print("Getting from cache")
             return dict_with_args[args]
 
         dict_with_args.update({args: func(*args)})
         print("Calculating new result")
-        return func
+        return func(*args)
 
     return wrapper
