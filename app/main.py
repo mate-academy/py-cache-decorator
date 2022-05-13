81da13b3b3e67f72e8e5d25e7dddf3f1303da642
bohdan <bodyan1108@gmail.com>
2022-05-13 18:20:28 +0300

    make solution to hw3 decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+
+    cache_list_ = {}
+
+    def wrapper(*args):
+        if args in cache_list_.keys():
+            print("Getting from cache")
+            return cache_list_[args]
+        else:
+            print("Calculating new result")
+            cache_list_[args] = func(*args)
+            return func(*args)
+    return wrapper
