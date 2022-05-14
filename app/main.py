a00be955166f6da0937f231bf32067c7ae02eb5b
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-14 14:55:48 +0300

    corrected solution

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ def cache(func):
     cash = {}
 
     def inner(*args, **kwargs):
-        nonlocal cash
         if args not in cash:
             print("Calculating new result")
             cash[args] = func(*args, **kwargs)
+            return func(*args, **kwargs)
         else:
             print("Getting from cache")
+            return cash[args]
     return inner
