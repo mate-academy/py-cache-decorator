7e6f1dd3d9b4a1aa389df617d67d65c97f6b71da
Hectorovich <andrew.gutnik10@gmail.com>
2022-05-15 20:18:12 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 def cache(func):
-    # Write your code here
-    pass
+    data = dict()
+
+    def wrapper(*args):
+        nonlocal data
+
+        if args in data:
+            print("Getting from cache")
+            return data[args]
+        else:
+            result = func(*args)
+            data[args] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
