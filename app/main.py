898d6e4ff42fe6fd32fa9bed56bfce45fdc1b03a
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-05-15 16:15:18 +0300

    cache_decorator solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 def cache(func):
-    # Write your code here
-    pass
+    cached_values_list = []
+
+    def inner(*args, **kwargs):
+        nonlocal cached_values_list
+
+        sublist = [*args]
+
+        if sublist in cached_values_list:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+
+        cached_values_list += [[*args]]
+
+        return func(*args, **kwargs)
+
+    return inner
