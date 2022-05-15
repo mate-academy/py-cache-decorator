68bff869ca20127d6b0aa3616c30bccf886756a8
Vasyl Smutok <23.smutok@gmail.com>
2022-05-15 23:04:16 +0300

    Second commit Replaced the data list with a dictionary in which key are the arguments of the function, and the value is the result of this function, if the arguments of the function are repeated, the function is not executed, and its result is taken from cache_date

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def cache(func):
-    cache_date = []
+    cache_date = {}
 
-    def wrapper(*args, **kwargs):
-        if func(*args, **kwargs) in cache_date:
+    def wrapper(*args):
+        if args in cache_date:
             print("Getting from cache")
         else:
+            cache_date[args] = func(*args)
             print("Calculating new result")
-        cache_date.append(func(*args, **kwargs))
-        return func(*args, **kwargs)
+        return cache_date[args]
     return wrapper
