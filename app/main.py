2a5c24346a8d9dc256122624d34f563a1757d0e0
Artem Kharchenko <darten360@gmail.com>
2022-05-15 15:58:36 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,28 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
-    pass
+    memo = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        if args in memo:
+            print('Getting from cache')
+            return memo[args]
+        else:
+            print("Calculating new result")
+            rv = func(*args)
+            memo[args] = rv
+            return rv
+
+    return wrapper
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
