f043441defbe8160c4a49e5f52a5d42cffd55ad6
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-05-15 15:09:31 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+
+    lis = []
+
+    def inner(*args):
+
+        if args not in lis:
+            lis.append(args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+    return inner
+
