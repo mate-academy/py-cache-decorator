fffdba6b6e618a0fae3196c617f79d7672c50961
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-05-16 14:28:23 +0300

    After pytest

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def cache(func):
         if args not in lis:
             lis.append(args)
             print("Calculating new result")
+            return func(*args)
         else:
             print("Getting from cache")
+            return func(*args)
     return inner
