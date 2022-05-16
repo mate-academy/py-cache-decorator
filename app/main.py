7955be6d10355ae68911d36f9ab0656a0ad004a2
Daria Zhuravlova <darizhur.vol2@gmail.com>
2022-05-16 19:44:09 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ def cache(func):
     result = {}
 
     def decorate(*args, **kwargs):
-        nonlocal result
         if args in result:
             print('Getting from cache')
             return result[args]
         else:
             result[args] = func(*args, **kwargs)
             print('Calculating new result')
-            return func(*args, **kwargs)
+            return result[args]
 
     return decorate
