ea7ec244ca4add98e068fd2ecdbf1142ff696e18
andrii kravchenko <andriej.krawczenko@gmail.com>
2022-05-16 20:42:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,9 +4,8 @@ def cache(func):
     def inner(*args):
         if args in c_dict:
             print('Getting from cache')
-            return c_dict[args]
         else:
             print('Calculating new result')
             c_dict[args] = func(*args)
-            return c_dict[args]
+        return c_dict[args]
     return inner
