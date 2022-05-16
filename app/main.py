fb9cb73d0c78695366688b2ddecb23c9dade1c15
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-05-16 19:10:00 +0300

    Done with using dict

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 def cache(func):
-    cached_values_list = []
+    cached_values_dict = {}
 
     def inner(*args, **kwargs):
-        nonlocal cached_values_list
 
-        sublist = [*args]
-
-        if sublist in cached_values_list:
+        if args in cached_values_dict:
             print("Getting from cache")
+            result = cached_values_dict[args]
         else:
             print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached_values_dict[args] = result
 
-        cached_values_list += [[*args]]
-
-        return func(*args, **kwargs)
+        return result
 
     return inner
