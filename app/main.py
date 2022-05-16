1f3dac0da4e301e211b56a2041d97b8d156f937e
Flashmobber <ufinaid@gmail.com>
2022-05-16 15:55:52 +0300

    Recommendations applied

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ def cache(func):
     results = {}
 
     def inner(*args):
-        if args in results is not None:
+        if args in results:
             print("Getting from cache")
-            return results[args]
+        else:
+            print("Calculating new result")
+            results[args] = func(*args)
 
-        print("Calculating new result")
-        results[args] = func(*args)
         return results[args]
 
     return inner
