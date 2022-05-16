1ccc6fbd5a07e71e8349cb86c4d751ca10e4994d
Dima Svirsa <august4smile@gmail.com>
2022-05-16 19:13:19 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ def cache(func):
     def inc_sort(*args):
         if args in results_for_cache:
             print("Getting from cache")
-            return results_for_cache[args]
         else:
             print("Calculating new result")
             results_for_cache[args] = func(*args)
-            return results_for_cache[args]
+        return results_for_cache[args]
 
     return inc_sort
