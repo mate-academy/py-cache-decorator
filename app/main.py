b37d492d1d5ddf9edc8708f9abe62b4239a28203
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-05-16 18:29:39 +0300

    Remove testing functions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def cache(func):
-    cache_dict = {}
+    cache_dict = dict()
 
     def inner(*args, **kwargs):
         if args in cache_dict:
@@ -9,21 +9,6 @@ def cache(func):
             print("Calculating new result")
             result = func(*args, **kwargs)
             cache_dict[args] = result
-        return result
+            return result
 
     return inner
-
-
-@cache
-def sum(a, b):
-    return a + b
-
-
-@cache
-def long_time_func(a, b, c):
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
