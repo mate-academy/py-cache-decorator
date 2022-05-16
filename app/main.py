376c87da7650717deb265b2cb4e27c34d9605d0f
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-05-16 16:00:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ def cache(func):
     def inner(*args, **kwargs):
         if args in res:
             print("Getting from cache")
-            return res[args]
         else:
             print("Calculating new result")
             res[args] = func(*args)
-            return res[args]
+
+        return res[args]
     return inner
