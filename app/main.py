bb42931c3d22d4193dc914c5a9304b833ad3e675
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-16 15:06:44 +0300

    'Solution_16/05_#1'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+
+    long_time_list = []
+
+    def inner(*args, **kwargs):
+
+        list_args = [*args]
+        if list_args in long_time_list:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+        long_time_list.append([*args])
+        return func(*args, **kwargs)
+    return inner
