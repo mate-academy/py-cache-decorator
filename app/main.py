a40ebdee709cd65246a8afa0909e065dc502a99e
Dasha Protsko <protscodasha@icloud.com>
2022-05-16 17:26:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    cached_values_lst = []
+
+    def inner(*args, **kwargs):
+
+        sub_lst = [*args]
+
+        if sub_lst in cached_values_lst:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+
+        cached_values_lst.append([*args])
+
+        return func(*args, **kwargs)
+
+    return inner
