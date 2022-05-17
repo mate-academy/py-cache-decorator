ba78f35500368275c7cf3b8ba5ecbd886f341e13
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-05-17 11:50:40 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,18 +2,10 @@ def cache(func):
     cache_dict = {}
 
     def inner(*args):
-        mutable = False
-        for argument in args:
-            if type(argument) in [list, dict, set]:
-                mutable = True
-        if mutable:
-            return func(*args)
+        if args in cache_dict.keys():
+            print("Getting from cache")
         else:
-            if args in cache_dict.keys():
-                print("Getting from cache")
-                return cache_dict[args]
-            else:
-                cache_dict[args] = func(*args)
-                print("Calculating new result")
-                return cache_dict[args]
+            cache_dict[args] = func(*args)
+            print("Calculating new result")
+        return cache_dict[args]
     return inner
