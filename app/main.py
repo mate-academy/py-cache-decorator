e249a871c5456d1f764ad8c0b370b8db16b85ffb
Yura Beseda <yuriy.beseda@gmail.com>
2022-05-17 10:54:01 +0200

    'List_changed_to_dict'

--- app/main.py
+++ app/main.py
@@ -1,26 +1,12 @@
 def cache(func):
-    caches = []
+    caches = {}
 
     def inner(*args):
-        local_cache = []
-        for arg in args:
-            local_cache.append(arg)
-        if local_cache in caches:
+        if args in caches:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            caches.append(local_cache)
+            caches[args] = func(*args)
 
-        return func(*args)
-
+        return caches[args]
     return inner
-
-
-@cache
-def long_time_func(a, b, c):
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
