4cada0cd12b6921f28b07a49d76830eca1a3b07e
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-05-18 11:29:54 +0300

    'Solution_16/05_#3'

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ def cache(func):
 
     def inner(*args, **kwargs):
 
-        list_args = [*args]
-        if str(list_args) in long_time_list:
+        if tuple(args) in long_time_list:
             print("Getting from cache")
-            return long_time_list[str(list_args)]
+            return long_time_list[tuple(args)]
         else:
             print("Calculating new result")
-            long_time_list[str(list_args)] = func(*args, **kwargs)
-            return func(*args, **kwargs)
+            long_time_list[tuple(args)] = func(*args, **kwargs)
+            return long_time_list[tuple(args)]
     return inner
