49014904fbbd56da101269a519989d569b09a1be
Anton Lasko <anton.lasko1993@gmail.com>
2022-05-20 16:46:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
-    pass
+    my_dict = {}
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        storage = (func, args)
+        if storage in my_dict:
+            print('Getting from cache')
+            result = my_dict[storage]
+        else:
+            print('Calculating new result')
+            result = func(*args)
+            my_dict[storage] = result
+        return result
+    return wrapper
