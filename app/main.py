617e8efca52d797043e831d90f997274256c45a1
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-05-27 15:18:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
 def cache(func):
-    # Write your code here
-    pass
+    cached_data = {}
+
+    def wrap(*args):
+        nonlocal cached_data
+        for key, value in {args: func(*args)}.items():
+            if str(key) not in cached_data:
+                cached_data.update({f"{args}": func(*args)})
+                print("Calculating new result")
+                return cached_data[str(key)]
+            else:
+                print("Getting from cache")
+                return cached_data[str(key)]
+    return wrap
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
