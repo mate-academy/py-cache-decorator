0d4ae66135ebce2f786df377c1db5ef4717d8826
Andrii Padalko <a.padalko@gmail.com>
2022-05-30 15:07:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+cache_dict = {}
+
+
 def cache(func):
-    # Write your code here
-    pass
+    def inner(*args):
+        global cache_dict
+        if not cache_dict or func not in cache_dict:
+            cache_dict.update({func: {args: func(*args)}})
+            print("Calculating new result")
+            return func(*args)
+        else:
+            if func in cache_dict and args not in cache_dict[func].keys():
+                cache_dict[func][args] = func(*args)
+                print("Calculating new result")
+                return func(*args)
+        print("Getting from cache")
+        return cache_dict[func][args]
+    return inner
