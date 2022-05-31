6451c51539b20b393b4ddbbd99ad29017138aa30
SenchaPencha <SenchaPencha@mail.ru>
2022-05-31 18:06:31 +0300

    Final

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@ from functools import wraps
 
 
 def cache(func):
-    f_name = func.__name__
     dict_cache = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args):
         if f"{args}" in dict_cache:
             print("Getting from cache")
             return dict_cache[f"{args}"]
