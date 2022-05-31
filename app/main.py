ba6a6af9462eb197fc1004ff842a705561e68664
SenchaPencha <SenchaPencha@mail.ru>
2022-05-31 16:30:39 +0300

    first

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from functools import wraps
+
+dict_cache = {}
+
+
 def cache(func):
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if f"{func.__name__} {args}" in dict_cache:
+            print("Getting from cache")
+            return dict_cache[f"{func.__name__} {args}"]
+        else:
+            dict_cache.update({f"{func.__name__} {args}": func(*args, **kwargs)})
+            print("Calculating new result")
+            return func(*args, **kwargs)
+
+    return wrapper
