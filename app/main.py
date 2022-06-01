a3baaedd1fd0cb13a0110b57f21207c9bf5c041b
Denis <sairustv228@gmail.com>
2022-06-01 19:17:23 +0300

    v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_date = {}
+
+    def wrapper(*args):
+        if args in cache_date:
+            print("Getting from cache")
+        else:
+            cache_date[args] = func(*args)
+            print("Calculating new result")
+        return cache_date[args]
+    return wrapper
