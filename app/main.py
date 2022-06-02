081c0783f1782d50cb4983f098397a8556214f82
MIlan <milan.manoylov@gmail.com>
2022-06-02 16:45:03 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_ = {}
+
+    def wrapper(*args):
+        if args in cache_:
+            print("Getting from cache")
+        else:
+            cache_[args] = func(*args)
+            print("Calculating new result")
+        return cache_[args]
+    return wrapper
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
