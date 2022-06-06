6355ae565bdb75ca66cd40747f3c25c7305b0219
MIlan <milan.manoylov@gmail.com>
2022-06-06 14:12:38 +0200

    'Last_Solution_1'

--- app/main.py
+++ app/main.py
@@ -19,7 +19,3 @@ def long_time_func(a, b, c):
 @cache
 def long_time_func_2(n_tuple, power):
     return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
