f0c4bc9c2cb6d12ea6203722fa4eb779d72257a7
muhactive <muhactive@gmail.com>
2022-06-09 21:27:24 +0300

    one instead of two return

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ def cache(func):
         if args in stores_results:
 
             print("Getting from cache")
-            return stores_results[args]
 
         else:
 
             stores_results[args] = func(*args)
             print("Calculating new result")
 
-            return stores_results[args]
+        return stores_results[args]
 
     return wrapper
