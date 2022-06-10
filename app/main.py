94a716098ce1b147704cb40ddf619d4d0af218f5
Misha Hrynko <misha@mate.academy>
2022-06-10 16:50:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,12 +2,11 @@ def cache(func):
     cache_of_args = {}
 
     def wrapper(*args, **kwargs):
+        if args in cache_of_args.keys():
+            print("Getting from cache")
         if args not in cache_of_args:
             res = func(*args, **kwargs)
             cache_of_args[args] = res
             print("Calculating new result")
-            return res
-        if args in cache_of_args.keys():
-            print("Getting from cache")
         return cache_of_args[args]
     return wrapper
