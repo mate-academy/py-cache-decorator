5141235f76eab23fc170db4267841b8444708776
Tetiana Omelchenko <t.omel4enko@gmail.com>
2022-06-11 22:22:06 +0800

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
-    # Write your code here
+    cache_dict = {}
+
+    def wrapper(*args):
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache_dict[args] = result
+            return result
+
+    return wrapper
     pass
