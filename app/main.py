40e5f7de3c12916eac70062b481fb3faffb6ca82
Slava Seredenko <vrungelsp@gmail.com>
2022-06-14 20:15:18 +0300

    Solution_try_1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
 def cache(func):
-    # Write your code here
-    pass
+    total_list = []
+
+    def inside(*args):
+        for i in total_list:
+            if args in i.keys():
+                print("Getting from cache")
+                return i[args]
+
+        print("Calculating new result")
+        result = func(*args)
+        total_list.append({args: result})
+        return result
+
+    return inside
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
