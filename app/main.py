50efd4ad9722b42e326900aa66cffee5a51e476c
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-06-30 21:02:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+
+    def inner(*args):
+        nonlocal results
+        if args not in results.keys():
+            print("Calculating new result")
+            result_func = func(*args)
+            results[args] = result_func
+            return result_func
+        else:
+            print("Getting from cache")
+            return results[args]
+
+    return inner
