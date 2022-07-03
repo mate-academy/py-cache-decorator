45773ccff0aca2c6cb23c14e4e629aaf25ce906f
Roman <coderc770@gmail.com>
2022-07-03 19:09:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    list_arguments = []
+
+    def inner(*args):
+
+        if args not in list_arguments:
+            list_arguments.append(args)
+            print('Calculating new result')
+            global result
+            result = func(*args)
+
+        else:
+            print('Getting from cache')
+
+        return result
+
+    return inner
