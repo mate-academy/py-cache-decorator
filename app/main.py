a6e398568a3b683edaf6b6abf84cdcec0ecea1c6
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-07-04 13:09:08 +0300

    fix 2 problem

--- app/main.py
+++ app/main.py
@@ -3,13 +3,12 @@ def cache(func):
 
     def inner(*args):
         results
-        if args not in results.keys():
+        if args not in results:
             print("Calculating new result")
             result_func = func(*args)
             results[args] = result_func
             return result_func
-        else:
-            print("Getting from cache")
-            return results[args]
+        print("Getting from cache")
+        return results[args]
 
     return inner
