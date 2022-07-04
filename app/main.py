624538b10bd221d4f581d2ea7ad4682215ab3c7b
KevinXD <kevinfdevid@gmail.com>
2022-07-04 09:44:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_results = {}
+
+    def wrapped(*args, **kwargs):
+        function_args = tuple([*args])
+        if function_args not in cache_results:
+            res = func(*args)
+            cache_results[function_args] = res
+            print("Calculating new result")
+            return res
+        else:
+            print("Getting from cache")
+            return cache_results.get(function_args)
+
+    return wrapped
