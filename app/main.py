7699956b68aa877e340b90a0d25a92f8b073f2a1
Timur <timur.hike@gmail.com>
2022-07-13 00:11:41 +0300

    solution2

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ def cache(func):
     save = {}
 
     def wrapper(*args):
-        if args not in save:
-            result = func(*args)
-            save[args] = result
-            print("Calculating new result")
-            return result
-        print("Getting from cache")
-        return save[args]
+        if args in save:
+            print("Getting from cache")
+            return save[args]
+        result = func(*args)
+        save[args] = result
+        print("Calculating new result")
+        return result
 
     return wrapper
