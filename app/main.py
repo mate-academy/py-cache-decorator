b5e84589acb346aa546b5fc94d4450f47622b836
Anastasiia <nastya.gnusova@gmail.com>
2022-07-19 21:23:40 +0300

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 def cache(func):
-    count = 0
     dict_of_args = {}
 
     def wrapper(*args):
-        nonlocal count
-        nonlocal dict_of_args
-        count += 1
-        if count == 1 or args not in dict_of_args:
+        if args not in dict_of_args:
             print("Calculating new result")
             dict_of_args[args] = func(*args)
         else:
