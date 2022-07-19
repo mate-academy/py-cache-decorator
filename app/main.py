23b311c90cf98fcb58df140787b7981cf8c469e1
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-07-19 23:56:18 +0300

    Task Сompleted v2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def cache(func):
     temp_dict = {}
+
     def inner(*args):
+
         if args in temp_dict:
-            print(f"Getting from cache")
+            print("Getting from cache")
         else:
-            print(f"Calculating new result")
+            print("Calculating new result")
             temp_dict[args] = func(*args)
         return temp_dict[args]
     return inner
