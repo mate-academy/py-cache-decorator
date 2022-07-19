7d913a2d6eba6ac86b92fb0ddb8d9815f3b409c7
Anastasiia <nastya.gnusova@gmail.com>
2022-07-19 10:51:55 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    count = 0
+    dict_of_args = {}
+
+    def wrapper(*args):
+        nonlocal count
+        nonlocal dict_of_args
+        count += 1
+        if count == 1 or args not in dict_of_args:
+            print("Calculating new result")
+            dict_of_args[args] = func(*args)
+        else:
+            print("Getting from cache")
+
+        return dict_of_args[args]
+
+    return wrapper
