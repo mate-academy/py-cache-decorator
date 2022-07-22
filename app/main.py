2497ef5496a7e20d940291c0040585516759b251
Paul Starodub <amorallex@gmail.com>
2022-07-22 13:29:13 +0300

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def cache(func):
-    my_dictionary = {}
+    dictionary = {}
 
     def wrapper(*args):
-        my_key = args
-        if my_key not in my_dictionary.keys():
+        key = args
+        if key not in dictionary.keys():
             print("Calculating new result")
-            my_dictionary[my_key] = func(*args)
+            dictionary[key] = func(*args)
         else:
             print("Getting from cache")
-        return my_dictionary[my_key]
+        return dictionary[key]
 
     return wrapper
