48a546c3482c2d3e7f1b641c4a6e4f42ab4d5ea2
Volodymyr Panov <vpanov25@gmail.com>
2022-07-22 16:15:59 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 def cache(func):
-    # Write your code here
-    pass
+    name_of_the_function = {}
+    if func.__name__ not in name_of_the_function:
+        name_of_the_function[func.__name__] = {}
+
+    def inner(*args, **kwargs):
+        if len(name_of_the_function[func.__name__]) == 0:
+            print("Calculating new result")
+            try:
+                result = func(*args)
+                name_of_the_function[func.__name__][args] = result
+                return result
+            except TypeError:
+                name_of_the_function[func.__name__][args] = args
+
+        elif args in name_of_the_function[func.__name__]:
+            print("Getting from cache")
+            return name_of_the_function[func.__name__][args]
+
+        else:
+            print("Calculating new result")
+            try:
+                name_of_the_function[func.__name__][args] = func(*args)
+                return func(*args)
+            except TypeError:
+                name_of_the_function[func.__name__][args] = args
+    return inner
