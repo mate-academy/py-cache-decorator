611ba8e2c85520fd91e763a81557322441bad4a4
AnDy <ansmbox@gmail.com>
2022-07-23 17:01:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 def cache(func):
-    # Write your code here
-    pass
+    calls_cache = {}
+
+    def wrapper(*args):
+        nonlocal calls_cache
+        call_key = func.__name__ + "_".join([str(item) for item in args])
+
+        if call_key in calls_cache:
+            res = calls_cache[call_key]
+            print("Getting from cache")
+        else:
+            res = func(*args)
+            calls_cache[call_key] = res
+            print("Calculating new result")
+
+        return res
+
+    return wrapper
