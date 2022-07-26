9494df1ab3dc20a1525177a6c4b779f752c31de4
Volodymyr Panov <vpanov25@gmail.com>
2022-07-26 08:39:30 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,27 +2,16 @@ def cache(func):
     name_of_the_function = {}
 
     def inner(*args, **kwargs):
-        if name_of_the_function is False or func.__name__ \
-                not in name_of_the_function:
-            print("Calculating new result")
-            name_of_the_function[func.__name__] = {}
-            if isinstance(args, str):
-                name_of_the_function[func.__name__][args] = args
-            else:
-                result = func(*args)
-                name_of_the_function[func.__name__][args] = result
-                return result
-
-        elif args in name_of_the_function[func.__name__]:
+        nonlocal name_of_the_function
+        if args in name_of_the_function:
             print("Getting from cache")
-            return name_of_the_function[func.__name__][args]
-
+            return name_of_the_function[args]
         else:
             print("Calculating new result")
             if isinstance(args, str):
-                name_of_the_function[func.__name__][args] = args
+                name_of_the_function[args] = args
             else:
-                result = func(*args)
-                name_of_the_function[func.__name__][args] = result
-                return result
+                name_of_the_function[args] = func(*args)
+                return name_of_the_function[args]
+
     return inner
