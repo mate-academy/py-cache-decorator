c804fef5539be06c16e6f9b3404d921e91ea0506
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-08-05 13:46:36 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,19 +6,13 @@ def cache(func):
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if (func.__name__, args) in global_cache:
+        # if (func.__name__, args) in global_cache:
+        if args in global_cache:
             print("Getting from cache")
-            return global_cache[(func.__name__, args)]
+            return global_cache[args]
         else:
             print("Calculating new result")
-            # global_cache.append((func.__name__, args))
-        if func.__name__ == "long_time_func":
-            a, b, c = args
-            res = func(a, b, c)
-            global_cache[(func.__name__, args)] = res
-            return res
-        a, b = args
-        res = func(a, b)
-        global_cache[(func.__name__, args)] = res
+        res = func(*args)
+        global_cache[args] = res
         return res
     return wrapper
