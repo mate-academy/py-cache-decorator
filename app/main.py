fef9499bfb1b2e6d291189a2e8b83818c1dd8640
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-08-13 12:18:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 def cache(func):
-    # Write your code here
-    pass
+    runs_dict = {}
+
+    def wrapper(*args, **kwargs):
+        result = None
+        if id(func) in runs_dict:
+            for runs in runs_dict[id(func)]:
+                if args == runs['args'] and kwargs == runs['kwargs']:
+                    print('Getting from cache')
+                    result = runs['result']
+        else:
+            print('Calculating new result')
+            result = func(*args, **kwargs)
+            runs_dict.update({id(func):
+                             [{'args': args,
+                               'kwargs': kwargs,
+                               'result': result}]})
+        if result is None:
+            print('Calculating new result')
+            result = func(*args, **kwargs)
+            runs_dict[id(func)] += \
+                [{'args': args,
+                  'kwargs': kwargs,
+                  'result': result}]
+        return result
+
+    return wrapper
