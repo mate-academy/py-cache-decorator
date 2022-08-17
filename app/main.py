51044922e587d0c96d84666e0ae668aa33fe85ca
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-08-17 21:41:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
     # Write your code here
-    pass
+    dict_results = {}
+
+    def wrapper(*args):
+        nonlocal dict_results
+        if args in dict_results:
+            print("Getting from cache")
+            return dict_results[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            dict_results.update({args: result})
+        return result
+    return wrapper
