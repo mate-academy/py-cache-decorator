5ceb18803009ee92fe98cab141d40f7373b78c59
Ivan Antonishak <jus1stored@gmail.com>
2022-08-17 19:24:43 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrapper(*args):
+        nonlocal storage
+        if args in storage:
+            print("Getting from cache")
+            return storage[args]
+        else:
+            print("Calculating new result")
+            storage[args] = func(*args)
+            return storage[args]
+    return wrapper
