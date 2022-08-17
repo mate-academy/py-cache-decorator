04a67dbf56cd4e4a0490af7d91a3265dd6724b9c
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-08-17 10:13:41 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_list = {}
+
+    def wraper(*args, **kwargs):
+        nonlocal cache_list
+        if args in cache_list:
+            print("Getting from cache")
+            return cache_list[args]
+        else:
+            print("Calculating new result")
+            new_value = func(*args)
+            cache_list[args] = new_value
+            return new_value
+    return wraper
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
