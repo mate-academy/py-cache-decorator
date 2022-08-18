1a5755b1f57d522a096c889268784eb0349a4058
Davyd Kryvko <davidkrivko@gmail.com>
2022-08-18 17:50:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    d_memory = {}
+
+    def inner(*args):
+        if args in d_memory.keys():
+            print("Getting from cache")
+            return func(*args)
+        else:
+            d_memory[args] = func(*args)
+            print("Calculating new result")
+            return func(*args)
+    return inner
