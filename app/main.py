d98fc48d79edb67ffce6a3268dd9a9a046223c71
Eugene <buntyc8@gmail.com>
2022-08-23 16:39:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_result = {}
+
+    def wripper(*args):
+        if args not in cache_result:
+            result_func_args = func(*args)
+            cache_result[args] = result_func_args
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_result[args]
+    return wripper
