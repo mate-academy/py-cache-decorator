270358f0f0ff64e18bc3069013527148bec23268
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-08-25 23:17:54 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+    def wrapper(*args):
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            result_new = func(*args)
+            cache_dict[args] = result_new
+            print("Calculating new result")
+            return result_new
+
+    return wrapper
