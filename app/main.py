464f33b5b1a710828ef9a1b49b1f67eb37e855ec
Soroka Igor <copokaigor@gmail.com>
2022-08-25 16:39:08 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -10,13 +10,3 @@ def cache(func):
             print("Getting from cache")
             return cache_dict[args]
     return wrapper1
-
-
-@cache
-def long_time_func(a, b, c):
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
