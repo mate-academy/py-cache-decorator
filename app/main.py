1b85ddeb72b2e203e06e1be5255673837ff0d693
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-08-27 19:58:59 +0300

    Solution for Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
     # Write your code here
-    pass
+    dict_for_cache = {}
+
+    def inner(*args):
+        key_name_dict = args  # For search value in dictionary with name function
+        if key_name_dict in dict_for_cache:
+            print("Getting from cache")
+            return dict_for_cache[key_name_dict]
+        new_result = func(*args)
+        dict_for_cache[key_name_dict] = new_result
+        print("Calculating new result")
+        return new_result
+
+    return inner
