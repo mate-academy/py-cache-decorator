1b748c2213700d3822b4743a697215c2f6bd5218
LitvinOleksandr <sanek.litvin@gmail.com>
2022-08-30 14:11:46 +0300

    create decorator cache, whom save result function to storage var, if function has equals inputs from another function - return result from cache storage var.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    storage = []
+
+    def run(*args):
+        nonlocal storage
+        for numb, val in enumerate(storage):
+            if storage[numb].get(args) is not None:
+                print("Getting from cache")
+                return storage[numb].get(args)
+        out = func(*args)
+        storage.append({args: out})
+        print("Calculating new result")
+        return out
+    return run
