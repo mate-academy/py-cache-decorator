a3e90214201716efd8273b9de068dbbe5c232909
Kyryll Katash <kyryllkatash@gmail.com>
2022-08-31 20:27:43 +0300

    Solution for decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+
+    def inner(*args):
+        if args in results:
+            print("Getting from cache")
+        else:
+            results[args] = func(*args)
+            print("Calculating new result")
+        return results[args]
+    return inner
