fb92d10adc0dcbcb609ff9c7d0ed612a6234b8c1
Your Name <y>
2022-09-03 17:56:50 +0300

    Solution 'cashe-decorator' was added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+
+    def wrapper(*args, **kwargs):
+        if isinstance(args, list) or isinstance(args, dict)\
+                or isinstance(args, set):
+            return "This function isn't working with mutable arguments"
+        else:
+            if args in results:
+                print("Getting from cache")
+            else:
+                print("Calculating new result")
+                results[args] = func(*args)
+
+            return results[args]
+
+    return wrapper
