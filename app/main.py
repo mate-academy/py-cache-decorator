3d0d38bc65e7263157fad32cc49cd2635e85a52b
Your Name <y>
2022-09-05 11:24:18 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -2,16 +2,12 @@ def cache(func):
     results = {}
 
     def wrapper(*args, **kwargs):
-        if isinstance(args, list) or isinstance(args, dict)\
-                or isinstance(args, set):
-            return "This function isn't working with mutable arguments"
+        if args in results:
+            print("Getting from cache")
         else:
-            if args in results:
-                print("Getting from cache")
-            else:
-                print("Calculating new result")
-                results[args] = func(*args)
+            print("Calculating new result")
+            results[args] = func(*args)
 
-            return results[args]
+        return results[args]
 
     return wrapper
