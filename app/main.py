1aa20455b314dccc3161c5bbc40b3a114ba05187
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-09-05 14:53:33 +0300

    Solution v.02 (change to one "return")

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 def cache(func):
     result = {}
 
-    def inner(*args):
+    def wrapper(*args):
         if args in result:
             print("Getting from cache")
-            return result[args]
         else:
+            result[args] = func(*args)
             print("Calculating new result")
-            new_result = func(*args)
-            result.update({args: new_result})
-            return new_result
-    return inner
+        return result[args]
+
+    return wrapper
