3ca944d87d15e6e97999cc35523672d7dbc1969f
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-05 18:43:35 +0300

    14

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 def cache(func):
     result_dict = {}
-    
+
     def wrapper(*args):
         if args in result_dict:
             print("Getting from cache")
         else:
             result_dict[args] = func(*args)
             print("Calculating new result")
-
         return result_dict[args]
     return wrapper
