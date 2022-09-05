84772e76a267bd72500a6e64f9a5f59bd7802cef
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-05 18:09:53 +0300

    refactoring values name

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def cache(func):
-    result = {}
+    result_dict = {}
     def wrapper(*args):
-        if args in result:
+        if args in result_dict:
             print("Getting from cache")
         else:
-            result[args] = func(*args)
+            result_dict[args] = func(*args)
             print("Calculating new result")
 
-        return result[args]
+        return result_dict[args]
     return wrapper
