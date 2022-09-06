9487ea745a05c9f3b928047a505ab90414192ca1
Artem Dubinin <artemdubinin.ad@gmail.com>
2022-09-06 21:24:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
 def cache(func):
-    # Write your code here
-    pass
+    storage = {}
+
+    def inner(*args):
+        args = tuple(args)
+        func_name = func.__name__
+
+        if func_name in storage and args in storage[func_name]:
+            print('Getting from cache')
+            return storage[func_name][args]
+
+        print('Calculating new result')
+        func_res = func(*args)
+
+        if func_name not in storage:
+            storage[func_name] = {args: func_res}
+        else:
+            storage[func_name][args] = func_res
+        return storage[func_name][args]
+
+    return inner
