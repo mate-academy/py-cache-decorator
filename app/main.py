1cea4c44eaf4ebe525b6029eab4f1269669736fe
Anton Chumak <chumak7377@gmail.com>
2022-09-07 15:30:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(function):
+    data_array = {}
+
+    def runtime(*data):
+        if data in data_array:
+            print("Getting from cache")
+            return data_array[data]
+        else:
+            print("Calculating new result")
+            function_result = function(*data)
+        data_array[data] = function_result
+
+        return function_result
+
+    return runtime
