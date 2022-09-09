4dac234fb1e5bd7b09c746c306e0d2cb7ed29fdd
Vitalii Khmura <vitalikkhmura13@gmail.com>
2022-09-09 23:15:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+
 def cache(func):
-    # Write your code here
-    pass
+    data = dict()
+
+    def wrapper(*args, **kwargs):
+        if args not in data:
+            data[args] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return data[args]
+    return wrapper
