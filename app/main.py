04b58506a5307ead16543f1923a6c0374f2869d7
IvanGLS <gladiolus0707@gmail.com>
2022-09-09 14:32:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cash_data = dict()
+
+    def time_cashed(*args, **kwargs):
+        if args not in cash_data:
+            cash_data[args] = func(*args)
+            print('Calculating new result')
+            return cash_data[args]
+        elif args in cash_data:
+            print('Getting from cache')
+            return cash_data[args]
+    return time_cashed
