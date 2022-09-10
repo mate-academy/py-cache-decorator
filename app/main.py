4b866dc7277f1a0f41d6fd91742b5cb7e8a5d5c4
Ivan Berezyanskiy <65457374+Vanya2389@users.noreply.github.com>
2022-09-10 15:05:14 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ def cache(func):
         if args in cache_dict:
             print('Getting from cache')
             return cache_dict[args]
-            
             print('Calculating new result')
             cache_dict[args] = func(*args)
             return cache_dict[args]
