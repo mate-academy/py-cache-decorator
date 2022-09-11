050a1e67a7abaea2705f2610005d4e90988de6fc
Taras Zelinskyi <taras.sv89@gmail.com>
2022-09-12 00:09:49 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args):
+        nonlocal cache_dict
+        if args in cache_dict.keys():
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            my_func = func(*args)
+            cache_dict[args] = my_func
+            print("Calculating new result")
+            return my_func
+    return inner
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple, power):
+    return [number ** power for number in n_tuple]
