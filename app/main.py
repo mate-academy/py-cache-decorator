6d3decc26680705425522f1939a7f1904ed2703e
Ivan Antonishak <jus1stored@gmail.com>
2022-09-12 16:21:44 +0300

    Remove nonlocal

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ def cache(func):
     storage = {}
 
     def wrapper(*args):
-        nonlocal storage
         if args in storage:
             print("Getting from cache")
-            return storage[args]
         else:
             print("Calculating new result")
             storage[args] = func(*args)
-            return storage[args]
+        return storage[args]
     return wrapper
