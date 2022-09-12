cb9ad1f6692f03c6419e65ca22bd67d3da5ee5bc
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-09-12 20:17:45 +0300

    added cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    saved = {}
+
+    def inner(*args):
+        func_with_args = (func, args)
+
+        if func_with_args in saved:
+            print("Getting from cache")
+            return saved[func_with_args]
+
+        print("Calculating new result")
+        res = func(*args)
+        saved[func_with_args] = res
+
+        return res
+
+    return inner
