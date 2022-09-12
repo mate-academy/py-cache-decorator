7b62122f0c86e0c972d59d598a750c560dbcaa9a
Taras Andruschenko <And999110@gmail.com>
2022-09-12 19:54:27 +0300

    Remove 'nonlocal'

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ def cache(func):
     cache_dict = {}
 
     def wrap_cache(*args):
-        nonlocal cache_dict
         stored_result = cache_dict.get(args, False)
         if args in cache_dict:
             print("Getting from cache")
@@ -12,4 +11,5 @@ def cache(func):
             func_result = func(*args)
             cache_dict[args] = func_result
             return func_result
+
     return wrap_cache
