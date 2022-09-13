4aa8ecb1c8a15b56ff187251f604b06d974e0aae
Taras Andruschenko <And999110@gmail.com>
2022-09-13 16:55:30 +0300

    Remove useless variable and use single return statement

--- app/main.py
+++ app/main.py
@@ -2,14 +2,11 @@ def cache(func):
     cache_dict = {}
 
     def wrap_cache(*args):
-        stored_result = cache_dict.get(args, False)
         if args in cache_dict:
             print("Getting from cache")
-            return stored_result
         else:
             print("Calculating new result")
-            func_result = func(*args)
-            cache_dict[args] = func_result
-            return func_result
+            cache_dict[args] = func(*args)
+        return cache_dict[args]
 
     return wrap_cache
