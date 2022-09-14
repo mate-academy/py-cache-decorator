122406fd475b8e96b8c61fdf08566ebbb266176f
Mazur Mariya <mazurinka29@gmail.com>
2022-09-14 18:36:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
 def cache(func):
     cache_of_results = {}
-    cache_of_funcs = []
 
-    def wrapper(*args, **kwargs):
-        all_args = str(args) + str(**kwargs)
-        if func.__name__ in cache_of_funcs and all_args in cache_of_results:
+    def wrapper(*args):
+        if args in cache_of_results:
             print("Getting from cache")
-            return cache_of_results[all_args]
+            return cache_of_results[args]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_of_results.update({all_args: result})
-            cache_of_funcs.append(str(func.__name__))
+            result = func(*args)
+            cache_of_results.update({args: result})
             return result
     return wrapper
