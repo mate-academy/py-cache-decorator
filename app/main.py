11c3e57177d3bab358966ecdff74178da33c4faf
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-09-14 17:23:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 def cache(func):
-    # Write your code here
-    pass
+    cash_repository = {}
+
+    def wrapper(*args):
+        if args not in cash_repository:
+            print("Calculating new result")
+            cash_repository[args] = func(*args)
+        else:
+            print("Getting from cache")
+        return cash_repository[args]
+    return wrapper
