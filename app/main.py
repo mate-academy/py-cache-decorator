b722d9e94c7c925ed0869fb8e3c2ea55114036f0
Edik Marushchak <e.moscow138@gmail.com>
2022-09-15 08:31:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cash_storJ = {}
+
+    def wrapper(*args):
+        if args not in cash_storJ:
+            print("Calculating new result")
+            cash_storJ[args] = func(*args)
+        else:
+            print("Getting from cache")
+        return cash_storJ[args]
+
+    return wrapper
