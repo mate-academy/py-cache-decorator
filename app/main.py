dad1db5feed6bf0d5e3c5be983869728d60cafb6
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-09-15 21:14:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(function):
+    cache_results = {}
+
+    def wrapper(*args):
+        if args not in cache_results:
+            print("Calculating new result")
+            cache_results[args] = function(*args)
+        else:
+            print("Getting from cache")
+        return cache_results[args]
+
+    return wrapper
