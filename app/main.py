ac800d9a810f600897537947d9aa477954325f49
Nikyxa <kolisova.veronika81@gmail.com>
2022-09-16 18:24:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+
+    def inner(*args):
+        if args not in results:
+            print("Calculating new result")
+            results[args] = func(*args)
+        else:
+            print("Getting from cache")
+        return results[args]
+
+    return inner
+
