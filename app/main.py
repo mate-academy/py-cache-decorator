f8a361b96cc0183194ddf91a216f22c00a035497
Alexander <alexzubkoff123@gmail.com>
2022-09-16 17:40:26 +0300

    cache decorator func

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    dict_cache = {}
+
+    def inner(*args):
+        if args not in dict_cache:
+            dict_cache[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return dict_cache[args]
+
+    return inner
