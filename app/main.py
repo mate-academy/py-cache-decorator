6fbe4d3d7630d85ee1ea48a7661259570b3db005
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-09-17 09:29:16 +0300

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -2,15 +2,11 @@ def cache(func):
     info = {}
 
     def wrapper(*args):
-        nonlocal info
         if args in info:
             print('Getting from cache')
-            for name, inf in info.items():
-                if name == args:
-                    return inf
         else:
             print('Calculating new result')
             result = func(*args)
             info[args] = result
-            return result
+        return info[args]
     return wrapper
