a4bc26330b1bbd1a4b558d31b91b0471583e51a8
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-09-17 18:01:41 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
-    pass
+    cache_result = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args not in cache_result:
+            cache_result[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_result.get(args)
+
+    return wrapper
