834ed1d725c5bbbf068faa93199d0bf3184ff567
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-18 22:58:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
 def cache(func):
-    # Write your code here
-    pass
+    dict_args_result = {}
+    dict_name_func = {}
+
+    def inner(*args, **kwargs):
+
+        for key_name_func, value_name_func in dict_name_func.items():
+            if key_name_func == str(func.__name__):
+                for key_args_result, value_args_result in \
+                        value_name_func.items():
+                    if key_args_result == args:
+
+                        print('Getting from cache')
+                        return dict_args_result[args]
+
+        dict_args_result[args] = func(*args, **kwargs)
+        dict_name_func.update({func.__name__: dict_args_result})
+
+        print('Calculating new result')
+        return dict_args_result[args]
+
+    return inner
