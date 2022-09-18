bb4c433b291f20eb604da40c492b2b78879fbc95
illia riaboi <riaboy94@gmail.com>
2022-09-19 02:27:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
 def cache(func):
-    # Write your code here
-    pass
+    new_dict = {}
+
+    def inner(*args, **kwargs):
+        if args not in new_dict:
+            new_dict[args] = func(*args)
+            print("Calculating new result")
+
+        else:
+            print("Getting from cache")
+        return new_dict.get(args)
+    return inner
+
+
+@cache
+def long_time_func(a, b, c):
+    return (a ** b ** c) % (a * c)
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func(1, 2, 3)
