02c37e5f7ee000084ac92200beec0b73c322592a
Yevhenii Davydenko <akazhulich@gmail.com>
2022-09-19 13:10:33 +0300

    from 2 to 1 return

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def cache(func):
     def wrapper(*args, **kwargs):
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict.get(args)
         else:
             result = func(*args)
             cache_dict.update({args: result})
             print("Calculating new result")
-            return result
+
+        return cache_dict.get(args)
 
     return wrapper
