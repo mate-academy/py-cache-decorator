42e3c847766d9331b1661efc4424b9b07a391806
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-19 23:32:20 +0300

    Forget everything you have seen before

--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
 def cache(func):
-    dct_args_res = {}
-    dct_name_fun = {}
-
+    dict_res = {}
     def inner(*args, **kwargs):
-
-        for key_name_fun, val_name_fun in dct_name_fun.items():
-            if key_name_fun == str(func.__name__):
-                for key_args_res, value_args_res in val_name_fun.items():
-                    if key_args_res == args:
-
-                        print('Getting from cache')
-                        return dct_args_res[args]
-
-        dct_args_res[args] = func(*args, **kwargs)
-        dct_name_fun.update({func.__name__: dct_args_res})
-
-        print('Calculating new result')
-        return dct_args_res[args]
-
+        if args in dict_res:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            dict_res[args] = func(*args)
+        return dict_res[args]
     return inner
+
