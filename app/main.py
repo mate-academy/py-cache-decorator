ec243b65dc092dc28b874027611e65f0a55f0c50
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-20 15:05:54 +0300

    without unnecessary function

--- app/main.py
+++ app/main.py
@@ -9,13 +9,3 @@ def cache(func):
             result_dict[args] = func(*args)
         return result_dict[args]
     return inner
-
-
-@cache
-def long_time_func(a, b, c):
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
