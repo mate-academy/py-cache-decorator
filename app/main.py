f69edb698aafa270c40713f7b4c94b8183067f11
Artem <Artemvyhovanec@gmail.com>
2022-09-20 13:52:54 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -4,9 +4,8 @@ def cache(func):
     def inner(*args):
         if args in data:
             print("Getting from cache")
-            return data[args]
         else:
             print("Calculating new result")
             data[args] = func(*args)
-            return data[args]
+        return data[args]
     return inner
