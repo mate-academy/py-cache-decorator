6f5eead7c523e6e1580cd00fad1b758074c8ae05
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-21 13:22:39 +0300

    change single quotes to double quotes

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ def cache(func):
 
     def inner(*args):
         if args in result_dict:
-            print('Getting from cache')
+            print("Getting from cache")
         else:
-            print('Calculating new result')
+            print("Calculating new result")
             result_dict[args] = func(*args)
         return result_dict[args]
     return inner
