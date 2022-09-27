f5ffbb2a11533015bf67e1584d50568a1509dbad
Yevhen Satskyi <yevgesha143@gmail.com>
2022-09-27 16:17:52 +0300

    Solotion

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+
 def cache(func):
-    # Write your code here
-    pass
+
+    d = {}
+
+    def wrapper(*args):
+        if args not in d:
+            d[args] = func(*args)
+            print("Calculating new result")
+            return d[args]
+        elif args in d:
+            print("Getting from cache")
+            return d[args]
+
+    return wrapper
+
+
+@cache
+def subtraction(a, b):
+    return a - b
