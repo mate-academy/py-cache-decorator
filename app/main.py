03520fbb7fdadd5236a8fd0d8413b068fcea933d
Paul Maslov <maslovp67@gmail.com>
2022-09-28 14:24:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
 def cache(func):
-    # Write your code here
-    pass
+
+    c = func.__name__
+    global cash_dict
+    cash_dict = {}
+
+    def inner(*args, **kwargs):
+
+        nonlocal c
+        result = str([item for item in args])
+        result += c
+        if result in cash_dict.keys():
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cash_dict[result] = func(*args, **kwargs)
+        return cash_dict[result]
+
+    return inner
