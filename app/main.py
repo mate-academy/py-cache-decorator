5528540227080e3ed297b0ba7a1293a21bc9f619
George <georgepavlej@gmail.com>
2022-09-28 19:00:24 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+
+    caches = {}
+
+    def wrapper(*args):
+        if args not in caches.keys():
+            caches[args] = func(*args)
+            print("Calculating new result")
+            return caches[args]
+        else:
+            print("Getting from cache")
+            return caches[args]
+
+    return wrapper
