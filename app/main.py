30b5d071c82e4fc5fa2606a699896d6b743368bb
Paul Maslov <maslovp67@gmail.com>
2022-09-29 14:20:05 +0300

    Solution, ver 1.02

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 def cache(func):
 
     c = func.__name__
-    global cash_dict
     cash_dict = {}
 
     def inner(*args, **kwargs):
 
         nonlocal c
-        result = str([item for item in args])
-        result += c
+        result = args + tuple(c)
         if result in cash_dict.keys():
             print("Getting from cache")
         else:
