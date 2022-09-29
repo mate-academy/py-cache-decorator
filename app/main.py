e5410265fb481caf6cde850cf464b8e167060622
Marina Bidenko <bidenko.bmw@gmail.com>
2022-09-29 17:46:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    dt = {}
+
+    def wrapper(*args):
+        if not dt.get(args, False) is False:
+            print("Getting from cache")
+            return dt[args]
+        else:
+            result = func(*args)
+            dt[args] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
