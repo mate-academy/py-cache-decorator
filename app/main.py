d3f7b969b73fa9d07bd9c994f8dabc9fea4c528b
Andrew.H <john.riderr9@gmail.com>
2022-09-29 11:27:55 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cashed = dict()
+
+    def wrapper(*args):
+        if args not in cashed:
+            print("Calculating new result")
+            cashed[args] = func(*args)
+            return cashed[args]
+        else:
+            print("Getting from cache")
+            return cashed[args]
+    return wrapper
