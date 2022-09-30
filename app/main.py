15899c9c1002e45ad6eedc806dba0d817cd612c4
Paul Maslov <maslovp67@gmail.com>
2022-09-30 16:13:04 +0300

    app/main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 def cache(func):
 
-    c = func.__name__
     cash_dict = {}
 
     def inner(*args, **kwargs):
 
-        nonlocal c
-        result = args + tuple(c)
-        if result in cash_dict.keys():
+        if args in cash_dict.keys():
             print("Getting from cache")
         else:
             print("Calculating new result")
-            cash_dict[result] = func(*args, **kwargs)
-        return cash_dict[result]
+            cash_dict[args] = func(*args, **kwargs)
+        return cash_dict[args]
 
     return inner
