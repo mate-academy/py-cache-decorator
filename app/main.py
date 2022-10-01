0c7e4fbdcbefdd37d5fa2c1d6597e496a1d06022
Igor Pylat <igorpylat@gmail.com>
2022-10-01 03:26:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 def cache(func):
-    # Write your code here
+    cached_list = []
+
+    def inner(*args):
+        access = True
+        element = list(args)
+
+        if cached_list:
+            for value in cached_list:
+                if value == element:
+
+                    print("Getting from cache")
+                    access = False
+
+        if access:
+
+            print("Calculating new result")
+            cached_list.append(element)
+
+        else:
+            for value in element:
+
+                return value
+
+        return func(*args)
+    return inner
     pass
