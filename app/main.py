ab95b393cdccf402c62518c80e63ed4acc537857
Taras Martyniuk <bythewaters800@gmail.com>
2022-10-02 21:44:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
 def cache(func):
-    # Write your code here
-    pass
+    result_cache = {}
+
+    def wrapper(*args):
+
+        nonlocal result_cache
+
+        if args not in result_cache:
+            result_cache[args] = func(*args)
+            print("Calculating new result")
+
+        elif args in result_cache:
+            print("Getting from cache")
+
+        return result_cache[args]
+
+    return wrapper
