bcd77543e00d7b2bc7b89ff8ada6644e5a5ece1c
Vladimir Titarenko <tytarenkova@gmail.com>
2022-10-02 16:44:04 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,28 @@
+import functools
+import json
+
+
 def cache(func):
-    # Write your code here
-    pass
+    saved_results = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if func.__name__ in saved_results:
+            if str(args) in saved_results[func.__name__]:
+                print("Getting from cache")
+                return saved_results[func.__name__][str(args)]
+            else:
+                print("Calculating new result")
+                saved_results[func.__name__].\
+                    update({str(args): func(*args, **kwargs)})
+                with open("data.json", "w") as f:
+                    json.dump(saved_results, f)
+                return saved_results[func.__name__][str(args)]
+        else:
+            print("Calculating new result")
+            saved_results[func.__name__] = {str(args): func(*args, **kwargs)}
+            with open("data.json", "w") as f:
+                json.dump(saved_results, f)
+            return saved_results[func.__name__][str(args)]
+
+    return wrapper
