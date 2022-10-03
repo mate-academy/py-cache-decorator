308ccf697c2ff3593a81536d57098db2d0e00680
Ivan Mayor <maramorosh2@gmail.com>
2022-10-03 21:21:20 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    saved_funcs = {}
+
+    def inner(*args):
+        if not saved_funcs.get(args, False) is not False:
+            res = func(*args)
+            saved_funcs[args] = res
+            print("Calculating new result")
+            return res
+        else:
+            print("Getting from cache")
+            return saved_funcs[args]
+
+    return inner
