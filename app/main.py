731d50ee44389700e59d2e98f40ec53e2d53753a
Nataliia <amirammina@gmail.com>
2022-10-04 21:39:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
-    # Write your code here
-    pass
+    saved_answers = []
+
+    def inner(*args):
+        if len(saved_answers) > 0:
+            for item in saved_answers:
+                if args in item:
+                    print("Getting from cache")
+                    return item[1]
+        print("Calculating new result")
+        answer = func(*args)
+        saved_answers.append((args, answer))
+        return answer
+
+    return inner
