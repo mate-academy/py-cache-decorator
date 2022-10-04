2e98c31334431461904dce62cedbd0d80dc2dd0a
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-04 21:18:46 +0300

    Solution of py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+import functools
+
+
 def cache(func):
-    # Write your code here
-    pass
+    @functools.wraps(func)
+
+    cache_keys = []
+    cache_result = []
+
+    def wrapper(*args):
+        cache_key = (id(func), *args,)
+        if cache_key not in cache_keys:
+            res = func(*args)
+            cache_keys.append(cache_key)
+            cache_result.append(res)
+            print("Calculating new result")
+        else:
+            index = cache_keys.index(cache_key)
+            res = cache_result[index]
+            print("Getting from cache")
+        return res
+
+    return wrapper
+
