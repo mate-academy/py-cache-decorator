6c0e316e6ffe2de641201198ffd9e07ffac9cda9
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-05 09:59:17 +0300

    Solution of py-cache-decorator (dict)

--- app/main.py
+++ app/main.py
@@ -3,20 +3,17 @@ from functools import wraps
 
 def cache(func):
     """New decoration function with cache"""
-    cache_keys = []
-    cache_result = []
+    cache_data = {}
 
     @wraps(func)
     def wrapper(*args):
         cache_key = (id(func), *args,)
-        if cache_key not in cache_keys:
+        if cache_key not in cache_data:
             res = func(*args)
-            cache_keys.append(cache_key)
-            cache_result.append(res)
+            cache_data[cache_key] = res
             print("Calculating new result")
         else:
-            index = cache_keys.index(cache_key)
-            res = cache_result[index]
+            res = cache_data[cache_key]
             print("Getting from cache")
         return res
 
