aca59e3ab211e595125b70dd5197c517bfd2e923
LavrykN <nlavryk@kaaiot.io>
2022-10-05 11:27:53 +0300

    Add Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def wrapper(*args):
+        if args in cache_data:
+            print('Getting from cache')
+        else:
+            cache_data[args] = func(*args)
+            print('Calculating new result')
+        return cache_data[args]
+
+    return wrapper
