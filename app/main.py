4ecaf843e4260e018ef4651ad9ae01b5f2f5da19
Igor <igornev83@gmail.com>
2022-10-05 17:01:42 +0300

    Tested solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    stored_results = {}
+
+    def wrapper(*args):
+        if args in stored_results:
+            print("Getting from cache")
+            return stored_results[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            stored_results[args] = result
+            return result
+    return wrapper
