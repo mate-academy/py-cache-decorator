92a109de54aaef4f7f795aec6de178b61e5322c5
Igor <igornev83@gmail.com>
2022-10-06 11:20:23 +0300

    Annotation added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> None:
+    stored_results = {}
+
+    def wrapper(*args: float) -> float:
+        if args in stored_results:
+            print("Getting from cache")
+            return stored_results[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            stored_results[args] = result
+            return result
+
+    return wrapper
