fba7bcae9eb574b39dbd4214db8fbf85d4233a3e
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-10-08 20:06:57 +0300

    1 fail with the time function, flake8 OK

--- app/main.py
+++ app/main.py
@@ -1,33 +1,15 @@
-def cache(func) -> None:
+def cache(func: any) -> any:
     result_runs = {}
 
-    def wrapper(*args):
+    def wrapper(*args: int) -> any:
         run_func = func(*args)
         result_func = {args: run_func}
         if (args, run_func) not in result_runs.items():
             result_runs.update(result_func)
-            return print(f"Calculating new result {result_func[args]}")
+            print("Calculating new result")
         else:
             for key in result_runs.keys():
                 if key == args:
-                    return print(f"Getting from cache {result_runs[key]}")
-
+                    print(f"Getting from cache")
+        return run_func
     return wrapper
-
-
-@cache
-def long_time_func(a, b, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
