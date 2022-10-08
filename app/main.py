b2678f5c92c97d8257ae38bf90012ca687353bbf
Artem Motrunich <motrunich2013@gmail.com>
2022-10-08 21:39:59 +0200

    Solution_v2

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-from functools import wraps
-
-
 def cache(func):
     cache_result = {}
 
-    @wraps(func)
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         if args in cache_result:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            cache_result[args] = func(*args)
+            cache_result[args] = func(*args, **kwargs)
         return cache_result[args]
     return wrapper
