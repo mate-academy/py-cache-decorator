495e7c23c0bce582a176814c3d3eb5750bd0d5b8
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-10-09 14:11:20 +0300

    Oll test Ok!

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ def cache(func: any) -> any:
     result_runs = {}
 
     def wrapper(*args: int) -> any:
-        run_func = func(*args)
-        result_func = {args: run_func}
-        if (args, run_func) not in result_runs.items():
+
+        if args not in result_runs.keys():
+            run_func = func(*args)
+            result_func = {args: run_func}
             result_runs.update(result_func)
             print("Calculating new result")
+            return run_func
         else:
-            for key in result_runs.keys():
-                if key == args:
-                    print("Getting from cache")
-        return run_func
+            print("Getting from cache")
+            return result_runs[args]
+
     return wrapper
