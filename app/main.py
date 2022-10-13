9674c5030c51b791a23ad19acd89d4b42b112bdc
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2022-10-13 12:15:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: object) -> object:
+    cache = {}
+
+    def wrapper(*args) -> str:
+        signature = (func, *args)
+        if signature in cache:
+            result = cache[signature]
+            print("Getting from cache")
+        else:
+            result = func(*args)
+            cache[signature] = result
+            print("Calculating new result")
+        return result
+    return wrapper
