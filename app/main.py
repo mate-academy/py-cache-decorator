4b996bca6b72a4e5a17617942466744c9d50cdd0
Vadim Tsytsanuk <jhtk333@gmail.com>
2022-10-13 14:34:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,13 +4,10 @@ from typing import Callable
 def cache(func) -> Callable:
     res_dict = {}
 
-    def inner(*args, **kwargs) -> Callable:
+    def inner(*args, **kwargs):
         if f"{args}" in res_dict:
             print("Getting from cache")
-            return res_dict[f"{args}"]
         else:
-            func(*args)
             res_dict[f"{args}"] = func(*args)
             print("Calculating new result")
-        return func
     return inner
