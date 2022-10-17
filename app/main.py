9400d337ab12a1a3c7e26807af37067bc4c02e27
Admin <winofkill@gmail.com>
2022-10-17 21:11:20 +0400

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_backet = {}
+
+    def wrapper(*args):
+        if args not in cache_backet:
+            result = func(*args)
+            cache_backet[args] = result
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_backet[args]
+    return wrapper
