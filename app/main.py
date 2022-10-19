8137d2c144b4fa77ee7b184f27ca978910186981
Vladimir Titarenko <tytarenkova@gmail.com>
2022-10-19 12:18:21 +0300

    third commit

--- app/main.py
+++ app/main.py
@@ -1,23 +1,13 @@
-import functools
-
 
 def cache(func):
     saved_results = {}
 
-    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if func.__name__ in saved_results:
-            if str(args) in saved_results[func.__name__]:
-                print("Getting from cache")
-                return saved_results[func.__name__][str(args)]
-            else:
-                print("Calculating new result")
-                saved_results[func.__name__].\
-                    update({str(args): func(*args, **kwargs)})
-                return saved_results[func.__name__][str(args)]
+        if args in saved_results:
+            print("Getting from cache")
         else:
             print("Calculating new result")
-            saved_results[func.__name__] = {str(args): func(*args, **kwargs)}
-            return saved_results[func.__name__][str(args)]
+            saved_results[args] = func(*args, **kwargs)
 
+        return saved_results[args]
     return wrapper
