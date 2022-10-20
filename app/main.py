fd86c94f773d3aaa0445325149ea64ffdc7fa62a
Dmytro Prokofiev <forbuffa@gmail.com>
2022-10-20 16:47:12 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    cache_dct = {}
+
+    def wrapped(*args) -> callable:
+        if args in cache_dct:
+            print("Getting from cache")
+            return cache_dct[args]
+        else:
+            print("Calculating new result")
+            total = func(*args)
+            cache_dct[args] = total
+            return total
+    return wrapped
