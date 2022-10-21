98cee8bbcad16aca277fb30925c258e9d651a838
Evgeniy Nozdrenko <evgenijnozdrenko@gmail.com>
2022-10-21 23:37:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    dict_res = {}
+
+    def inner(*args) -> callable:
+        if args not in dict_res:
+            res = func(*args)
+            dict_res[args] = res
+            print("Calculating new result")
+            return dict_res[args]
+        else:
+            print("Getting from cache")
+            return dict_res[args]
+    return inner
