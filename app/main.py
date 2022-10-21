0dfa66e25e8a0f96d8603052571e690b5ae6489a
Oksana Feshchenko <foksanv@gmail.com>
2022-10-21 16:33:31 +0200

    make cashe decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    store = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        if args not in store:
+            print("Calculating new result")
+            immut = (float, int, tuple, str)
+            if all([isinstance(arg, immut) for arg in args]):
+                store[args] = func(*args, *kwargs)
+                return store[args]
+        else:
+            print("Getting from cache")
+            return store[args]
+    return wrapper
