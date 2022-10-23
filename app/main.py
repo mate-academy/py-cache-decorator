bda7d7507ca9584661cf2cbb14d7e82a431f0698
6aPa6aH <6apa6ah666@gmail.com>
2022-10-23 13:22:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cached_dict = {}
+    def wrapper(*args):
+        if args in cached_dict.keys():
+            print("Getting from cache")
+            return cached_dict[args]
+        elif args not in cached_dict.keys():
+            cached_dict[args] = func(*args)
+            print("Calculating new result")
+            return cached_dict[args]
+        return func
+    return wrapper
+
