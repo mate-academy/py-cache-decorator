df582af5aceed17476585018d9b20b303948c66e
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-10-27 16:36:48 +0200

    Solution with one return statement

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> Callable:
 
         if args in memory:
-
             print("Getting from cache")
-            return memory[args]
 
         else:
             memory[args] = func(*args)
             print("Calculating new result")
-            return memory[args]
+
+        return memory[args]
 
     return wrapper
