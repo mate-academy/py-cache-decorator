8374cfdc83dc9895abc535f3ccc06b9ec6659d3b
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-10-27 15:15:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ def cache(func: Callable) -> Callable:
     memory = {}
 
     def wrapper(*args) -> Callable:
-        if f"Result with arguments{args}" in memory:
+
+        if args in memory:
 
             print("Getting from cache")
-            return memory[f"Result with arguments{args}"]
+            return memory[args]
 
         else:
-            result_of_function = func(*args)
-            memory[f"Result with arguments{args}"] = result_of_function
+            memory[args] = func(*args)
             print("Calculating new result")
-            return result_of_function
+            return memory[args]
 
     return wrapper
