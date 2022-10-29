c4b5f4da5751eeefb755bcf47e8023aed8a0acaa
Haishuk Pavlo <xomas@ukr.net>
2022-10-29 18:13:40 +0300

    done, all works

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    cache_lib = {}
+
+    def inner(*args) -> callable:
+        nonlocal cache_lib
+        if args not in cache_lib:
+            cache_lib[args] = func(*args)
+            print("Calculating new result")
+            return cache_lib[args]
+        if args in cache_lib:
+            print("Getting from cache")
+            return cache_lib[args]
+    return inner
