bf25c4b7636c791a54a1c4a4ad1e883ad3a35e7c
Heorhii <jorjio22@gmail.com>
2022-11-04 18:05:02 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,13 +5,13 @@ def cache(func: Callable) -> Callable:
     func_arg_res = {}
 
     def inner(*args, **kwargs) -> Callable:
-        if f"{args},{kwargs}" not in func_arg_res:
+        if args not in func_arg_res:
             func_result = func(*args, **kwargs)
-            func_arg_res[f"{args},{kwargs}"] = func_result
+            func_arg_res[args] = func_result
             print("Calculating new result")
             return func_result
         else:
             print("Getting from cache")
-            return func_arg_res[f"{args},{kwargs}"]
+            return func_arg_res[args]
 
     return inner
