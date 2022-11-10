d32e940aec8ce34ee31b79d43e813eb5280c9d9b
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2022-11-10 17:27:41 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -2,20 +2,15 @@ import functools
 from typing import Callable, Any
 
 
-def cache(func: Callable) -> str:
+def cache(func: Callable) -> Callable:
     store = {}
 
     @functools.wraps(func)
-    def inner(*args: Any) -> Any:
-        if f"{func.__name__}" not in store.keys():
-            store.update({f"{func.__name__}": {args: func(*args)}})
+    def inner(*args: Any) -> str:
+        if args not in store:
+            store.update({args: func(*args)})
             print("Calculating new result")
         else:
-            if args not in store[f"{func.__name__}"]:
-                store[f"{func.__name__}"].update({args: func(*args)})
-                print("Calculating new result")
-            else:
-                print("Getting from cache")
-        return store[f"{func.__name__}"].get(args)
-
+            print("Getting from cache")
+        return store[args]
     return inner
