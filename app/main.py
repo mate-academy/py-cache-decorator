81c512ce2020eaaa3fdbe1b036e1ceb84ff873af
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-11-16 18:38:14 +0800

    Solution2 use only one return

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ def cache(func: Callable) -> Callable:
         if args not in res:
             print("Calculating new result")
             res[args] = func(*args)
-            return res[args]
+
         else:
             print("Getting from cache")
-            return res[args]
+
+        return res[args]
 
     return inner
