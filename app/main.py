70b5c135408022a26f4126bde5e8711862c8d55f
Vitalii Panochko <vpapplic@gmail.com>
2022-11-17 02:20:15 +0200

    Cache

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    buffer = {}
+
+    def inner(*args: Any) -> Any:
+        if args in buffer:
+            print("Getting from cache")
+            return buffer[args]
+
+        if args not in buffer:
+            print("Calculating new result")
+            # buffer.append(args)
+            buffer[args] = func(*args)
+            return buffer[args]
+
+    return inner
