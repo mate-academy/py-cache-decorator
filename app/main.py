be18d9dbcf3f668b9cf4fb3b9bc6dcf4d6bc5f73
Vitalii Panochko <vpapplic@gmail.com>
2022-11-19 00:00:38 +0200

    redone 'return'

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ def cache(func: Callable) -> Callable:
     def inner(*args: Any) -> Any:
         if args in buffer:
             print("Getting from cache")
-            return buffer[args]
 
         if args not in buffer:
             print("Calculating new result")
             buffer[args] = func(*args)
-            return buffer[args]
+
+        return buffer[args]
 
     return inner
