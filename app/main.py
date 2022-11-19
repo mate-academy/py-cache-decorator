b62cda14a7f7470b7587fc05c96a5c1287c38c3b
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-11-19 17:00:25 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -16,19 +16,19 @@ def cache(func: Callable) -> Callable:
     return inner
 
 
-@cache
-def long_time_func(a, b, c):
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple, power):
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((3, 4, 5), 2)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
+# @cache
+# def long_time_func(a, b, c):
+#     return (a ** b ** c) % (a * c)
+#
+#
+# @cache
+# def long_time_func_2(n_tuple, power):
+#     return [number ** power for number in n_tuple]
+#
+#
+# long_time_func(1, 2, 3)
+# long_time_func(2, 2, 3)
+# long_time_func_2((3, 4, 5), 2)
+# long_time_func(1, 2, 3)
+# long_time_func_2((5, 6, 7), 10)
+# long_time_func_2((5, 6, 7), 10)
