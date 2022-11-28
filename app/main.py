28c6dc543331859898a166a3174eccd8f5889189
Andrii Kozlovskyi <0503424511@ukr.net>
2022-11-28 15:01:17 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,31 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+rezult_all = []
+
+
+def cache(func: Callable) -> Callable:
+
+    def inner(*args, **kqargs) -> Any:
+        rezult = {"name": func, "arg": args}
+        if len(rezult_all) < 1:
+            rez_func = func(*args, **kqargs)
+            print("Calculating new result")
+            rezult["rezult"] = rez_func
+            rezult_all.append(rezult)
+            return rez_func
+            return inner
+
+        for rez in rezult_all:
+            if rez["name"] == rezult["name"] and rez["arg"] == rezult["arg"]:
+                print("Getting from cache")
+                return rez["rezult"]
+                return inner
+
+        print("Calculating new result")
+        rez_func = func(*args, **kqargs)
+        rezult["rezult"] = rez_func
+        rezult_all.append(rezult)
+        return rez_func
+
+    return inner
