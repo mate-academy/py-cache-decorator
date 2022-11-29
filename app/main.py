9b52d705a6a6e009e93711b0bc1e9c6b8a8443cd
Anatoliy Pilipey <pilipeytolya@gmail.com>
2022-11-29 16:39:58 +0200

    2ndSolushen

--- app/main.py
+++ app/main.py
@@ -5,23 +5,17 @@ def cache(func: Callable) -> Callable:
     memory_cache = {}
 
     def inner(*args, **kwargs) -> list and int:
-        nonlocal memory_cache
-        immute = (dict, set, list)
-        for argumets in args:
-            if type(argumets) in immute:
-                return
-
-        memory_cache_key = ""
+        cod_number = ""
         input_num = args[0] if type(args[0]) is tuple else args
         for each in input_num:
-            memory_cache_key += str(each) + "$"
-        memory_cache_key += str(args[1])
+            cod_number += str(each) + "$"
+        cod_number += str(args[1])
 
-        if memory_cache_key in memory_cache:
+        if cod_number in memory_cache:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            memory_cache[memory_cache_key] = func(*args, **kwargs)
-        return memory_cache[memory_cache_key]
+            memory_cache[cod_number] = func(*args, **kwargs)
+        return memory_cache[cod_number]
 
     return inner
