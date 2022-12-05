9d2acd40a6f7d08f09019c83d1fc816879338e03
vitalii_yelinskyi <yelinskyizp@gmail.com>
2022-12-05 06:37:29 -0600

    Checkstyle

--- app/main.py
+++ app/main.py
@@ -7,10 +7,9 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> Any:
         if args in cache_results:
             print("Getting from cache")
-            return cache_results[args]
+        else:
+            print("Calculating new result")
+            cache_results[args] = func(*args)
+        return cache_results[args]
 
-        print("Calculating new result")
-        new_result = func(*args)
-        cache_results[args] = new_result
-        return new_result
     return wrapper
