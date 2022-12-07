7a41ba194dec8c932176ab1f6bc3caf58fa8a326
Taras Savchyn <savik1992@gmail.com>
2022-12-07 20:31:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
+cache_archive = {}
+
+
 def cache(func):
-    # Write your code here
-    pass
+    def wrapper(*args, **kwargs):
+        if f"{func} {args} {kwargs}" not in cache_archive.keys():
+            print("Calculating new result")
+            res = func(*args, **kwargs)
+            cache_archive[f"{func} {args} {kwargs}"] = res
+            return res
+        else:
+            print("Getting from cache")
+            return cache_archive[f"{func} {args} {kwargs}"]
+
+    return wrapper
