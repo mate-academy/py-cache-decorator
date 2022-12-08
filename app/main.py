2f9fb9ce67271791cad170f038c9314abcf3377c
Taras Savchyn <savik1992@gmail.com>
2022-12-08 18:23:46 +0200

    Solution v1

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 from typing import Callable, Any
+
 cache_archive = {}
 
 
 def cache(func: Callable) -> Callable:
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        if f"{func} {args} {kwargs}" not in cache_archive.keys():
+    def wrapper(*args: tuple, **kwargs: dict) -> Any:
+        save_func_done = f"{func} {args} {kwargs}"
+        if save_func_done in cache_archive.keys():
+            print("Getting from cache")
+            res = cache_archive[save_func_done]
+        else:
             print("Calculating new result")
             res = func(*args, **kwargs)
-            cache_archive[f"{func} {args} {kwargs}"] = res
-            return res
-        else:
-            print("Getting from cache")
-            return cache_archive[f"{func} {args} {kwargs}"]
+            cache_archive[save_func_done] = res
+
+        return res
 
     return wrapper
