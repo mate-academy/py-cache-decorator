4dce31d40d3136919a8cda3f46dbb2cc53e6dca2
drapail <mv.dibrov@gmail.com>
2022-12-10 21:02:45 +0100

    refactored codestyle and namings

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-def cache(func):
-    results = {}
-    if func.__name__ not in results.keys():
-        results[func.__name__] = {}
+def cache(func: callable) -> callable:
+    previous_function_calls = {}
+    if func.__name__ not in previous_function_calls.keys():
+        previous_function_calls[func.__name__] = {}
 
-    def wrapper(*args):
-        if args not in results[func.__name__]:
+    def wrapper(*args) -> int:
+        if args not in previous_function_calls[func.__name__]:
             print("Calculating new result")
-            results[func.__name__][args] = func(*args)
-            return results[func.__name__][args]
+            previous_function_calls[func.__name__][args] = func(*args)
+            return previous_function_calls[func.__name__][args]
         else:
             print("Getting from cache")
-            return results[func.__name__][args]
+            return previous_function_calls[func.__name__][args]
     return wrapper
