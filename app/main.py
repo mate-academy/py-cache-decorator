cf0d403b0d42c2a2c6d1af20cc3a79b39fd56f0f
drapail <mv.dibrov@gmail.com>
2022-12-10 20:59:03 +0100

    tests passing

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+    if func.__name__ not in results.keys():
+        results[func.__name__] = {}
+
+    def wrapper(*args):
+        if args not in results[func.__name__]:
+            print("Calculating new result")
+            results[func.__name__][args] = func(*args)
+            return results[func.__name__][args]
+        else:
+            print("Getting from cache")
+            return results[func.__name__][args]
+    return wrapper
