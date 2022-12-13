b37ad92ead9bfb437d2fbb3ed6477fb2666cabd8
Taras Savchyn <savik1992@gmail.com>
2022-12-13 23:08:18 +0200

    recommendations are taken into account

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
 from typing import Callable, Any
 
-cache_archive = {}
-
 
 def cache(func: Callable) -> Callable:
-    def wrapper(*args: tuple, **kwargs: dict) -> Any:
-        save_func_done = f"{func} {args} {kwargs}"
-        if save_func_done in cache_archive.keys():
+    cache_archive = {}
+
+    def wrapper(*args: tuple) -> Any:
+
+        if args in cache_archive.keys():
             print("Getting from cache")
-            res = cache_archive[save_func_done]
+            res = cache_archive[args]
         else:
             print("Calculating new result")
-            res = func(*args, **kwargs)
-            cache_archive[save_func_done] = res
+            res = func(*args)
+            cache_archive[args] = res
 
         return res
 
