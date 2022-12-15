d1cec319b8e8d58fb6f79dbb504a1d28b06bef5b
Vitalii <fahivchik1@gmail.com>
2022-12-15 23:25:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    my_cache = dict()
+
+    def wrapped_func(*args, **kwargs) -> callable:
+        key = str(args) + str(kwargs)
+        if key in my_cache:
+            print("Getting from cache")
+            return my_cache[key]
+        value = func(*args, **kwargs)
+        my_cache[key] = value
+        print("Calculating new result")
+        return value
+
+    return wrapped_func
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
