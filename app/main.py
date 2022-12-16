16690c60d607195ca07b05c08cb86018c91a6be8
Tymur Semelinskyi <t.semelinsky@gmail.com>
2022-12-16 12:51:23 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    data = {}
+
+    def inner(*args) -> int:
+        nonlocal data
+        if args not in data:
+            print("Calculating new result")
+            result = func(*args)
+            data[args] = result
+            return result
+        else:
+            print("Getting from cache")
+            return data[args]
+    return inner
