f0a0d1f4fb42e7777d4f3c478a13ce4a2c061ccf
Vitalii <fahivchik1@gmail.com>
2022-12-17 13:11:36 +0200

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ def cache(func: callable) -> callable:
 
     def wrapped_func(*args, **kwargs) -> callable:
         key = str(args)
-        if key in my_cache:
+        if key not in my_cache:
+            print("Calculating new result")
+            my_cache[key] = func(*args, **kwargs)
+        else:
             print("Getting from cache")
-            return my_cache[key]
-        my_cache[key] = func(*args, **kwargs)
-        print("Calculating new result")
         return my_cache[key]
 
     return wrapped_func
