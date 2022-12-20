615e8cc414065ad750c8478c4456f3500bf87386
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2022-12-20 19:32:59 +0200

    Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    results_in_cache = {}
+
+    def inner(*args) -> Any:
+        nonlocal results_in_cache
+        if tuple(args) not in results_in_cache:
+            print("Calculating new result")
+            results_in_cache[tuple(args)] = func(*args)
+        else:
+            print("Getting from cache")
+        return results_in_cache[tuple(args)]
+
+    return inner
