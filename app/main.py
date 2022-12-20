d865da050725797d5ef0d534e23b9796c803b933
Kostyantyn Chernyk <chernykko@gmail.com>
2022-12-20 17:26:59 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any
+
+
+def cache(func: callable) -> callable:
+    results = {}
+
+    def wrapper(*args: Any) -> Any:
+        if args not in results:
+            value = func(*args)
+            results[args] = value
+            print("Calculating new result")
+            return value
+
+        elif args in results:
+            print("Getting from cache")
+            return results[args]
+    return wrapper
