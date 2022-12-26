2915108a4125a17841c18de5803df80ff17ee72b
Illia Smyrnov <ilushasm@gmail.com>
2022-12-26 21:23:16 +0200

    reworked Solution

--- app/main.py
+++ app/main.py
@@ -6,20 +6,13 @@ def cache(func: Callable) -> Callable:
     cache_storage = {}
 
     @functools.wraps(func)
-    def inner(*args) -> Any:
-        items = args
-        name_of_func = func.__name__
-        if name_of_func in cache_storage \
-                and items in cache_storage[name_of_func]:
+    def inner(*args: Any) -> Any:
+        if (func.__name__, args) in cache_storage:
             print("Getting from cache")
-            return cache_storage[name_of_func][items]
+            result = cache_storage[(func.__name__, args)]
         else:
             print("Calculating new result")
-            result = func(*items)
-            storage_update = {items: result}
-            if name_of_func in cache_storage:
-                cache_storage[name_of_func].update(storage_update)
-            else:
-                cache_storage.update({name_of_func: storage_update})
-            return result
+            result = func(*args)
+            cache_storage[(func.__name__, args)] = result
+        return result
     return inner
