c8fc43552175180351f0351471eb95e6ae2ed66e
Yuliia Stopkyna <styopkinayuliya@gmail.com>
2022-12-28 18:03:49 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,10 @@ def cache(func: Callable) -> Callable:
     results = {}
 
     def inner(*args) -> int:
-        nonlocal results
         if args in results:
             print("Getting from cache")
-            return results[args]
         else:
-            result = func(*args)
-            results[args] = result
             print("Calculating new result")
-            return result
+            results[args] = func(*args)
+        return results[args]
     return inner
