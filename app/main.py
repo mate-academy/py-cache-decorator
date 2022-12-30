95197bfdee58936ae30b224f03220f32ddf2ac9d
Roman <parom95@ukr.net>
2022-12-31 00:31:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Any:
+    cash = {}
+
+    @wraps(func)
+    def wrapper(*args: tuple) -> Any:
+        if args in cash:
+            print("Getting from cache")
+            return cash[args]
+        else:
+            print("Calculating new result")
+        cash[args] = func(*args)
+        return cash[args]
+    return wrapper
