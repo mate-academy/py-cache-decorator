1d5a07ec456a6c6f64c6e0370af6113b34fb7692
Igor Starodub <igorastar@gmail.com>
2022-12-31 11:00:14 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    dict1 = {}
+    cash_dict = {}
 
     def wrapper(*args: tuple) -> int:
-        if args not in dict1:
-            dict1[args] = func(*args)
+        if args not in cash_dict:
+            cash_dict[args] = func(*args)
             print("Calculating new result")
         else:
             print("Getting from cache")
 
-        return dict1[args]
+        return cash_dict[args]
     return wrapper
