8557a5369466ffca56a5000f086fcc143732a8f0
sergk <srx92@protonmail.com>
2023-01-02 10:54:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any
+
+
+def cache(func: callable) -> callable:
+    _cache = {}
+
+    def some_inputs(*inputs: Any) -> str:
+        if func.__name__ not in _cache:
+            _cache[func.__name__] = {}
+        if inputs not in _cache[func.__name__]:
+            _cache[func.__name__][inputs] = func(*inputs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+
+        return _cache[func.__name__][inputs]
+
+    return some_inputs
