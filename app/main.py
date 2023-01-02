ef0781349150c04716384be4bed30ed9a6b1d020
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-01-02 17:02:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: list) -> str:
+    add_func = []
+
+    def wrapps(*args, **kwargs) -> str:
+        perfom_func = func(*args, **kwargs)
+        if perfom_func not in add_func:
+            print("Calculating new result")
+            add_func.append(perfom_func)
+        else:
+            print("Getting from cache")
+        return perfom_func
+    return wrapps
