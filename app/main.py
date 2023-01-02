0da0bc3184d6471cea25343abfb3757fc5de470f
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-01-02 11:11:07 +0200

    remove extra list

--- app/main.py
+++ app/main.py
@@ -2,15 +2,13 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Any:
-    all_number = []
     result = {}
 
     def check_data(*args: int) -> Callable:
-        if args in all_number:
+        if args in result:
             print("Getting from cache")
         else:
             result[args] = func(*args)
-            all_number.append(args)
             print("Calculating new result")
         return result[args]
     return check_data
