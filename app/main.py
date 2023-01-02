c22daa0f1305bdab4d223230c300b79df126a934
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-01-02 15:28:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    dict_of_date = {}
+
+    def wrapper(*args) -> Any:
+        if args in dict_of_date.keys():
+            print("Getting from cache")
+            return dict_of_date.get(args)
+        else:
+            dict_of_date[args] = func(*args)
+            print("Calculating new result")
+            return dict_of_date[args]
+
+    return wrapper
