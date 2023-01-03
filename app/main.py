a756d5e17ee7efc8a3c4d78dd587223407496201
Anton Kamenetskyi <kamenetskyy@gmail.com>
2023-01-03 18:23:13 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Dict, Any
+
+
+def cache(func: Callable) -> Callable:
+    cache: Dict[Any, Any] = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        if args in cache:
+            print("Getting from cache")
+            return cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache[args] = result
+            return result
+
+    return wrapper
