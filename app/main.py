8d7c1fd77c33408855167806b0b5d2dea1b19a26
drapail <mv.dibrov@gmail.com>
2023-01-04 22:37:07 +0100

    mentors notes implemented

--- app/main.py
+++ app/main.py
@@ -3,14 +3,12 @@ from typing import Any
 
 def cache(func: callable) -> callable:
     previous_function_calls = {}
-    if func.__name__ not in previous_function_calls.keys():
-        previous_function_calls[func.__name__] = {}
 
     def wrapper(*args) -> Any:
-        if args not in previous_function_calls[func.__name__]:
+        if args not in previous_function_calls.keys():
             print("Calculating new result")
-            previous_function_calls[func.__name__][args] = func(*args)
+            previous_function_calls[args] = func(*args)
         else:
             print("Getting from cache")
-        return previous_function_calls[func.__name__][args]
+        return previous_function_calls[args]
     return wrapper
