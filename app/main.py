9378db3e1c81c19e76311b11d23d87b3f37d846f
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-05 10:34:26 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from collections.abc import Hashable
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    def inner(*args: Any) -> Any:
+        if not isinstance(args, Hashable):
+            return func(*args)
+        if args in inner.cache:
+            print("Getting from cache")
+            return inner.cache[args]
+        print("Calculating new result")
+        result = func(*args)
+        inner.cache[args] = result
+        return result
+    inner.cache = {}
+    return inner
