83e15b0b24265308b40934edc22cd358a45f878c
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-06 15:19:52 +0200

    simplify solution

--- app/main.py
+++ app/main.py
@@ -4,13 +4,11 @@ from typing import Callable, Any
 
 def cache(func: Callable) -> Callable:
     def inner(*args: Any) -> Any:
-        if not isinstance(args, Hashable):
-            return func(*args)
-        if args in cache_dict:
+        if args not in cache_dict:
+            print("Calculating new result")
+            cache_dict[args] = func(*args)
+        else:
             print("Getting from cache")
-            return cache_dict[args]
-        print("Calculating new result")
-        cache_dict[args] = func(*args)
         return cache_dict[args]
     cache_dict = {}
     return inner
