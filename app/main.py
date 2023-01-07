e3b57ff5754f43b9095bfae6e3d30093d25ba0be
Iryna Ishchenko <irynaira@gmail.com>
2023-01-07 17:38:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -15,21 +15,3 @@ def cache(func: Callable) -> Callable:
 
     wrapper.cache = {}
     return wrapper
-
-
-@cache
-def long_time_func(data_a: int, data_b: int, data_c: int) -> Any:
-    return (data_a ** data_b ** data_c) % (data_a * data_c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> Any:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
