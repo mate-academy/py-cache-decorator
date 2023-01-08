395e82e1748ae4d434194acf2a2131ca22c4bff4
Roman Muraviov <info@roman-muraviov.com>
2023-01-08 18:09:22 +0200

    update_4

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    non_repeating_elements = {}
+
+    def checking(*elements) -> Any:
+        if elements not in non_repeating_elements:
+            print("Calculating new result")
+            non_repeating_elements[elements] = func(*elements)
+        else:
+            print("Getting from cache")
+        return non_repeating_elements[elements]
+    return checking
