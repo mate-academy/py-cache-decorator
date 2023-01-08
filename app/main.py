9b8b9a4772bb599bceb6cdf8702efe613b525012
Iryna Ishchenko <irynaira@gmail.com>
2023-01-08 15:50:55 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
+    wrapper_cache = {}
+
     @wraps(func)
-    def wrapper(*args, **kwargs) -> Any:
-        dict_key = args + tuple(kwargs.items())
-        if dict_key not in wrapper.cache:
-            wrapper.cache[dict_key] = func(*args, **kwargs)
+    def wrapper(*args) -> Any:
+        if args not in wrapper_cache:
+            wrapper_cache[args] = func(*args)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return wrapper.cache[dict_key]
+        return wrapper_cache[args]
 
-    wrapper.cache = {}
     return wrapper
