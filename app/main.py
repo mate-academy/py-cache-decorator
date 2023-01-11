ae948d53f29fc469f1180bbab00da211c7e32b5c
Anastasiia <nastyashch1601@gmail.com>
2023-01-11 11:16:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+import functools
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    stored_calculations = {}
+
+    @functools.wraps(func)
+    def inner(*args) -> Any:
+        if args not in stored_calculations:
+            print("Calculating new result")
+            result = func(*args)
+            stored_calculations[args] = result
+        else:
+            print("Getting from cache")
+        return stored_calculations[args]
+    return inner
