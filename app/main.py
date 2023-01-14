d5024ad82f0f32e9b197ce6993340697172fec25
Zoriana Dvulit <zdvulit@ukr.net>
2023-01-14 16:40:40 +0200

    New_Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ from typing import Callable
 def cache(func: Callable, *args, **kwargs) -> Callable:
     cache_dict = {}
 
-    def wrapper(*spisok) -> Callable:
-        if spisok in cache_dict:
+    def wrapper(*new_list) -> Callable:
+        if new_list in cache_dict:
             print("Getting from cache")
-            return cache_dict.get(spisok)
-        else:
-            print("Calculating new result")
-            result = func(*spisok)
-            cache_dict[spisok] = result
-            return result
+            return cache_dict.get(new_list)
+
+        print("Calculating new result")
+        result = func(*new_list)
+        cache_dict[new_list] = result
+        return result
     return wrapper
 
 
@@ -26,9 +26,10 @@ def long_time_func_2(n_tuple: tuple, power: int) -> list:
     return [number ** power for number in n_tuple]
 
 
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
+if __name__ == "__main__":
+    long_time_func(1, 2, 3)
+    long_time_func(2, 2, 3)
+    long_time_func_2((5, 6, 7), 5)
+    long_time_func(1, 2, 3)
+    long_time_func_2((5, 6, 7), 10)
+    long_time_func_2((5, 6, 7), 10)
