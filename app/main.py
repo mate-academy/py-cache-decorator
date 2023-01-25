ac3d78687ae6fd326ecbc93037e97d927ea212c1
Daria Zayka <dariazayka@MacBook-Air-Daria.local>
2023-01-25 14:38:32 +0200

    Fix bugs

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     result = {}
 
-    def inner(*args) -> Callable:
+    def inner(*args, **kwargs) -> Callable:
 
-        if args in result:
+        if (args or kwargs) in result:
             print("Getting from cache")
 
         else:
             print("Calculating new result")
-            result[args] = func(*args)
-        return result[args]
+            result[(args or kwargs)] = func(*args, **kwargs)
+        return result[(args or kwargs)]
 
     return inner
