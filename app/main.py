28cba23eb16ffe5f5efcb89d48e3cf49595f3380
Natalie Goriela <natalie.goriela@gmail.com>
2023-01-26 12:01:11 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+
+    results = {}
+
+    def inner(*args) -> int | str:
+
+        if args in results:
+            print("Getting from cache")
+            return results[args]
+        else:
+            print("Calculating new result")
+            results[args] = func(*args)
+            return results[args]
+
+    return inner
