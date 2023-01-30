26cb87f25c248c44e5e66f57026f5a2366be0a5f
Serhii Terletckiy <serhii.terletskiy.ua@gmail.com>
2023-01-30 19:20:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 def cache(func):
-    # Write your code here
-    pass
+    values = []
+
+    def inner(*args, **kwargs):
+        nonlocal values
+
+        cached = list(filter(lambda x: x[0] == args, values))
+        if cached:
+            print("Getting from cache")
+            return cached[0][1]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            pre_comp = (args, result, func.__name__)
+            values.append(pre_comp)
+            return result
+
+    return inner
