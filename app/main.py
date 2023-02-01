67ee4adf2e4797a816ea46b940391717315d3c87
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-01 21:40:32 +0200

    2  Commit

--- app/main.py
+++ app/main.py
@@ -2,28 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    database = []
+    database = {}
 
-    def inner(*args, **kwargs) -> Callable:
-        in_cache = [
-            data["result"] for data in database if data["arguments"] == args
-        ]
-        if in_cache:
+    def inner(*args) -> Callable:
+        if args in database:
             print("Getting from cache")
-            return in_cache[0]
+            return database[args]
         print("Calculating new result")
         result = func(*args)
-        database.append({"result": result, "arguments": args})
+        database[args] = result
         return result
 
     return inner
-
-
-@cache
-def long_time_func(name_a: int, name_b: int, name_c: int) -> int:
-    return (name_a ** name_b ** name_c) % (name_a * name_c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
