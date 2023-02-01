6383d892b5834fc27ce87fce733f3baec442c40e
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-01 14:50:51 +0200

    change code according to mentor's comments

--- app/main.py
+++ app/main.py
@@ -5,13 +5,12 @@ def cache(func: Callable) -> Any:
     func_result = {}
 
     def wrapper(*args) -> Any:
-        if args in func_result.keys():
+        if args in func_result:
             print("Getting from cache")
-            return func_result[args]
         else:
-            res = func(*args)
-            func_result[args] = res
+            result = func(*args)
+            func_result[args] = result
             print("Calculating new result")
-            return res
+        return func_result[args]
 
     return wrapper
