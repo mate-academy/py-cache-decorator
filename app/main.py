7993927e0815041ec5e83791710242701ac61e2f
dmytro <manjustice1998@gmail.com>
2023-02-01 17:11:51 +0200

    Changed two returns into single

--- app/main.py
+++ app/main.py
@@ -7,11 +7,10 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args: Any) -> Any:
         if args in operations:
             print("Getting from cache")
-            return operations[args]
         else:
             print("Calculating new result")
-            result = func(*args)
-            operations[args] = result
-            return result
+            operations[args] = func(*args)
+
+        return operations[args]
 
     return wrapper
