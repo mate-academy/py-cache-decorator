3b7473823404412470ed9a4ce043ccf1499b5262
Zenon <zenion@ukr.net>
2023-02-02 23:54:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+from functools import wraps
+
+
+def cache(func: Callable) -> Callable:
+    dict__ = dict()
+
+    @wraps(func)
+    def inner(*args) -> Any:
+        nonlocal dict__
+
+        if not dict__.get((*args, inner.__name__,)) is None:
+            print("Getting from cache")
+            return dict__.get((*args, inner.__name__,))
+        print("Calculating new result")
+
+        dict__[(*args, inner.__name__,)] = func(*args)
+        return dict__[(*args, inner.__name__,)]
+
+    return inner
