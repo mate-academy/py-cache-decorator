986fccde4821a37cfc1dc9552c174dc5756ac18b
Vadym Rabotiuk <vadymrabotiuk@gmail.com>
2023-02-02 22:24:45 +0200

    Solution 2.0. Fixed double return

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args: tuple) -> int:
         if args in data_dict:
             print("Getting from cache")
-            return data_dict[args]
         else:
             print("Calculating new result")
             data_dict[args] = func(*args)
-            return data_dict[args]
+        return data_dict[args]
     return inner
