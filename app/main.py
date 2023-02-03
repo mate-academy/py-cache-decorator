eb19c54da6dc278a093d91d8743d2434b9ea97d7
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-03 11:56:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    cache_date = {}
+
+    def inner(*args, **kwargs) -> any:
+        if args in cache_date:
+            print("Getting from cache")
+            for date in cache_date:
+                if date == args:
+                    return cache_date[args]
+        else:
+            result = func(*args, **kwargs)
+            cache_date[args] = result
+            print("Calculating new result")
+            return result
+
+    return inner
