ff584ee1154ec07417ca69b58ac9a1408107fac3
name <email@gmail.com>
2023-02-04 00:09:09 +0200

    return statement in if was removed for adhering to DRY principle

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args, **kwargs) -> Callable:
         if args in results:
             print("Getting from cache")
-            return results[args]
-
-        print("Calculating new result")
-        results[args] = func(*args, **kwargs)
+        else:
+            print("Calculating new result")
+            results[args] = func(*args, **kwargs)
         return results[args]
     return inner
