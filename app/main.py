b721d834da3f9599012533a24c5b9d61b8956e3f
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-03 15:46:03 +0200

    Simplified hash key, made single return out of if/else block

--- app/main.py
+++ app/main.py
@@ -7,17 +7,14 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def results_save_wrapper(*args, **kwargs) -> None:
-        func_arg_kwarg_hash = hash(args) + hash(func)
-
-        for item in kwargs.items():
-            func_arg_kwarg_hash += hash(item)
+        func_arg_kwarg_hash = (*args, *kwargs.values(), hash(func))
 
         if cache_hash_result.get(func_arg_kwarg_hash) is not None:
             print("Getting from cache")
-            return cache_hash_result.get(func_arg_kwarg_hash)
+        else:
+            print("Calculating new result")
+            cache_hash_result[func_arg_kwarg_hash] = func(*args, **kwargs)
 
-        print("Calculating new result")
-        cache_hash_result[func_arg_kwarg_hash] = func(*args, **kwargs)
         return cache_hash_result.get(func_arg_kwarg_hash)
 
     return results_save_wrapper
