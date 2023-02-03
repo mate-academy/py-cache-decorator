0cd3ae443913999e937134010bd5d08a4f2ae152
Oleksandr Leha <legus96@gmail.com>
2023-02-03 14:46:17 +0200

    'Solution_with_changes'

--- app/main.py
+++ app/main.py
@@ -6,15 +6,14 @@ def cache(func: typing.Callable) -> typing.Callable:
 
     def inner(*args) -> typing.Any:
 
-        if ((args), (func.__name__)) in cache_func.keys():
+        if ((args), (func.__name__)) in cache_func:
             print("Getting from cache")
-            return cache_func[(args), (func.__name__)]
 
         else:
             print("Calculating new result")
             complited_func = func(*args)
             key_for_func = ((args), (func.__name__))
             cache_func[key_for_func] = complited_func
-            return complited_func
+        return cache_func[(args), (func.__name__)]
 
     return inner
