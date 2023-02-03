5214e5c272dbdba755a059bb392052f81c4f9031
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-03 19:43:12 +0200

    hotfix Union[Any | None] -> Union[Any, None]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     cache_hash_result = {}
 
     @wraps(func)
-    def results_save_wrapper(*args, **kwargs) -> Union[Any | None]:
+    def results_save_wrapper(*args, **kwargs) -> Union[Any, None]:
         func_arg_kwarg_hash = (*args, *kwargs.values(), hash(func))
 
         if cache_hash_result.get(func_arg_kwarg_hash) is not None:
