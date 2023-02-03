1af8b6c9723da41af5f97013d67df8654c4d78b0
Danil Datsenko <danildacenko@gmail.com>
2023-02-03 14:55:34 +0200

    Removed key() method

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     results = {}
 
-    def inner(*args, **kwargs) -> Any:
-        if args not in results.keys():
+    def inner(*args) -> Any:
+        if args not in results:
             print("Calculating new result")
-            results[args] = func(*args, **kwargs)
+            results[args] = func(*args)
         else:
             print("Getting from cache")
         return results[args]
