2d1cfce641adc5092ef6330858182ee32df267b6
Yura Hudzovskiy <50211819+yura-hudzovskyi@users.noreply.github.com>
2023-02-03 15:20:28 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,9 +7,7 @@ def cache(func: Callable) -> Callable:
     def inner(*args, **kwargs) -> any:
         if args in cache_date:
             print("Getting from cache")
-            for date in cache_date:
-                if date == args:
-                    return cache_date[args]
+            return cache_date[args]
         else:
             result = func(*args, **kwargs)
             cache_date[args] = result
