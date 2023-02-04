e5eeef4420c14e07d8c8c038f680dc6f6d050280
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-02-05 00:36:28 +0200

    Solution: cache dacorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Any:
+    cache_dict = {}
+
+    def wrapper(*args) -> Any:
+        if hash(args) in cache_dict:
+            print("Getting from cache")
+            return cache_dict[hash(args)]
+        else:
+            func_res = func(*args)
+            cache_dict[hash(args)] = func_res
+            print("Calculating new result")
+            return func_res
+
+    return wrapper
