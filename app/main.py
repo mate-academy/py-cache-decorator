bbe7042ecfd3899a8e4355dea7acdc04af8fcedd
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-04 18:55:46 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,26 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    store_results = {}
+
+    def inner(*args, **kwargs) -> any:
+        key = (args, tuple(kwargs.items()))
+        if key not in store_results:
+            store_results[key] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return store_results[key]
+
+    return inner
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
