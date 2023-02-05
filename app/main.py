e48210edd3a2fd7486bef6c7d8e7f7d43e91c5aa
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-05 12:47:50 -0600

    Change anotation type

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import functools
 from typing import Callable
+from typing import Union
 
 
 def cache(func: Callable) -> int:
     cache_store = {}
 
     @functools.wraps(func)
-    def inner(*args: int) -> int:
+    def inner(*args: Union[int, float, str, tuple]) -> Union[int, float, str, tuple]:
         nonlocal cache_store
         if args in cache_store:
             print("Getting from cache")
