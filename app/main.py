31f3530186933555cb066ac7dd9f7bb7055c5a5f
Halyna Petrova <1208petrova@gmail.com>
2023-02-06 22:39:24 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+def cache(func: Callable) -> None:
+
+    new_dict = {}
+
+    def inner(*args) -> str:
+        if args not in new_dict.keys():
+            new_dict[args] = func(*args)
+            print("Calculating new result")
+            return new_dict[args]
+        else:
+            print("Getting from cache")
+            return new_dict[args]
+
+    return inner
+
