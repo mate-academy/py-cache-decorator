f1bf1ec1c53447c7677ced59f78be913e5a61e07
Anna Boichenko <ann.boychenko199@gmail.com>
2023-02-06 16:28:23 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def cache(func: Callable) -> Callable:
             cache_dict[args] = func(*args)
             print("Calculating new result")
             return cache_dict[args]
-        else:
-            print("Getting from cache")
-            return cache_dict[args]
+
+        print("Getting from cache")
+        return cache_dict[args]
 
     return inner
