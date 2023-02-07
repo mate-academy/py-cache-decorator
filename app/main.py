74883c3b8408afea7023c9ec770fc7564b2d10cb
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-02-07 18:33:01 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    dict_cache = {}
+
+    def inner(*args, **kwargs) -> tuple:
+        inputs = (*args, *kwargs.values())
+        if inputs not in dict_cache:
+            result = func(*args)
+            dict_cache[inputs] = result
+            print("Calculating new result")
+            return result
+        print("Getting from cache")
+        return dict_cache[inputs]
+    return inner
