0092186ebc56f230d0d393224d77e6ace8c3d803
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-02-07 11:28:32 -0600

    Solved type anotation for decorator

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     @functools.wraps(func)
-    def inner(*args: Union[int, float, str, tuple]) -> Callable:
+    def inner(*args: any) -> Callable:
         if args in cache_store:
             print("Getting from cache")
         else:
