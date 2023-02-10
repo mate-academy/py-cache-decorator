82a1f5e29c01978093c3311b420c8c92fe181ce9
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-10 16:49:52 +0200

    Result

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ from typing import Any, Callable
 
 def cache(func: Callable) -> Callable:
     previous_results = {}
+
     def wrapper(*args) -> Any:
         if args in previous_results:
             print("Getting from cache")
-            return previous_results[*args]
-        print("Calculating new result")
-        result = func(*args)
-        previous_results[args] = result
-        return result
+        else:
+            print("Calculating new result")
+            previous_results[args] = func(*args)
+        return previous_results[args]
     return wrapper
