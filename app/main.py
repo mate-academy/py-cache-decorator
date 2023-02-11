15761142d6f7338bd5bc7cedfffdc33a453aab24
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-11 14:55:54 +0200

    fixed return type; optimized code

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         arguments = *args, *kwargs.values()
         if arguments not in cache_dict:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[arguments] = result
-            return result
+            cache_dict[arguments] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-            return cache_dict[arguments]
+        return cache_dict[arguments]
 
     return wrapper
