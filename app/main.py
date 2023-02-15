d484ffe9742a317b658f8d255b99c9202867f800
Yegor Turenko <ytferma@gmail.com>
2023-02-15 17:05:51 +0200

    applied all the recommendations from the comment

--- app/main.py
+++ app/main.py
@@ -1,18 +1,14 @@
-import functools
 from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache_store = {}
+    storage = {}
 
-    @functools.wraps(func)
     def wrapper(*args) -> Any:
-        if (func.__name__, args) in cache_store:
+        if args in storage:
             print("Getting from cache")
-            result = cache_store[(func.__name__, args)]
         else:
-            result = func(*args)
-            cache_store[(func.__name__, args)] = result
             print("Calculating new result")
-        return result
+            storage[args] = func(*args)
+        return storage[args]
     return wrapper
