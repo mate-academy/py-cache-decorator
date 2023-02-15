312ec190d5bc08c3a290f2205ee452bb355a0021
Ihor Trokhymchuk <just.unsoul@gmail.com>
2023-02-15 18:06:37 +0100

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
 
     storage = {}
 
-    def inner(*args, **kwargs) -> Callable:
-        if args in storage:
+    def inner(*args: Any, **kwargs: Any) -> Callable:
+        if args and args in storage:
             print("Getting from cache")
         else:
             print("Calculating new result")
             storage[args] = func(*args, **kwargs)
-        return storage[args]
+        return storage.get(args)
 
     return inner
