8bd7b2f0641962728e0a3cfc86df65050d865f6a
Volodymyr Yerokhov <kavbian@gmail.com>
2023-02-16 20:15:53 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    previous_value = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        nonlocal previous_value
+        if args in previous_value:
+            print("Getting from cache")
+            return previous_value[args]
+        else:
+            print("Calculating new result")
+            previous_value[args] = func(*args)
+            return previous_value[args]
+    return wrapper
