e9271259927c6fbbfba2ecb539606bb7aac4dfbc
Volodymyr Yerokhov <kavbian@gmail.com>
2023-02-17 14:13:10 +0200

    'remove_nonlocal_one_return'

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ def cache(func: Callable) -> Callable:
     previous_value = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        nonlocal previous_value
         if args in previous_value:
             print("Getting from cache")
-            return previous_value[args]
         else:
             print("Calculating new result")
             previous_value[args] = func(*args)
-            return previous_value[args]
+
+        return previous_value[args]
     return wrapper
