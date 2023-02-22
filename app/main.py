30c8b4f5b6d08ec439e5bb7b45cd80e9c6c4402a
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-02-22 16:00:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any
+
+
+def cache(func: callable) -> callable:
+    data_cache = {}
+
+    def inner(*args) -> Any:
+        if args in data_cache:
+            print("Getting from cache")
+            return data_cache[args]
+        else:
+            result = func(*args)
+            data_cache[args] = result
+            print("Calculating new result")
+            return result
+
+    return inner
