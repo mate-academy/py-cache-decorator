72e5c0ba5d0ebca2bf1be3f1b5b09c431876e45a
Halyna Petrova <1208petrova@gmail.com>
2023-02-23 14:28:53 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> None:
+def cache(func: Callable) -> Callable:
     my_cash = {}
 
     def inner(*args) -> str:
         if args not in my_cash.keys():
             my_cash[args] = func(*args)
             print("Calculating new result")
-            return my_cash[args]
         else:
             print("Getting from cache")
-            return my_cash[args]
 
+        return my_cash[args]
     return inner
