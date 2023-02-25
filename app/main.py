fd2921e33226799a8c0d2c74ce4ab11ab71ea812
AngelinaRemark <45980553+AngelinaRemark@users.noreply.github.com>
2023-02-25 10:31:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def cache(func: typing.Callable) -> typing.Callable:
     stored_values = {}
 
     def wrapper(*args) -> typing.Any:
-        if args in stored_values.keys():
+        if args in stored_values:
             print("Getting from cache")
         else:
             stored_values[args] = func(*args)
