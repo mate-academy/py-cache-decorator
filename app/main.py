4ad5cefd784ef992e137bc11bb07550eb65892d7
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-02-25 21:04:32 +0200

    solution_1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-#https://github.com/sbilikepy/py-cache-decorator
-#https://github.com/mate-academy/py-task-guideline/blob/main/README.md
+from typing import Any
+from typing import Callable
 
-def cache(func):
-    # Write your code here
-    pass
-#test
+
+def cache(func: Callable) -> None:
+    storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        wrapper_dict = {}
+
+        if args in storage.keys():
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            wrapper_dict[args] = func(*args, **kwargs)
+            storage.update(wrapper_dict)
+
+        return storage[args]
+
+    return wrapper
