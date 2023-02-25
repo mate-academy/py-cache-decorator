71e6c79a993bc66432966cdd580a5772674cddf1
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-02-25 21:32:23 +0200

    *args: Any

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from typing import Callable
 def cache(func: Callable) -> None:
     storage = {}
 
-    def wrapper(*args) -> Any:
+    def wrapper(*args: Any) -> Any:
         wrapper_dict = {}
 
         if args in storage.keys():
