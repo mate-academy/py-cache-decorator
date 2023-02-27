bfeb0a14cffe3390eed8a01baaaa05f0067c83d7
Viktor Yaroshenko <yaroshenkoviktorwork@gmail.com>
2023-02-27 22:14:29 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> None:
+
+    completed_runs = {}
+
+    def inner(*args) -> str:
+        if args in completed_runs:
+            print("Getting from cache")
+            return completed_runs[args]
+        else:
+            print("Calculating new result")
+            completed_runs[args] = func(*args)
+            return completed_runs[args]
+    return inner
