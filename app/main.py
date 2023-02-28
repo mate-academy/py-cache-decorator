bdecf1e667a8cf91d5416e4068f8160f97d288fc
Dmytro Sylenok <dmytros@suntechinnovation.com>
2023-02-28 11:17:16 +0200

    Fixed comments

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache = {}
 
+    @wraps(func)
     def wrapper(*args) -> Any:
         if args in cache:
             print("Getting from cache")
         else:
-            result = func(*args)
-            cache[args] = result
+            cache[args] = func(*args)
             print("Calculating new result")
         return cache[args]
 
