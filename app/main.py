35ffe31e20930fb6c1ed922eba776f378882dc74
Nikita <melniknikita2002@icloud.com>
2023-03-05 19:45:17 +0200

    Ready task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> None:
+    stored_values = {}
+
+    def inner(*args, **kwargs) -> None:
+        if args not in stored_values:
+            stored_values[args] = func(*args, **kwargs)
+            print("Calculating new result")
+            return stored_values[args]
+        else:
+            print("Getting from cache")
+            return stored_values[args]
+    return inner
