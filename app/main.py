809312a42fb1ac998d9c9c3bbd109e3ab1d05ed9
Natalia Stepanenko <natti8006@gmail.com>
2023-03-06 19:46:53 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+import functools
+
+
+def cache(func: Callable) -> Callable:
+    completed_runs = {}
+
+    @functools.wraps(func)
+    def wrapper(*args) -> Callable:
+        if args in completed_runs:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            completed_runs.update({args: func(*args)})
+        return completed_runs[args]
+    return wrapper
