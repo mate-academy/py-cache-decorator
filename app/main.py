d0a498189ac14dda1294429b4d21310adbab25a6
nickkozlov90 <116264846+nickkozlov90@users.noreply.github.com>
2023-03-06 13:34:47 +0200

    Update main.py
    deleted excessive code

--- app/main.py
+++ app/main.py
@@ -15,13 +15,3 @@ def cache(func: Callable) -> Callable:
         results.update({args: func(*args)})
         return results[args]
     return inner
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
