8e994fe25d5f3349f2decbca8d498e25a73a5b64
Anna Blahitka <anettaabl@gmail.com>
2023-03-06 13:38:41 +0300

    ?

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     results = {}
 
-    def inner(*args: any) -> any:
+    def inner(*args: Any) -> Any:
         if args in results.keys():
             print("Getting from cache")
             return results[args]
