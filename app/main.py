80b5fb7d46371af69770f68bf34d988d6f81e7b1
oithegod <serejathegod@gmail.com>
2023-03-07 14:07:14 +0200

    type fix

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    cache_dict = {}
+
+    def wrapper(*args, **kwargs) -> tuple:
+        if args not in cache_dict.keys():
+            result = func(*args)
+            cache_dict[args] = result
+            print("Calculating new result")
+            return result
+
+        print("Getting from cache")
+        return cache_dict[args]
+
+    return wrapper
