acb4769a86a962b649fb5af85c1344e571559b35
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-07 21:28:41 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     data_dict = {}
+    
     def inner(*args) -> Callable:
         if args in data_dict:
             print("Getting from cache")
@@ -15,8 +16,8 @@ def cache(func: Callable) -> Callable:
 
 
 @cache
-def long_time_func(a, b, c) -> int:
-    return(a ** b ** c) % (a * c)
+def long_time_func(var_a: int, var_b: int, var_c: int) -> int:
+    return (var_a ** var_b ** var_c) % (var_a * var_c)
 
 
 @cache
