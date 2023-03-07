c5bb7b5b22345249f985f67940a49c2315a41656
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-03-07 17:17:44 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    dict_of_cache = {}
+
+    def wrapper(*args) -> dict:
+
+        if args in dict_of_cache:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            dict_of_cache[args] = func(*args)
+
+        return dict_of_cache[args]
+
+    return wrapper
