aa74ffc8887c695cdd89603955e025c1253ec620
nickkozlov90 <116264846+nickkozlov90@users.noreply.github.com>
2023-03-08 19:06:34 +0200

    check for arguments type excluded

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ def cache(func: Callable) -> Callable:
     results = dict()
 
     def inner(*args) -> Any:
-        for _ in args:
-            if not type(_) in {int, float, tuple, str}:
-                return inner
         if args in results.keys():
             print("Getting from cache")
             return results[args]
