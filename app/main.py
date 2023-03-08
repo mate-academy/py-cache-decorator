a66d0124c57690f1b962ea7f43a68d55e07c9feb
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-08 11:29:45 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
+def cache(func: Callable) -> Any:
     data_dict = {}
 
-    def inner(*args) -> Callable:
+    def inner(*args) -> Any:
         if args in data_dict:
             print("Getting from cache")
         else:
@@ -12,13 +12,3 @@ def cache(func: Callable) -> Callable:
             data_dict[args] = func(*args)
         return data_dict[args]
     return inner
-
-
-@cache
-def long_time_func(var_a: int, var_b: int, var_c: int) -> int:
-    return (var_a ** var_b ** var_c) % (var_a * var_c)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
