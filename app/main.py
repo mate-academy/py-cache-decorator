43da09a535d0a73b51baca99318291732e7de99d
Dmytry Bondariev <bondarievmd@gmail.com>
2023-03-10 13:48:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ def cache(func: Callable) -> int:
     cache_dict = {}
 
     def wrapper(*args) -> int:
-        if str(args) not in cache_dict.keys():
+        if args not in cache_dict.keys():
             print("Calculating new result")
             result = func(*args)
-            cache_dict[str(args)] = result
+            cache_dict[args] = result
         else:
             print("Getting from cache")
 
-        return cache_dict[str(args)]
+        return cache_dict[args]
     return wrapper
