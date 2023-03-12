e15237551816ed1f94ba8dcaed9fe39d2bc129ef
unknown <michaeltwins@gmail.com>
2023-03-12 20:05:15 +0200

    Solution decor2

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any, Callable
+
+
+def cache(func: Callable) -> Any:
+    sum_cache = {}
+
+    def wrapped(*args: Callable) -> Any:
+        if args not in sum_cache:
+            temp_cache = func(*args)
+            sum_cache[args] = temp_cache
+            print("Calculating new result")
+            return temp_cache
+        else:
+            print("Getting from cache")
+            return sum_cache[args]
+
+    return wrapped
