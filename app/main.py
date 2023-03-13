24ea1c511969aabacc19b5c6874c21774c72807b
Philip Pl <filllick13@gmail.com>
2023-03-13 21:12:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cache_database = {}
+
+    def wrapper(*args) -> Any:
+        if tuple(args) not in cache_database:
+            print("Calculating new result")
+            cache_database[tuple(args)] = func(*args)
+            return cache_database[tuple(args)]
+
+        print("Getting from cache")
+        return cache_database[tuple(args)]
+    return wrapper
