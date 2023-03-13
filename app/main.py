14ef87a4c2c4caee4efb0cd9612166cde8a6de50
Philip Pl <filllick13@gmail.com>
2023-03-13 21:45:34 +0200

    Added wraps, removed tuples

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_database = {}
 
+    @functools.wraps(func)
     def wrapper(*args) -> Any:
-        if tuple(args) not in cache_database:
+        if args not in cache_database:
             print("Calculating new result")
-            cache_database[tuple(args)] = func(*args)
-            return cache_database[tuple(args)]
+            cache_database[args] = func(*args)
+            return cache_database[args]
 
         print("Getting from cache")
-        return cache_database[tuple(args)]
+        return cache_database[args]
     return wrapper
