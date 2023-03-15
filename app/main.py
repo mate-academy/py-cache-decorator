a7cd63d694a5b2d5d1251dc7f7eb08188f4c200d
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-15 17:46:46 +0200

    Solution py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    memory = {}
+
+    def decorator(*args, **kwargs) -> Callable:
+        key = str(args) + str(kwargs)
+        if key in memory:
+            print("Getting from cache")
+            return memory[key]
+        else:
+            value = func(*args, **kwargs)
+            memory[key] = value
+            print("Calculating new result")
+            return memory[key]
+    return decorator
