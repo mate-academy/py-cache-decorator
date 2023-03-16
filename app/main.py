cd715487c0f91bd2bad6f7dd8979f919eee4c89e
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-16 13:37:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any, Callable
+
+
+def cache(func: Callable) -> Callable:
+    memory = {}
+
+    def decorator(*args: tuple, **kwargs: dict) -> Any:
+        key = str(args) + str(kwargs)
+        if key in memory:
+            print("Getting from cache")
+            return memory[key]
+        value = func(*args, **kwargs)
+        memory[key] = value
+        print("Calculating new result")
+        return memory[key]
+    return decorator
