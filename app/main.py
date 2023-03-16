9027771f73e9654100a6c4e5e755e943fd7dfee0
NVMBR13 <108105455+NVMBR13@users.noreply.github.com>
2023-03-16 03:09:45 +0200

    v1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    cached_results = {}
+
+    def inner(*args: any) -> any:
+        if args in cached_results.keys():
+            print("Getting from cache")
+            return cached_results[args]
+        print("Calculating new result")
+        cached_results.update({args: func(*args)})
+        return cached_results[args]
+
+    return inner
