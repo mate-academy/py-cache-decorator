4d272b185ceee04ea81b779118ae55d833a011ef
z1k01ff <z1k01ff@gmail.com>
2023-03-25 20:42:57 +0200

    Solved Task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Any:
+    result = {}
+
+    @wraps(func)
+    def inner(*args: Any) -> Any:
+        if args in result:
+            print("Getting from cache")
+            return result[args]
+        else:
+            result.update({args: func(*args)})
+            print("Calculating new result")
+        return result[args]
+
+    return inner
