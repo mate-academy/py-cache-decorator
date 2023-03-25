4ec0010d1579f350e430bb91d87aa9bd86ffd1f0
Alim Selemetov <alimselemetow@gmail.com>
2023-03-25 18:13:50 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,40 @@
-def cache(func):
-    # Write your code here
-    pass
+import functools
+from typing import Callable, Any
+
+def cache(func: Callable):
+    area_cache = {}
+    @functools.wraps(func)
+    def wrap_cache(*args, **kwargs)-> Any:
+        key = args + tuple(kwargs.values())
+        if key in area_cache:
+            print("Getting from cache")
+            return area_cache[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            area_cache[key] = result
+            return result
+    return wrap_cache
+
+
+
+
+
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+
+print(long_time_func(2,4,5))
+print(long_time_func(2,4,6))
+print(long_time_func(2,4,5))
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
+print(long_time_func(2,4,5))
+print(long_time_func(2,4,5))
