d7cefad98f2c4d9d48f6f080491efe52d3ac922e
Volodymyr Borovets <vborovets0@gmail.com>
2023-03-25 10:59:50 +0200

    add correct type annotations

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     saved_data = {}
 
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
+    def wrapper(*args: tuple, **kwargs: dict) -> Any:
         if args not in saved_data:
             print("Calculating new result")
             saved_data[args] = saved_data.get(args, func(*args, **kwargs))
