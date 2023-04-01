7c92a392ddfc6c2b66b7463269fdf1a5ad9554be
Olexandr <maxbovling23432@gmail.com>
2023-04-01 19:52:29 +0300

    Solution010423

--- app/main.py
+++ app/main.py
@@ -1,3 +1,41 @@
-def cache(func):
-    # Write your code here
-    pass
+import time
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+
+    cache_storage = {}
+
+    def wrapper(*args, **kwargs) -> any:
+        key = (func, args, frozenset(kwargs.items()))
+        if key not in cache_storage:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
+        else:
+            print("Getting from cache")
+
+        return cache_storage[key]
+
+    return wrapper
+
+
+@cache
+def long_time_func(aa: int, bb: int, cc: int) -> int:
+    return (aa ** bb ** cc) % (aa * cc)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    result = cache()
+    end_time = time.time()
+    long_time_func(1, 2, 3)
+    long_time_func(2, 2, 3)
+    long_time_func_2((5, 6, 7), 5)
+    long_time_func(1, 2, 3)
+    long_time_func_2((5, 6, 7), 10)
