73e9e8a50d0729b25656e1786cdf8dd07f276779
Olexandr <maxbovling23432@gmail.com>
2023-04-01 20:28:15 +0300

    Solution010423

--- app/main.py
+++ app/main.py
@@ -18,24 +18,3 @@ def cache(func: Callable) -> Callable:
         return cache_storage[key]
 
     return wrapper
-
-
-@cache
-def long_time_func(aa: int, bb: int, cc: int) -> int:
-    return (aa ** bb ** cc) % (aa * cc)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-if __name__ == "__main__":
-    start_time = time.time()
-    result = cache()
-    end_time = time.time()
-    long_time_func(1, 2, 3)
-    long_time_func(2, 2, 3)
-    long_time_func_2((5, 6, 7), 5)
-    long_time_func(1, 2, 3)
-    long_time_func_2((5, 6, 7), 10)
