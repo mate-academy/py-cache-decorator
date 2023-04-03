309d38f6330b5e096d2bfb4562e60e4101bc7eb8
Marina <marishka28023@gmail.com>
2023-04-03 13:07:59 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,32 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    arr = {}
+    storage = {}
 
     def inner(*args) -> Callable:
-        if args not in arr.keys():
-            arr[args] = func(*args)
+        if args not in storage.keys():
+            storage[args] = func(*args)
             print("Calculating new result")
         else:
             print("Getting from cache")
 
-        return arr[args]
+        return storage[args]
     return inner
-
-
-# @cache
-# def long_time_func(a: int, b: int, c: int) -> int:
-#     return (a ** b ** c) % (a * c)
-#
-#
-# @cache
-# def long_time_func_2(n_tuple: tuple, power: int) -> int:
-#     return [number ** power for number in n_tuple]
-#
-#
-# long_time_func(1, 2, 3)
-# long_time_func(2, 2, 3)
-# long_time_func_2((5, 6, 7), 5)
-# long_time_func(1, 2, 3)
-# long_time_func_2((5, 6, 7), 10)
-# long_time_func_2((5, 6, 7), 10)
