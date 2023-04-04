0a039be21628408880ca06bd0b9901b26f6afb09
Illya Svyshcho <iill44iikk@gmail.com>
2023-04-04 17:54:02 +0300

    Solution of decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+def cache(func: Callable) -> Callable:
+    dict_of_args = {}
+
+    def inner(*args) -> Callable:
+        if args in dict_of_args.keys():
+            print("Getting from cache")
+        else:
+            dict_of_args[args] = func(*args)
+            print("Calculating new result")
+        return dict_of_args[args]
+    return inner
+
