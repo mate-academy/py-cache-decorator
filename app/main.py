02a4ca32b7c5cdd2b6b7628e7ff029b7a88277d6
Roman Fedorchuk <slasherroman@gmail.com>
2023-04-05 17:28:02 +0300

    'revised'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def cache(func: Callable) -> Callable:
 
     results_dict = {}
 
-    def inner(*args) -> Any:
+    def inner(*args: tuple) -> Any:
         if args not in results_dict.keys():
             print("Calculating new result")
             results_dict[args] = func(*args)
