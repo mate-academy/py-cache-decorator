c270249c8ba4f96ab89d55dcc8cf4020ec5f5b6d
Bohdan Zakora <b.v.zakora@gmail.com>
2023-04-09 15:22:09 +0300

    fixed the mistakes

--- app/main.py
+++ app/main.py
@@ -9,11 +9,9 @@ def cache(func: Callable) -> Callable:
     def wrappers(*args: Any) -> Any:
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_dict[args] = result
-            return result
+            cache_dict[args] = func(*args)
+        return cache_dict[args]
 
     return wrappers
