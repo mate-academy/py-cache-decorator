5fed2cbfeeaebed13a00905611cfee3ea3dbc540
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-04-10 13:59:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+from functools import wraps
+
+
+def cache(func: Callable) -> Callable:
+    cached_data = {}
+
+    @wraps(func)
+    def wrapper(*args) -> Any:
+        nonlocal cached_data
+        if args in cached_data.keys():
+            print("Getting from cache")
+            result = cached_data.get(args)
+        else:
+            result = func(*args)
+            cached_data.update({args: result})
+            print("Calculating new result")
+        return result
+
+    return wrapper
