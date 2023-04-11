ce12820b0c65b96dde8a8bbc86fb8224b505ca8b
Oleksandr Yatsiuk <oleksandr.yatsiuk95@gmail.com>
2023-04-11 22:06:11 +0300

    Solution #2 what do you mean by "duplicate code with last return?"

--- app/main.py
+++ app/main.py
@@ -5,12 +5,10 @@ def cache(func: Callable) -> Callable:
     result = {}
 
     def wrapper(*args, **kwargs) -> Callable:
-        key = args
-        if key in result:
+        if args in result:
             print("Getting from cache")
-            return result[key]
         else:
-            result[key] = func(*args, **kwargs)
+            result[args] = func(*args, **kwargs)
             print("Calculating new result")
-        return result[key]
+        return result[args]
     return wrapper
