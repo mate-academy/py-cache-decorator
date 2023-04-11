dee0d61b1d6ea22155610014c8d95ca0ddb01ac1
Iryna Bulakevych <iryna.bulakevych@gmail.com>
2023-04-11 13:05:32 +0300

    'fixed'

--- app/main.py
+++ app/main.py
@@ -13,22 +13,3 @@ def cache(func: Callable) -> Callable:
         print("Calculating new result")
         return cache_args[key]
     return wrapper
-
-
-# @cache
-# def long_time_func(a: int, b: int, c: int) -> int:
-#     return (a ** b ** c) % (a * c)
-#
-#
-# @cache
-# def long_time_func_2(n_tuple: tuple, power: int) -> Any:
-#     return [number ** power for number in n_tuple]
-#
-#
-# long_time_func(1, 2, 3)
-# long_time_func(2, 2, 3)
-# long_time_func_2((5, 6, 7), 5)
-# long_time_func(1, 2, 3)
-# long_time_func_2((5, 6, 7), 10)
-# long_time_func_2((5, 6, 7), 5)
-# long_time_func_2((5, 6, 7), 10)
