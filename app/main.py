3fdffc4643d13304fd73d050f3569449dc9da7d0
leetwinoff <leetwinoff92@gmail.com>
2023-04-18 10:27:22 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -10,8 +10,7 @@ def cache(func: Callable) -> Callable:
         if args in cashed_result:
             print("Getting from cache")
             return cashed_result[args]
-        else:
-            print("Calculating new result")
-            cashed_result[args] = func(*args)
-            return cashed_result[args]
+        print("Calculating new result")
+        cashed_result[args] = func(*args)
+        return cashed_result[args]
     return wrapper
