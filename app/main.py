326cb59d009f0c0f5663612ff202d30c532ae181
academic_karnal <ybondarenko80@gmail.com>
2023-04-20 12:07:17 +0300

    Solution_1

--- app/main.py
+++ app/main.py
@@ -5,13 +5,11 @@ def cache(func: Callable) -> Callable:
     local_cache = {}
 
     def wrapper(*args) -> int:
-        if all(isinstance(arg, (int, str, float, tuple)) for arg in args):
-            if args in local_cache:
-                print("Getting from cache")
-                return local_cache[args]
-            else:
-                value = func(*args)
-                local_cache[args] = value
-                print("Calculating new result")
-                return value
+        if args in local_cache:
+            print("Getting from cache")
+            return local_cache[args]
+        else:
+            local_cache[args] = func(*args)
+            print("Calculating new result")
+            return local_cache[args]
     return wrapper
