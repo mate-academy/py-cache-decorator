15913725c645f7e79e7dd569701a2556ff8292b3
Bohdan2001007 <bogdan1991007@gmail.com>
2023-04-20 16:57:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -13,26 +13,7 @@ def cache(func: Callable) -> Callable:
             return memory[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            memory[key] = result
-            return result
+            memory[key] = func(*args, **kwargs)
+            return memory[key]
 
     return wrapper
-
-
-@cache
-def long_time_func(a_value: int, b_value: int, c_value: int) -> int:
-    return (a_value ** b_value ** c_value) % (a_value * c_value)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
