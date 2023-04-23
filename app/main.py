77031d320e60096a14406ca4d43c192e53f29e5d
Dmitry <dimior1992@gmail.com>
2023-04-23 12:32:47 +0300

    fixed typehints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-def cache(func: callable) -> callable:
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def inner(*args: any) -> callable:
+    def inner(*args) -> any:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict.get(args)
