1525a7cced7954dcd3e346285e0ad6a1d3981d69
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-04-30 19:27:31 +0200

    decorator is done

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ def cache(func):
     stored_results = {}
     def wrapper(*args):
         if args in stored_results:
-            print('Getting from cache')
+            print("Getting from cache")
             return stored_results[args]
         else:
-            print('Calculating new result')
+            print("Calculating new result")
             result = func(*args)
             stored_results[args] = result
             return result
