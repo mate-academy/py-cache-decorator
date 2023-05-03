e7347adeca19d1b08d9dd4cc0fa8a20e951a4be4
Borys Kirichenko <127791726+bkirichenko@users.noreply.github.com>
2023-05-03 20:49:56 +0200

    Apply suggestions from code review
    Co-authored-by: Vasyl Smutok <vasyl.smutok@gmail.com>

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> Any:
         if args in stored_results:
             print("Getting from cache")
-            return stored_results[args]
         else:
+            stored_results[args] = func(*args)
             print("Calculating new result")
-            result = func(*args)
-            stored_results[args] = result
-            return result
+        return stored_results[args]
     return wrapper
