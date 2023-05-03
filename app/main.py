c0e25ab01fe9671372bb72dcdaf1de58403c4b5f
Viacheslav Shcherbak <32074944+SlavikSherbak@users.noreply.github.com>
2023-05-03 13:50:19 +0300

    Fix errors

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def cache(func: Callable) -> Callable:
             return results[args]
 
         print("Calculating new result")
-        result = func(*args)
+        results[args] = func(*args)
         return results[args]
 
     return wrapper
