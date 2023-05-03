3b6d677b320a8fc903dad120357535d6094cc107
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-05-03 15:51:57 +0300

    Create cash decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cash_dict = {}
+
+    def wrapper(*args) -> Any:
+        if args in cash_dict:
+            print("Getting from cache")
+            return cash_dict[args]
+
+        print("Calculating new result")
+        cash_dict[args] = func(*args)
+        return cash_dict[args]
+
+    return wrapper
