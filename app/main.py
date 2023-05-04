3de9cafddafcdadcbd09968c9572e77b9a544551
Denys Raboshchuk <kontroler444@gmail.com>
2023-05-04 09:27:33 +0300

    Removed "else"

--- app/main.py
+++ app/main.py
@@ -8,9 +8,8 @@ def cache(func: Callable) -> Callable:
         if args in dict_cache:
             print("Getting from cache")
             return dict_cache[args]
-        else:
-            print("Calculating new result")
-            dict_cache[args] = func(*args, **kwargs)
-            return dict_cache[args]
+        print("Calculating new result")
+        dict_cache[args] = func(*args, **kwargs)
+        return dict_cache[args]
 
     return wrapper
