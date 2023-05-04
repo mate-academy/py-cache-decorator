3db01ef79131e5d21488435d8a66f1c40319970c
Ihor Bachuk <igor.bachuk04@gmail.com>
2023-05-04 18:07:19 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    storage = {}
+
+    def checker(*args: list[Any], **kwargs: dict[Any]) -> Any:
+        request = str([*args, {**kwargs}])
+        if request not in storage:
+            print("Calculating new result")
+            res = func(*args, **kwargs)
+            storage.update({request: res})
+            return res
+        else:
+            print("Getting from cache")
+            return storage[request]
+
+    return checker
