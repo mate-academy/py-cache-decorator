8c8cb94b836b34e3b263b204475e53b7338d4dc0
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-05-06 20:32:59 +0300

    Implement cache decorator task

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cached_results = {}
+
+    @wraps(func)
+    def wrapper(*args: tuple) -> Any:
+        if args in cached_results:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cached_results.update({args: func(*args)})
+        return cached_results.get(args)
+
+    return wrapper
