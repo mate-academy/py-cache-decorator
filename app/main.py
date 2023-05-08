496533af7526425e2ff3fc1b847c5f1e88ee6cfe
Volodymyr Bohomol <8773927@gmail.com>
2023-05-08 21:50:05 +0300

    Developer cache decorator that can remember results of executing some functions.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    args_cache = {}
+
+    def inner(*args) -> int:
+        nonlocal args_cache
+        if args in args_cache:
+            print("Getting from cache")
+            return args_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            args_cache[args] = result
+            return result
+    return inner
