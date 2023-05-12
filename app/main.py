b9286fc59f1ea4c6e78eee6589b9314af88d09e2
XOctOpus1 <dd111320@gmail.com>
2023-05-12 21:00:41 +0300

    Finish Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    stored_cash_results_per_function = {}
+
+    def inner(*args) -> Callable:
+
+        if args in stored_cash_results_per_function:
+            print("Getting from cache")
+            return stored_cash_results_per_function[args]
+
+        else:
+            stored_cash_results_per_function[args] = func(*args)
+            print("Calculating new result")
+
+            return stored_cash_results_per_function[args]
+
+    return inner
