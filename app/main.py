c507b0818cc7e841976dc1c4e47959c33bf1d2c2
Volodymyr Vilisov <vilisovv@gmail.com>
2023-05-13 08:44:52 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    cache_item = {}
+
+    def long_time(*args) -> str:
+
+        if args not in cache_item.keys():
+            cache_item[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_item[*args]
+    return long_time
