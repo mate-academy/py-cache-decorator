e12918ef1f2a05d9cc606c8686550dcba3591c0d
Ivan <ivan.korshunov.1997@gmail.com>
2023-05-14 13:55:24 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    all_results = {}
+
+    @wraps(func)
+    def decorated_function(*args, **kwargs) -> Callable:
+        cache_key = (func.__name__, args)
+        available_in_cache = all_results.get(cache_key, None)
+        if available_in_cache is not None:
+            print("Getting from cache")
+            return available_in_cache
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        all_results[cache_key] = result
+
+        return result
+
+    return decorated_function
