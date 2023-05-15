63f78a39dce6f36f83a5b50a12c250691d25a1a0
Petro Havrysh <petro.havrysh@gmail.com>
2023-05-15 20:44:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+
+
+def cache(func: callable) -> callable:
+    cache_dict = {}
+
+    @wraps(func)
+    def wrapper(*args: tuple, **kwargs: dict) -> any:
+        key = args + tuple(kwargs.items())
+        if key not in cache_dict:
+            cache_dict[key] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_dict[key]
+
+    return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
