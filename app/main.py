200ae014f6e3c5a4e50c3f56c0b3050c51cdcde9
Vadym <seredavadimsv@gmail.com>
2023-05-17 10:31:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from functools import wraps
+
 def cache(func):
-    # Write your code here
-    pass
+    results = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in results:
+            print("Getting from cache")
+            return results[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            results[key] = result
+            return result
+
+    return wrapper
