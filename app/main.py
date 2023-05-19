d592876d340cbdd32bc17c5fd3a03aa8d14e1672
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2023-05-19 15:23:07 +0300

    Implementation cache decorator

--- app/main.py
+++ app/main.py
@@ -10,9 +10,8 @@ def cache(func: Callable) -> Callable:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
-        else:
-            print("Calculating new result")
-            res = func(*args)
-            cache_dict[args] = res
-            return res
+        print("Calculating new result")
+        res = func(*args)
+        cache_dict[args] = res
+        return res
     return inner
