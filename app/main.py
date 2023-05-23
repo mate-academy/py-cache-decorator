66b24e6f064b18857591694c11ab14f8039d4c4d
Mikhail <tiunovmixs@gmail.com>
2023-05-23 10:32:47 +0300

    Fix flake8 Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from functools import wraps
 from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
+    @wraps(func)
     def wrapper(*args) -> Any:
         if args in cache_dict:
             print("Getting from cache")
