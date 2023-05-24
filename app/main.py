8c52ddfe1659e37071ef905e588220c7ebd23e76
Glieb <gleblitvinenko3007@gmail.com>
2023-05-24 22:35:37 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cache_dict:
