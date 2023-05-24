34554d984e0e9b508132420d9c17f9ad3fa7f1ff
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-05-24 13:15:07 +0300

    Refactored to reflect requested changes

--- app/main.py
+++ app/main.py
@@ -5,11 +5,10 @@ def cache(func: callable) -> callable:
     cache_vault = {}
 
     def inner(*args) -> Any:
-        if args in cache_vault:
+        if args not in cache_vault:            
+            print("Calculating new result")
+            cache_vault[args] = func(*args)
+        else:
             print("Getting from cache")
-            return cache_vault[args]
-        print("Calculating new result")
-        result = func(*args)
-        cache_vault[args] = result
-        return result
+        return cache_vault[args]
     return inner
