99c7e53415e314dc78f065b3339ad9378b60ff17
Oleksii Kolii <igravkompik@gmail.com>
2023-05-25 20:48:35 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cached = {}
+
+    def inner(*args) -> Any:
+        if args not in cached:
+            result = func(*args)
+            cached[args] = result
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cached[args]
+    return inner
