a85911ab752910a418d1b20f5577d8972464eefd
Veinmax <aturjust88@gmail.com>
2023-05-25 19:23:28 +0300

    First

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args) -> Any:
         if args in some_dict:
+
             print("Getting from cache")
         else:
             print("Calculating new result")
