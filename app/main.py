f40994c4899f4e3ecd21bb54b7636a0b99f646b2
Anton Komarov <anton_komarov_qa@ukr.net>
2023-05-25 18:14:18 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,15 +5,10 @@ def cache(func: Callable) -> Callable:
     stored_data = {}
 
     def wrapper(*args) -> int:
-
-        nonlocal stored_data
-
         if args not in stored_data:
             print("Calculating new result")
             stored_data[args] = func(*args)
             return stored_data[args]
-
         print("Getting from cache")
         return stored_data[args]
-
     return wrapper
