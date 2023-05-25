2e3dbaa9d4e8ea4018eab1f45953beb45900d3e0
Diana Shyrokikh <diashiroks@gmail.com>
2023-05-25 18:52:09 +0300

    Rewrited solution with only one return

--- app/main.py
+++ app/main.py
@@ -5,11 +5,10 @@ def cache(func: Callable) -> Callable:
     saved_results = {}
 
     def inner(*args) -> Any:
-        if args in saved_results:
+        if args not in saved_results:
+            print("Calculating new result")
+            saved_results[args] = func(*args)
+        else:
             print("Getting from cache")
-            return saved_results[args]
-
-        print("Calculating new result")
-        saved_results[args] = func(*args)
         return saved_results[args]
     return inner
