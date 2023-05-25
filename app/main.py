6b457d442f792fc3ae5ec2cfdd0ef6858b7f11e9
Vlad <vladislav.zhyla@gmail.com>
2023-05-25 18:03:59 +0300

    Removed "else" and "nonlocal" from Solution

--- app/main.py
+++ app/main.py
@@ -5,13 +5,11 @@ def cache(func: Callable) -> Callable:
     storage = {}
 
     def inner(*args) -> int:
-        nonlocal storage
         if args not in storage:
             print("Calculating new result")
             storage[args] = func(*args)
             return storage[args]
-        else:
-            print("Getting from cache")
-            return storage[args]
+        print("Getting from cache")
+        return storage[args]
 
     return inner
