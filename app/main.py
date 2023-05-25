5e78ad246d914b8e616646f1575fed2353dc6be8
Anton Shpakovych <shpakovych18@gmail.com>
2023-05-25 18:57:19 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    stored_cashe = {}
+
+    def wrapper(*args) -> str:
+        if args in stored_cashe:
+            print("Getting from cache")
+            return stored_cashe[args]
+        else:
+            new_result = func(*args)
+            stored_cashe[args] = new_result
+            print("Calculating new result")
+            return new_result
+    return wrapper
