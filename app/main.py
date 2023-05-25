da119bc7b5781a5de73f1cb4707d890961e78eb7
Yaryk Konoplyov <kannabis252@gmail.com>
2023-05-25 22:46:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cache_dict = {}
+
+    def inner(*args) -> Any:
+        nonlocal cache_dict
+        res = 0
+        if args not in cache_dict:
+            print("Calculating new result")
+            res = func(*args)
+            cache_dict[args] = res
+        elif args in cache_dict:
+            print("Getting from cache")
+            res = cache_dict[args]
+        return res
+
+    return inner
