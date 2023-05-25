7cd0e36eae45f262937899a5c2e48034cb31d2bd
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-05-25 16:07:48 +0300

    removed redundant "else"

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ def cache(func: Callable) -> Callable:
         if args in result_dict:
             print("Getting from cache")
             return result_dict.get(args)
-        else:
-            result = func(*args)
-            result_dict.update({args: result})
-            print("Calculating new result")
-            return result
+
+        result = func(*args)
+        result_dict.update({args: result})
+        print("Calculating new result")
+        return result
 
     return wrapper
