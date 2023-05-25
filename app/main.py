366263636719497eab29824a2189ae16e9c0b6f1
YOUR_NAME <YOUR_EMAIL>
2023-05-25 17:24:11 +0300

    Decoration (task #4(fix comment from mentor))

--- app/main.py
+++ app/main.py
@@ -10,20 +10,8 @@ def cache(func: Callable) -> Callable:
         if args in results:
             print("Getting from cache")
             return results[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            results[args] = result
-            return result
+        print("Calculating new result")
+        results[args] = func(*args)
+        return results[args]
 
     return wrapper
-
-
-@cache
-def long_time_func(first_num: int, second_num: int, third_num: int) -> int:
-    return (first_num ** second_num ** third_num) % (first_num * third_num)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
