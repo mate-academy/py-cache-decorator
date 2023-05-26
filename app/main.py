2b912a7c35169d96d9d65d35f2fbab62d9eed9b4
Vlad Dukh <vladdukh15@gmail.com>
2023-05-26 16:46:58 +0300

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> None:
-    cache = {}
+def cache(func: Callable) -> Callable:
+    cache_dict = {}
 
-    def inner(*args) -> None:
-        if args not in cache:
+    def inner(*args) -> Callable:
+        if args not in cache_dict:
             print("Calculating new result")
-            cache[args] = func(*args)
+            cache_dict[args] = func(*args)
         else:
             print("Getting from cache")
-        return cache[args]
+        return cache_dict[args]
     return inner
