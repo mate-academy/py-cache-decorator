8f3ef1df99cb549cf6d3d9529abba18c55d12515
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-05-26 17:09:12 +0300

    Cache-decorator-task implemented

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    result_store = {}
+
+    def wrapper(*args) -> Any:
+        if args not in result_store:
+            print("Calculating new result")
+            result_store[args] = func(*args)
+            return result_store.get(args)
+        else:
+            print("Getting from cache")
+            return result_store.get(args)
+
+    return wrapper
