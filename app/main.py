ab898c46050d275aa52fdac33d0cb6f70b7a8726
Denys Seliverstov <catsperr322@gmail.com>
2023-05-26 15:11:56 +0300

    Fixed the repeating code

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> Callable:
         if args in stored_results:
             print("Getting from cache")
-            return stored_results[args]
         else:
             stored_results[args] = func(*args)
             print("Calculating new result")
-            return stored_results[args]
+        return stored_results[args]
     return wrapper
