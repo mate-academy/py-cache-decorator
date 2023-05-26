c4ea527735b57f306bb6a2332c381c5c16f6ee82
Tarasidze <dtar84@ukr.net>
2023-05-26 13:01:04 +0300

    Fixed after reject

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-def cache(func: callable) -> callable:
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
     stored_results = {}
 
-    def inner(*args) -> callable:
+    def inner(*args) -> Any:
         if args not in stored_results:
             stored_results[args] = func(*args)
             print("Calculating new result")
-            return stored_results[args]
+        else:
+            print("Getting from cache")
 
-        print("Getting from cache")
         return stored_results[args]
 
     return inner
