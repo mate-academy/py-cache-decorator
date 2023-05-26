59fb5c3a0433898d2ad5f57e097df147f3c7daa8
Yaryk Konoplyov <kannabis252@gmail.com>
2023-05-26 13:28:51 +0300

    Solution + variable res removed, removed nonlocal, elif replaced with else

--- app/main.py
+++ app/main.py
@@ -5,15 +5,11 @@ def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     def inner(*args) -> Any:
-        nonlocal cache_dict
-        res = 0
         if args not in cache_dict:
             print("Calculating new result")
-            res = func(*args)
-            cache_dict[args] = res
-        elif args in cache_dict:
+            cache_dict[args] = func(*args)
+        else:
             print("Getting from cache")
-            res = cache_dict[args]
-        return res
+        return cache_dict[args]
 
     return inner
