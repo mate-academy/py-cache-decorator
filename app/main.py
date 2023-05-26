5d2b4e0ad0aadf5c9d989f53b3ed8eb1214d7761
Anton Shpakovych <shpakovych18@gmail.com>
2023-05-26 15:20:46 +0300

    Fixed type annotation for wrapper and also del unnecessary return

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     stored_cashe = {}
 
-    def wrapper(*args) -> int:
+    def wrapper(*args) -> Any:
         if args in stored_cashe:
             print("Getting from cache")
-            return stored_cashe[args]
-
-        stored_cashe[args] = func(*args)
-        print("Calculating new result")
+        else:
+            stored_cashe[args] = func(*args)
+            print("Calculating new result")
         return stored_cashe[args]
     return wrapper
