161731ff8548f7f0db6b8ee101d7081c08253ece
Vlad Larin <lordgadyka71@gmail.com>
2023-05-27 17:35:22 +0300

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+cached = {}
+
+
+def cache(func: Callable) -> Callable:
+    cached_values = {}
+
+    def wrapper(*args) -> Any:
+        func_name = func.__name__
+        if func_name not in cached_values.keys():
+            cached_values[func_name] = {}
+        if args in cached_values[func_name].keys():
+            print("Getting from cache")
+            return cached_values[func_name][args]
+        else:
+            print("Calculating new result")
+            function_result = func(*args)
+            cached_values[func_name][args] = function_result
+            return function_result
+
+    return wrapper
