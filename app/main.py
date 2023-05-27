fcd3d01805c51f623b2a172d0f55621c86965865
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-05-27 15:14:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Union
+
+
+def cache(func: Callable) -> Callable:
+    cached_data = {}
+
+    def wrapper(*args, **kwargs) -> Union[int, list]:
+        key = (args, tuple(kwargs.items()))
+        if key in cached_data:
+            print("Getting from cache")
+            result = cached_data[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached_data[key] = result
+        return result
+    return wrapper
