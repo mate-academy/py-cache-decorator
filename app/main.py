af712a0770ba6b77be02822d96ce6737579d75ab
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-05-29 11:26:09 +0300

    Fix: annotation for args, use return just once

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     result_store = {}
 
-    def wrapper(*args) -> Any:
+    def wrapper(*args: Any) -> Any:
         if args not in result_store:
             print("Calculating new result")
             result_store[args] = func(*args)
-            return result_store.get(args)
         else:
             print("Getting from cache")
-            return result_store.get(args)
+
+        return result_store.get(args)
 
     return wrapper
