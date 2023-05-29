5b3f6c1835539f249b7b3f8be68a6be38aade0b3
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-05-29 20:14:47 +0300

    removed the 'result' variable

--- app/main.py
+++ app/main.py
@@ -4,14 +4,12 @@ from typing import Callable, Union
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def wrapper(*args, **kwargs) -> Union[int, list]:
-        key = (args, tuple(kwargs.items()))
+    def wrapper(*args) -> Union[int, list]:
+        key = args
         if key in cached_data:
             print("Getting from cache")
-            result = cached_data[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_data[key] = result
-        return result
+            cached_data[key] = func(*args)
+        return cached_data[key]
     return wrapper
