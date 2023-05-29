bd3ccd5be79ee42c268ed1418fdb83c46095904c
Alex Yatsun <starcrafter4444@gmail.com>
2023-05-29 14:06:15 +0300

    Flake Improve

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+# from typing import Callable, Any
+#
+#
+# def cache(func: Callable) -> Callable:
+#     arguments = {}
+#
+#     def inner(*args) -> Any:
+#         if (*args,) in arguments:
+#             print("Getting from cache")
+#             return arguments[*args]
+#         print("Calculating new result")
+#         arguments[args] = func(*args)
+#         return arguments[args]
+#     return inner
+
 from typing import Callable, Any
 
 
@@ -5,9 +20,9 @@ def cache(func: Callable) -> Callable:
     arguments = {}
 
     def inner(*args) -> Any:
-        if (*args,) in arguments:
+        if args in arguments:
             print("Getting from cache")
-            return arguments[*args]
+            return arguments[args]
         print("Calculating new result")
         arguments[args] = func(*args)
         return arguments[args]
