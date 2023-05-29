56583f8aa31a6b8128df7b20d0f1f7307f397b25
maccarets <maccarets@gmail.com>
2023-05-29 17:05:41 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def cache(func: Callable) -> Any:
         if args not in storage:
             print("Calculating new result")
             storage[args] = func(*args)
-            return storage[args]
-        print("Getting from cache")
+        else:
+            print("Getting from cache")
         return storage[args]
     return inner
