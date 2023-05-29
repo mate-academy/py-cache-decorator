f797fe95279ec88bac903170db81f536c357237b
Olha Tryhub <olha.tryhub@nure.ua>
2023-05-29 14:32:18 +0300

    unpacking, packing of args are removed

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def cache(func: Callable) -> Callable:
     results_dict = {}
 
     def inner(*args) -> int:
-        if (*args,) in results_dict:
+        if args in results_dict:
             print("Getting from cache")
         else:
-            results_dict[(*args,)] = func(*args)
+            results_dict[args] = func(*args)
             print("Calculating new result")
-        return results_dict[(*args,)]
+        return results_dict[args]
     return inner
