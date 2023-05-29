6f045a814adfe0b14032b6e2153821018bbf6a55
yakhlamova <yanaakhlamova@gmail.com>
2023-05-29 15:24:15 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -13,8 +13,7 @@ def cache(func: Callable) -> Callable:
             return results[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            results[key] = result
-            return result
+            results[key] = func(*args, **kwargs)
+            return results[key]
 
     return inner
