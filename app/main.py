5dad12f80ad7ff825c6a3e8dda67798eea209814
Alex Yatsun <starcrafter4444@gmail.com>
2023-05-29 17:50:49 +0300

    improve return

--- app/main.py
+++ app/main.py
@@ -1,18 +1,3 @@
-# from typing import Callable, Any
-#
-#
-# def cache(func: Callable) -> Callable:
-#     arguments = {}
-#
-#     def inner(*args) -> Any:
-#         if (*args,) in arguments:
-#             print("Getting from cache")
-#             return arguments[*args]
-#         print("Calculating new result")
-#         arguments[args] = func(*args)
-#         return arguments[args]
-#     return inner
-
 from typing import Callable, Any
 
 
@@ -22,8 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Any:
         if args in arguments:
             print("Getting from cache")
-            return arguments[args]
-        print("Calculating new result")
-        arguments[args] = func(*args)
+        else:
+            print("Calculating new result")
+            arguments[args] = func(*args)
         return arguments[args]
     return inner
