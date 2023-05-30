c7afccb5f2b40d68d74ea359dffd138d8f3ba461
Anastasia <anastasiiya.p@gmail.com>
2023-05-30 10:01:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,26 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Union, Tuple
+
+
+def cache(func: Callable) -> Callable:
+    results = {}
+
+    def inner(*args: Union[int, Tuple[int, float]]) -> Union[int, list]:
+        if args in results:
+            print("Getting from cache")
+            return results[args]
+        else:
+            result_value = func(*args)
+            results[args] = result_value
+            print("Calculating new result")
+            return result_value
+    return inner
+
+
+@cache
+def long_time_func(num1: int, num2: int, num3: int) -> int:
+    return (num1 ** num2 ** num3) % (num1 * num3)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[int]:
+    return [number ** power for number in n_tuple]
