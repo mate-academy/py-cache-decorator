28a1d7db9a5e6add7841583f97a86e15181d2bef
Andrii lazoryk <73848849+12-deadblue@users.noreply.github.com>
2023-05-30 14:35:08 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-def cache(func: callable) -> callable:
+ def cache(func: callable) -> callable:
     results = {}
 
     def inner(*args, **kwargs) -> callable:
         key = args + tuple(sorted(kwargs.items()))
         if key in results:
             print("Getting from cache")
-            return results[key]
         else:
             print("Calculating new result")
-            results[key] =  func(*args, **kwargs)
-            return  func(*args, **kwargs)
-
+            res = func(*args, **kwargs)
+            results[key] = res
+        return results[key]
     return inner
