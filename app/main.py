0d355393375628dbf2f6cf958b367dea5dcbc50f
Andrii <anrij.lazoryk@gmail.com>
2023-05-30 17:41:04 +0300

    solution4

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ def cache(func: callable) -> callable:
     results = {}
 
     def inner(*args) -> callable:
-        key = args
-        if key in results:
+
+        if args in results:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            results[key] = func(*args)
-        return results[key]
+            results[args] = func(*args)
+        return results[args]
     return inner
