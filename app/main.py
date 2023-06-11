c71053fddf6bff147db16a1b88936237d136bcfb
Dima Khrystenko <hrystanko.dmytro@outlook.com>
2023-06-11 16:51:30 +0200

    Correction for another solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> object:
         if args in data_collection.keys():
             print("Getting from cache")
-            return data_collection[args]
         else:
             print("Calculating new result")
             data_collection[args] = func(*args)
