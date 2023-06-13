a2ee5aa62fd0a4bd41d1df276554337fb94a97a2
ypyskun <eugene2piskun@gmail.com>
2023-06-13 19:17:43 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -4,15 +4,14 @@ from typing import Any, Callable
 def cache(func: Callable) -> Callable:
     cache_update = {}
 
-    def wrapper(*args) -> Any:
+    def wrapper(*args: Any) -> Any:
         if args in cache_update:
             print("Getting from cache")
-            return cache_update[args]
-
         else:
             result = func(*args)
             cache_update[args] = result
             print("Calculating new result")
-            return result
+
+        return cache_update[args]
 
     return wrapper
