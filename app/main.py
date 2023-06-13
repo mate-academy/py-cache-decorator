ebf9ad0845d7ef346afd7854b193bf3652820a13
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-13 14:46:04 +0300

    Completed cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+
+    store = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = args + tuple(kwargs.values())
+        if key in store:
+            print("Getting from cache")
+            return store[key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        store[key] = result
+        return result
+
+    return wrapper
