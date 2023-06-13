3a28193320046770a6627e3f2775faaf0b614beb
Kosta Stepanjuk <stepanjukkosta@qmail.com>
2023-06-13 17:52:29 +0300

    'Solution3_2'

--- app/main.py
+++ app/main.py
@@ -20,21 +20,3 @@ def cache(func: Callable) -> Callable:
         return cache_dic[key_cach]
 
     return inner
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
-
-
-@cache
-def long_time_func(num: int, num2: int, num3: int) -> int:
-    return (num ** num2 ** num3) % (num * num3)
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
