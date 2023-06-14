84089493c8d3b76d9aa01599ec209c34dfd3ac7a
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-06-15 02:22:52 +0300

    created cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    cash_dict = {}
+
+    def function_cacher(*args, **kwargs) -> Callable:
+        nonlocal cash_dict
+        args_kvargs_key = f"{args} {kwargs}"
+        if args_kvargs_key not in cash_dict:
+            print("Calculating new result")
+            cash_dict[args_kvargs_key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return cash_dict[args_kvargs_key]
+
+    return function_cacher
