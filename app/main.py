811d73f4bade07a873e2429e268755ca3a66c095
Maxym <maksim.pariychuk07@gmail.com>
2023-06-15 17:43:24 +0300

    Edited solution

--- app/main.py
+++ app/main.py
@@ -3,20 +3,17 @@ from typing import Callable, Any
 
 def cache(func: Callable) -> Callable:
     data = {}
-    result_history = {}
 
     def inner(*args: Any) -> Any:
         nonlocal data
-        nonlocal result_history
         param = args
         if param in data.keys():
             print("Getting from cache")
-            return result_history[param]
+            return data[param]
         else:
             print("Calculating new result")
             new_result = func(*args)
-            data[param] = param
-            result_history[param] = new_result
+            data[param] = new_result
             return new_result
 
     return inner
