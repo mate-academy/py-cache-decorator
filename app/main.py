36470dd9a6ebbaea51aa528810d39ec9177db3a2
Maxym <maksim.pariychuk07@gmail.com>
2023-06-15 17:21:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    data = {}
+    result_history = {}
+
+    def inner(*args: Any) -> Any:
+        nonlocal data
+        nonlocal result_history
+        param = args
+        if param in data.keys():
+            print("Getting from cache")
+            return result_history[param]
+        else:
+            print("Calculating new result")
+            new_result = func(*args)
+            data[param] = param
+            result_history[param] = new_result
+            return new_result
+
+    return inner
