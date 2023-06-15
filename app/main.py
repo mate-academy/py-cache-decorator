5db4a788e32aaae7ae70244d94611239eca9a832
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-06-15 13:30:53 +0300

    fixed calling arguments in the cache decorator

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
-from typing import Callable
+from typing import Any, Callable
 
 
 def cache(func: Callable) -> Callable:
     cash_dict = {}
 
-    def function_cacher(*args, **kwargs) -> Callable:
+    def function_cacher(*args) -> Any:
         nonlocal cash_dict
-        args_kvargs_key = f"{args} {kwargs}"
-        if args_kvargs_key not in cash_dict:
+        key = str(args)
+        if key not in cash_dict:
             print("Calculating new result")
-            cash_dict[args_kvargs_key] = func(*args, **kwargs)
+            cash_dict[key] = func(*args)
         else:
             print("Getting from cache")
-        return cash_dict[args_kvargs_key]
+        return cash_dict[key]
 
     return function_cacher
