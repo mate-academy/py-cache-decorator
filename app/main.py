d27fcbf366e9cc2622e8ba759b03182a69ccd380
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-06-18 00:11:19 +0300

    Cache_decorator_first

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> None:
+    list_of_answers = {}
+
+    def calc_or_cache(*args: int) -> int:
+        if args in list_of_answers.keys():
+            print("Getting from cache")
+            return list_of_answers[args]
+        else:
+            print("Calculating new result")
+            list_of_answers[args] = func(*args)
+            return list_of_answers[args]
+
+    return calc_or_cache
