4da9332949699590a58b6f2848934363069d566b
Serhii Osauliak <osaul1983@gmail.com>
2023-06-18 15:46:51 +0300

    Solution of py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    new_cache = {}
+
+    def wrapper(*args) -> Any:
+        if args in new_cache:
+            print("Getting from cache")
+            return new_cache[args]
+        new_cache[args] = func(*args)
+        print("Calculating new result")
+        return new_cache[args]
+    return wrapper
