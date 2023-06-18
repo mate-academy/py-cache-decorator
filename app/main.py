b8ff62619fb72a13bb7f1ac66b5e45dea64891c8
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-06-18 15:45:15 +0300

    Cache_decorator_second

--- app/main.py
+++ app/main.py
@@ -1,16 +1,14 @@
 from typing import Callable
 
 
-def cache(func: Callable) -> None:
+def cache(func: Callable) -> Callable:
     list_of_answers = {}
 
-    def calc_or_cache(*args: int) -> int:
+    def calc_or_cache(*args: tuple) -> dict:
         if args in list_of_answers.keys():
             print("Getting from cache")
-            return list_of_answers[args]
         else:
             print("Calculating new result")
             list_of_answers[args] = func(*args)
-            return list_of_answers[args]
-
+        return list_of_answers[args]
     return calc_or_cache
