b5fe005db9da8db82e20b45921104a7d548936f6
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-06-19 17:18:43 +0300

    Fixed code review comment

--- app/main.py
+++ app/main.py
@@ -7,13 +7,15 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(*args: not MutableSequence) -> Any:
+
         if args in _cache:
             print("Getting from cache")
-            return _cache[args]
+            func_result = _cache[args]
+        else:
+            func_result = func(*args)
+            _cache[args] = func_result
+            print("Calculating new result")
 
-        func_result = func(*args)
-        _cache[args] = func_result
-        print("Calculating new result")
         return func_result
 
     return wrapper
