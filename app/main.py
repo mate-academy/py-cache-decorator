5581bbbf068df4f66b63095878a07d80b8b084bd
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-06-20 16:47:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any, MutableSequence
+
+
+def cache(func: Callable) -> Callable:
+    cache_memory = {}
+
+    def wrapper(*args: not MutableSequence) -> Any:
+
+        if args in cache_memory:
+            print("Getting from cache")
+            result = cache_memory[args]
+        else:
+            result = func(*args)
+            cache_memory[args] = result
+            print("Calculating new result")
+
+        return result
+
+    return wrapper
