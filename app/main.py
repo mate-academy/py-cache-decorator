a69fb5b2121a1194ceb7798af4adc29323560a32
Vladyslav Shepilov <shepsterya@gmail.com>
2023-06-21 15:43:46 +0300

    resolved

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any, Hashable, Callable
+
+
+def cache(func: Callable) -> Callable:
+    cache_stored = dict()
+
+    def wrapper(*args: Hashable) -> Any:
+        if args in cache_stored:
+            print("Getting from cache")
+            return cache_stored[args]
+        cache_stored[args] = func(*args)
+        print("Calculating new result")
+        return cache_stored[args]
+
+    return wrapper
