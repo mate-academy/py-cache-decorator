4e7bd5045dae4ef1022e2012e80e9db400464cdb
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-06-25 12:28:30 +0300

    implemented cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+cache_dict = {}
+
+
+def cache(func: Callable) -> Callable:
+    def inner(*args) -> int:
+        if func not in cache_dict:
+            cache_dict[func] = {}
+        if args not in cache_dict[func]:
+            print("Calculating new result")
+            cache_dict[func][args] = func(*args)
+        else:
+            print("Getting from cache")
+
+        return cache_dict[func][args]
+
+    return inner
