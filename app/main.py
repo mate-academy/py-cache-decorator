f1e8a19f8941dbe2ae7752f758b4f8cbb67970f7
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-06-27 17:35:44 +0300

    updated cache decorator

--- app/main.py
+++ app/main.py
@@ -8,11 +8,9 @@ cache_dict = {}
 def cache(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args: Any) -> int:
-        if func not in cache_dict:
-            cache_dict[func] = {}
-        if args not in cache_dict[func]:
+        if func not in cache_dict or args not in cache_dict[func]:
             print("Calculating new result")
-            cache_dict[func][args] = func(*args)
+            cache_dict.setdefault(func, {})[args] = func(*args)
         else:
             print("Getting from cache")
 
