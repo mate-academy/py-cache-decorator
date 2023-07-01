468b83c0b7c86586ee949879367d102a95c2bda5
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-01 20:26:04 +0300

    rewrited function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 from functools import wraps
 
 
@@ -6,20 +6,12 @@ def cache(func: Callable) -> Callable:
     launch_cache = dict()
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> None:
-        key = str(args) + str(kwargs)
-        if func in launch_cache:
-            if key in launch_cache[func]:
-                print("Getting from cache")
-                return launch_cache[func][key]
-            else:
-                print("Calculating new result")
-                launch_cache[func][key] = func(*args, **kwargs)
-                return launch_cache[func][key]
-        else:
-            launch_cache[func] = dict()
-            print("Calculating new result")
-            launch_cache[func][key] = func(*args, **kwargs)
-            return launch_cache[func][key]
-
+    def wrapper(*args) -> Any:
+        if args in launch_cache:
+            print("Getting from cache")
+            return launch_cache[args]
+        print("Calculating new result")
+        result = func(*args)
+        launch_cache[args] = result
+        return result
     return wrapper
