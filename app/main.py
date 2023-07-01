3f883dc7dbb5423ea4ba6724fb666d354ba8b1d0
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-01 19:55:06 +0300

    add wraps

--- app/main.py
+++ app/main.py
@@ -12,3 +12,21 @@ def cache(func: Callable) -> Callable:
             cache_data[args] = func(*args)
         return cache_data[args]
     return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
+    return [number ** power for number in n_tuple]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
