1966b2595a26f2d50ee5a5fc4c3ee7b2560a3f82
Yuriy Makukh <106692775+makukh@users.noreply.github.com>
2023-07-03 01:07:35 +0300

    Update

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ def cache(func: Callable) -> Callable:
     results: dict = {}
 
     @wraps(func)
-    def wrapper(*args: int, float, str, tuple) -> dict:
+    def wrapper(*args: (int, float, str, tuple)) -> dict:
         if args in results:
             print("Getting from cache")
             return results[args]
-            
+        
         print("Calculating new result")
         result = func(*args)
         results[args] = result
