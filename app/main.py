0f6b919da5585b065a644f94ee3bbb1cf695f527
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-02 15:24:42 +0300

    add wraps fixed

--- app/main.py
+++ app/main.py
@@ -6,10 +6,11 @@ def cache(func: Callable) -> Callable:
     cache_data = {}
 
     @wraps(func)
-    def wrapper(*args: None) -> None:
+    def wrapper(*args: Callable) -> Callable:
         if args in cache_data:
             print("Getting from cache")
         else:
             print("Calculating new result")
             cache_data[args] = func(*args)
         return cache_data[args]
+    return wrapper
