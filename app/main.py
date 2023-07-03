85a2e07f01758f64a996e8e84bb098bada2efa9d
Manynik <s.onufriyenko@gmail.com>
2023-07-03 21:51:11 +0300

    New soution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     results = {}
 
-    def wrapper(*args) -> any:
+    @wraps(func)
+    def wrapper(*args) -> Any:
         if args in results:
             print("Getting from cache")
             return results[args]
         else:
-            print("Calculating new result")
             result = func(*args)
             results[args] = result
+            print("Calculating new result")
             return result
     return wrapper
