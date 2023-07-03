34d569bca3d76103380479db34c7b05c78961c39
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-07-03 11:45:14 +0300

    Implemented py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Any
+
+
+def cache(func) -> Any:
+    data_save = {}
+
+    def inner(*args: tuple[(int, float, tuple, str)]) -> Any:
+        nonlocal data_save
+        if args in data_save:
+            print("Getting from cache")
+            return data_save[args]
+        else:
+            print("Calculating new result")
+            data_save[args] = func(*args)
+            return data_save[args]
+    return inner
