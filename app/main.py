e4b6da2c2290477d15ea250a26421a56bf3a8b69
Alek Ilchenko <yaichenko87@gmail.com>
2023-07-04 18:21:37 +0100

    Solution passed tests

--- app/main.py
+++ app/main.py
@@ -1,24 +1,16 @@
 from typing import Callable
 
 
-result_args_dict = {}
-
-
 def cache(func: Callable) -> Callable:
+    result_args_dict = {}
+
     def inner(*args) -> None:
-        if func.__name__ in result_args_dict:
-            if args in result_args_dict[func.__name__]:
-                print("Getting from cache")
-                return result_args_dict[func.__name__][args]
-            else:
-                value = func(*args)
-                result_args_dict[func.__name__][args] = value
-                print("Calculating new result")
-                return value
+        if args in result_args_dict:
+            print("Getting from cache")
+            return result_args_dict[args]
         else:
             value = func(*args)
-            result_args_dict[func.__name__] = {}
-            result_args_dict[func.__name__][args] = value
+            result_args_dict[args] = value
             print("Calculating new result")
             return value
     return inner
