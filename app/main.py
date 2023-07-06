408a9e29523f4019adfe1b220235b2f844e41896
ve4nick <ve4nick98@gmail.com>
2023-07-06 18:20:49 +0300

    Remove 'else' and add @wraps decorator

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
+from functools import wraps
+
+
 def cache(func: callable) -> callable:
     storage = {}
 
+    @wraps(func)
     def inner(*args) -> None:
         if args not in storage:
             res = func(*args)
             storage.update({args: res})
             print("Calculating new result")
             return res
-        else:
-            print("Getting from cache")
-            return storage[args]
+
+        print("Getting from cache")
+        return storage[args]
 
     return inner
