afc418d10d7be15b4c24dcd813cbc666d4770686
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2023-07-11 11:21:00 +0300

    add @wraps and remove else

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     buffer = {}
 
+    @wraps(func)
     def wrapper(*args) -> Any:
         if args in buffer:
             print("Getting from cache")
             return buffer[args]
-        else:
-            print("Calculating new result")
-            res = func(*args)
-            buffer[args] = res
-            return res
+        print("Calculating new result")
+        res = func(*args)
+        buffer[args] = res
+        return res
 
     return wrapper
