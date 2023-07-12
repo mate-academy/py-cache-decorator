90d45cd6bae64f2758442d7005a67aec4c1d5ffe
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-07-12 16:08:49 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,14 +3,8 @@ def cache(func: None) -> None:
 
     def wrapper(*args: None) -> None:
         if args in cached_results:
-            print("Getting from cache"); return cached_results[args]
+            print("Getting from cache")
         else:
             print("Calculating new result")
             cached_results[args] = result = func(*args)
-            return cached_results
-
-
-
-
-
-    return wrapper
+        return cached_results
