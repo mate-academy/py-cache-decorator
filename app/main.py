4311a95516395dbdd7e2c87a166f9d88f1e09e2b
Ostap <ostapbojko04@gmail.com>
2023-07-13 21:17:38 +0300

    cache_decorator2

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 from functools import wraps
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict, Tuple, Hashable
 
 
 def cache(func: Callable[..., Any]) -> Callable[..., Any]:
     cache: Dict[Tuple, Any] = {}
 
     @wraps(func)
-    def wrapper(*args: Any) -> Any:
+    def wrapper(*args: Hashable) -> Any:
         if args in cache:
             print("Getting from cache")
             return cache[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            cache[args] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args)
+        cache[args] = result
+        return result
 
     return wrapper
