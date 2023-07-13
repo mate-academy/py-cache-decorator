cb87b6697c18443811c16d4e6cf83daa49596373
Ostap <ostapbojko04@gmail.com>
2023-07-13 18:52:50 +0300

    cache_decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Any, Callable, Dict, Tuple
+
+
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
+    cache: Dict[Tuple, Any] = {}
+
+    @wraps(func)
+    def wrapper(*args: Any) -> Any:
+        if args in cache:
+            print("Getting from cache")
+            return cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cache[args] = result
+            return result
+
+    return wrapper
