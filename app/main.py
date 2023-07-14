4f9522e21e0a1a8165b58543d48f889c26aa22bb
Anton Kozhukhovskiy <anton.kozhukhovskiy@g5e.com>
2023-07-15 00:05:20 +0300

    create cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cached_data_storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        nonlocal cached_data_storage
+        if cached_data_storage.get(args) is None:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached_data_storage[args] = result
+        else:
+            print("Getting from cache")
+        return cached_data_storage[args]
+    return wrapper
