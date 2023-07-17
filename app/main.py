d55977567fc9816c662c551f25041dbb84f3f7ef
Ruslan <rusipbox@gmail.com>
2023-07-17 17:58:33 +0300

    Test (py-cache-decorator)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args, **kwargs):
+        key = (tuple(args), tuple(kwargs.items()))
+
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            return result
+
+    return wrapper
