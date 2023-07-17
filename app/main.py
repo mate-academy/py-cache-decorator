f5295ce01130109c6f2132cb21bb8b69d9e9507a
Vladyslav Sabo <lebwa1337@gmail.com>
2023-07-17 22:02:13 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: tuple) -> str:
+    archive = {}
+
+    def inner(*args, **kwargs) -> None:
+        if args not in archive:
+            result = func(*args, **kwargs)
+            archive[args] = result
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return archive[args]
+    return inner
