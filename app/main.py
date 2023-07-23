cfcf6f06410a99f195918fe93292330f019a667b
niksgawrilenko <90038040+niksgawrilenko@users.noreply.github.com>
2023-07-23 18:23:52 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: None) -> None:
+    func_results = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        key = (args, frozenset(kwargs.items()))
+
+        if key in func_results:
+            print("Getting from cache")
+            return func_results[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            func_results[key] = result
+            return result
+
+    return wrapper
