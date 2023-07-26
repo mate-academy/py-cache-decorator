a5e4b49d8d33e640daa51af1df4a7a786d666b66
Nadiia <nadiia.developer777@gmail.com>
2023-07-26 16:28:18 +0300

    Solution-py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,29 @@
-def cache(func):
-    # Write your code here
-    pass
+import hashlib
+import json
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cache = {}
+
+    def compute_key(args: tuple, kwargs: dict) -> str:
+        key = hashlib.md5(
+            json.dumps(args, sort_keys=True).encode("utf-8")
+        ).hexdigest()
+        key += hashlib.md5(
+            json.dumps(kwargs, sort_keys=True).encode("utf-8")
+        ).hexdigest()
+        return key
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = compute_key(args, kwargs)
+        if key in cache:
+            print("Getting from cache")
+            return cache[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache[key] = result
+            return result
+
+    return wrapper
