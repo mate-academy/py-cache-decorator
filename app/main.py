2cb01cdc97786bc50d68bfa75ac32653a6ee8f8d
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-07-30 17:27:41 +0300

    Mission accomplished, attempt number 2

--- app/main.py
+++ app/main.py
@@ -4,15 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     results = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
-        key = str(args) + str(kwargs)
-        if key in results:
+    def wrapper(*args) -> Callable:
+        if str(args) in results:
             print("Getting from cache")
-            return results[key]
+            return results[str(args)]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            results[key] = result
+            result = func(*args)
+            results[str(args)] = result
             return result
 
     return wrapper
