a2b81f89a07f7aa726cb5f04ba3448db2f84d5fc
Bohdan Kolomiiets <kb907406@gmail.com>
2023-07-31 23:17:56 +0300

    Created decorator cache(updated)

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Any:
     memory = {}
 
-    def inner(*args) -> str:
+    @functools.wraps(func)
+    def inner(*args) -> Any:
         if args in memory:
             print("Getting from cache")
             return memory.get(args)
-        else:
-            res = func(*args)
-            memory[args] = res
-            print("Calculating new result")
-            return res
+        res = func(*args)
+        memory[args] = res
+        print("Calculating new result")
+        return res
     return inner
