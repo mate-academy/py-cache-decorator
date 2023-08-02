6baf4fa401f3a4ec7fd20f64d18d23623d6373d1
Illya Zhmurskiy <thebrawler6@gmail.com>
2023-08-02 14:15:50 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -9,20 +9,9 @@ def cache(func: Callable) -> Callable:
         if key in cached_results:
             print("Getting from cache")
             return cached_results[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_results[key] = result
-            return result
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cached_results[key] = result
+        return result
 
     return wrapper
-
-
-@cache
-def long_time_func(first_num: int, second_num: int, third_num: int) -> int:
-    return (first_num ** second_num ** third_num) % (first_num * third_num)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
