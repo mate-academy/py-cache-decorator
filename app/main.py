445a6f432dd33a4f69a7aa54ce2cbd3c1346b515
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-03 19:29:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    cached = []
+
+    def wrapper(*args, **kwargs) -> int:
+        cache_tuple = (*args, frozenset(kwargs.items()))
+        for cached_args, cached_result in cached:
+            if cached_args == cache_tuple:
+                print("Getting from cache")
+                return cached_result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cached.append((cache_tuple, result))
+        return result
+
+    return wrapper
