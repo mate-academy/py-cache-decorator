9fdfacd4f9e48884081889bce58da95aaed8a27b
anna-lybid <anna.lybid@gmail.com>
2023-08-03 20:10:23 +0300

    Implementation of py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    dict_cache = {}
+
+    def inner(*args, **kwargs) -> int:
+        key = (args, frozenset(kwargs.items()))
+        if key in dict_cache:
+            print("Getting from cache")
+            return dict_cache[key]
+        else:
+            result = func(*args, **kwargs)
+            dict_cache[key] = result
+            print("Calculating new result")
+            return result
+
+    return inner
