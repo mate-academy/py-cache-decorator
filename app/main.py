3e6c21d11f2531a5e6e1867d2557dff754aed965
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-08-04 18:07:20 +0300

    Implemented Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args):
+        if args not in cache_dict:
+            cache_dict.update({args: func(*args)})
+            print("Calculating new result")
+            return cache_dict[args]
+        else:
+            print("Getting from cache")
+            return cache_dict[args]
+
+    return wrapper
