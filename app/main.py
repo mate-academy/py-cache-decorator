92a3569700d86c0d7bf3f6ee2031c5e89ed402c3
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-08-04 21:50:00 +0300

    Used only one return. Added type annotations

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-def cache(func):
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args):
+    def wrapper(*args) -> Any:
         if args not in cache_dict:
             cache_dict.update({args: func(*args)})
             print("Calculating new result")
-            return cache_dict[args]
         else:
             print("Getting from cache")
-            return cache_dict[args]
+        return cache_dict[args]
 
     return wrapper
