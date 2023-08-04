6e73c901a1b96c1794177a1d33d2a03973dcbdb1
Ostap Ivakh <o.ivakh95@gmail.com>
2023-08-04 17:45:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,41 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    runs_cache = {}
+
+    def wrapper(*args) -> Callable:
+        if args not in runs_cache.keys():
+            runs_cache[args] = func(*args)
+            print("Calculating new result")
+            return runs_cache[args]
+        else:
+            print("Getting from cache")
+            return runs_cache[args]
+
+    return wrapper
+
+#
+# @cache
+# def long_time_func(a: int, b: int, c: int) -> int:
+#     return (a ** b ** c) % (a * c)
+#
+#
+# @cache
+# def long_time_func_2(n_tuple: tuple, power: int) -> int:
+#     return [number ** power for number in n_tuple]
+#
+#
+# long_time_func(1, 2, 3)
+# long_time_func(2, 2, 3)
+# long_time_func_2((5, 6, 7), 5)
+# long_time_func(1, 2, 3)
+# long_time_func_2((5, 6, 7), 10)
+# long_time_func_2((5, 6, 7), 10)
+#
+# # Calculating new result
+# # Calculating new result
+# # Calculating new result
+# # Getting from cache
+# # Calculating new result
+# # Getting from cache
