6bf84aa5d0e120d3a2cf24c00c62d10aac301665
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-05 16:56:27 +0200

    update

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any, Tuple, Dict
+
+
+def cache(func: Callable) -> Callable[[tuple[Any, ...], dict[str, Any]], Any]:
+    cached_results = {}
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
+        else:
+            result = func(*args, **kwargs)
+            cached_results[args] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
