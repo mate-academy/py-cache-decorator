6d5abdbee0e2a8e0df24ae83b86e1cbd37bb0582
Valeriia Danyliuk <bladeryz@gmail.com>
2023-08-06 17:15:25 +0300

    Imported "wraps" from functools and added it into the decorator

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
+    @wraps(func)
     def wrapper(*args) -> Callable:
         key = args
 
