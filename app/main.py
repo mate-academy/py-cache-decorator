3dce53cc823c0896ac4a3905825ee2ec664e7c4c
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-08-06 22:53:34 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> None:
+    results = {}
+
+    def inner(*args) -> Callable:
+        if args in results:
+            print("Getting from cache")
+            return results[args]
+
+        not_repeat = func(*args)
+        results[args] = not_repeat
+        print("Calculating new result")
+        return not_repeat
+
+    return inner
