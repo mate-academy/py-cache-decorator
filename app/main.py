1d84e1a6e5454f241fd460598842ebbc8413f5ec
Davydova Olha <o.chookity@gmail.com>
2023-08-09 21:43:02 +0300

    Add decorator wrapper from functors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cached = dict()
 
+    @wraps(func)
     def wrapped(*args) -> Any:
         if args in cached:
             print("Getting from cache")
