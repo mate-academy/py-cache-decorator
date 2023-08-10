5b1b6938adc55fb8fd7997c4885c702df567ae67
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-08-10 14:35:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+
+
+def cache(func: Callable) -> Callable:
+    caching_place = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        arguments = args + tuple(sorted(kwargs.items()))
+        if arguments in caching_place:
+            print("Getting from cache")
+            return caching_place[arguments]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            caching_place[arguments] = result
+            return result
+    return wrapper
