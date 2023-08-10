796e924f6a80e97575d3df91b923af1c63fba583
oreny <orenycdota@gmail.com>
2023-08-10 16:20:44 +0300

    Removed nonlocal, removed else, changed hash_key getting

--- app/main.py
+++ app/main.py
@@ -5,15 +5,13 @@ def cache(func: Callable) -> Callable:
     previous_runs = {}
 
     def inner(*args) -> object:
-        nonlocal previous_runs
-        hash_key = str(func.__name__) + str(args)
+        hash_key = args
         if hash_key in previous_runs.keys():
             print("Getting from cache")
             return previous_runs[hash_key]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            previous_runs[hash_key] = result
+        print("Calculating new result")
+        result = func(*args)
+        previous_runs[hash_key] = result
         return result
 
     return inner
