afc733d9aa001bdd1b66d77338d313e0f505dc38
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-11 17:14:01 +0300

    using dict for cached results

--- app/main.py
+++ app/main.py
@@ -2,18 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cached = []
+    cached = dict()
 
     def wrapper(*args, **kwargs) -> int:
         cache_tuple = (*args, frozenset(kwargs.items()))
-        for cached_args, cached_result in cached:
+        for cached_args, cached_result in cached.items():
             if cached_args == cache_tuple:
                 print("Getting from cache")
                 return cached_result
 
         print("Calculating new result")
         result = func(*args, **kwargs)
-        cached.append((cache_tuple, result))
+        cached[cache_tuple] = result
         return result
 
     return wrapper
