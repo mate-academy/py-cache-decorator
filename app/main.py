f6ab050af19ef18b72dad1c4b10717807c4d7bad
Denys Kibalenko <dennieking87@gmail.com>
2023-08-13 19:21:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cache_data = dict()
+
+    def wrapper(*args: Any) -> int | str:
+        for argument in args:
+            if isinstance(argument, (list, set, dict)):
+                return "Arguments must be immutable type."
+        if args not in cache_data:
+            cache_data[args] = func(*args)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return cache_data[args]
+    return wrapper
