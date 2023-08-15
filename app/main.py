c9b4e363e22f13b15ae1aa3f38526f09eec5967a
Nikolay Krasulin <krasulinphotograph@example.com>
2023-08-15 11:49:13 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
 def cache(func):
-    # Write your code here
-    pass
+    cached_results = {}
+
+    def wrapper(*args):
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
+        res = func(*args)
+        cached_results[args] = res
+        print("Calculating new result")
+        return res
+
+    return wrapper
