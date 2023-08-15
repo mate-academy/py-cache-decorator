94ba70c5defd542bed67810a900dc874ac2fca3d
Liftoff <vladislav70707@gmail.com>
2023-08-15 16:47:06 +0200

    'cache_decorator(15.08.23)'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+
+    saved_cache = {}
+
+    def wrapper(*args) -> Any:
+
+        if args in saved_cache:
+            print("Getting from cache")
+            return saved_cache[args]
+
+        print("Calculating new result")
+        result = func(*args)
+        saved_cache[args] = result
+        return result
+
+    return wrapper
