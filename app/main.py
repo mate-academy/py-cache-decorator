a6d9ad0cbd8d20a4abc246c1f37de25b2e50c962
Yaroslav Yarovyi <yarovoyyaroslav21@gmail.com>
2023-08-21 14:41:52 +0300

    decorator is developed

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 def cache(func):
-    # Write your code here
-    pass
+    cache_dict = {}
+    def inner(*args, **kwargs):
+        if (args, tuple(kwargs.items())) in cache_dict:
+            print("Getting from cache")
+            return cache_dict[(args, tuple(kwargs.items()))]
+        else:
+            result = func(*args, **kwargs)
+            print("Calculating new result")
+            cache_dict[(args, tuple(kwargs.items()))] = result
+            return result
+    return inner
