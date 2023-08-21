8de1bbaf9d9f41c8149c254df9bf0d7608e47e27
Yaroslav Yarovyi <yarovoyyaroslav21@gmail.com>
2023-08-21 14:50:26 +0300

    decorator is developed + cleaned up code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-def cache(func):
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
     cache_dict = {}
-    def inner(*args, **kwargs):
+
+    def inner(*args, **kwargs) -> Any:
         if (args, tuple(kwargs.items())) in cache_dict:
             print("Getting from cache")
             return cache_dict[(args, tuple(kwargs.items()))]
