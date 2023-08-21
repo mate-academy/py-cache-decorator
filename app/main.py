60f70f89ea364dd686131eeafdb33b797f7755cb
Oleksandr Ilchenko <oleksandrilchenko@Air-Oleksandr.lan>
2023-08-21 16:46:24 +0100

    Update 2

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> Callable:
         if args in result_args_dict:
             print("Getting from cache")
-            return result_args_dict[args]
         else:
             result_args_dict[args] = func(*args)
             print("Calculating new result")
             return result_args_dict[args]
+        return result_args_dict[args]
     return inner
