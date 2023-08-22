69fbc62fbcd3830738df224fe609302b24b00a8f
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-22 13:51:36 +0300

    Cache_decorator

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     dict_of_args = {}
 
-    def inner(*args, **kwargs) -> dict:
-        if args not in dict_of_args.keys():
+    def inner(*args) -> dict:
+        if args not in dict_of_args:
             dict_of_args[args] = func(*args)
             print("Calculating new result")
             return dict_of_args[args]
-        else:
-            print("Getting from cache")
-            return dict_of_args[args]
+        print("Getting from cache")
+        return dict_of_args[args]
 
     return inner
