af6c842fd9d530069e737bbee77e61ea90d5df58
Serhii Loboda <serhiy.loboda@gmail.com>
2023-08-22 12:13:36 +0300

    Solution: Cache Decorator.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cash_dict = {}
+
+    def wrapper(*args) -> Any:
+        if args in cash_dict:
+            print("Getting from cache")
+            result = cash_dict[args]
+            return result
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cash_dict[args] = result
+            return result
+    return wrapper
