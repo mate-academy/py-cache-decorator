7044b6e577ae2aa972ed1da1c7a004e4c32495c7
Olga Bordiug <obordiug@gmail.com>
2023-08-24 13:22:54 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    results_cache = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(kwargs.items()))
+
+        if key in results_cache:
+            print("Getting from cache")
+            return results_cache[key]
+        else:
+            result = func(*args, **kwargs)
+            results_cache[key] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
