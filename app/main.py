9b6ecb139113ca017bdc0318b9b03cff44226060
TROT <seotrotpol@gmail.com>
2023-08-27 11:22:36 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+import functools
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    func_dict = {}
+
+    @functools.wraps(func)
+    def wrapper(*args) -> Any:
+        if args in func_dict:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            func_dict[args] = func(*args)
+        return func_dict[args]
+
+    return wrapper
