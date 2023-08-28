4f7c4188c4eeda9219a0aa63115ee4c4d3c7c277
Ihor Nahnybida <79282749+ihor-jpeg@users.noreply.github.com>
2023-08-28 14:49:27 +0300

    Update README.md

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    agrums_and_results_dict = {}
+
+    def wpapper_func(*args) -> Any:
+        if args in agrums_and_results_dict.keys():
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            agrums_and_results_dict[args] = func(*args)
+        return agrums_and_results_dict[args]
+    return wpapper_func
+
+
+# @cache
+# def long_time_func(a: int, b: int, c: int) -> int:
+#     return (a ** b ** c) % (a * c)
