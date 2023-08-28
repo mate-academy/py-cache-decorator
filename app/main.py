c436360c5f2526e8ba53c4bab0afd8e7b14df146
Elena Komarova <elenkomarova4@gmail.com>
2023-08-28 11:24:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from functools import wraps
+from typing import Any, Tuple, Dict, Callable
+
+
+def cache(func: Callable) -> Callable:
+    results: Dict[Tuple[Any, ...], Any] = {}
+
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        arguments = args + tuple(kwargs.items())
+        if arguments in results:
+            print("Getting from cache")
+            return results[arguments]
+        else:
+            result = func(*args, **kwargs)
+            results[arguments] = result
+            print("Calculating new result")
+            return result
+    return wrapper
