55fb9e242061dbc804f3bf55b613fe2d145a24c3
Roman Shupak <romanshupak808@gmail.com>
2023-08-29 11:41:45 +0300

    Removed all unnecessary lines

--- app/main.py
+++ app/main.py
@@ -12,13 +12,3 @@ def cache(func: Callable) -> Callable:
             cache_results[args] = func(*args, **kwargs)
         return cache_results[args]
     return wrapper
-
-
-@cache
-def long_time_func(number_1: int, number_2: int, number_3: int) -> int:
-    return (number_1 ** number_2 ** number_3) % (number_1 * number_3)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
