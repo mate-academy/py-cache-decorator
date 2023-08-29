2a8204d2ba3f038775e807e15b1fd7f0bb1e3de2
Elena Komarova <elenkomarova4@gmail.com>
2023-08-29 18:56:45 +0300

    'Solution_5'

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from typing import Any, Tuple, Dict, Callable
 
 
 def cache(func: Callable) -> Callable:
-    results: Dict[Tuple[Any, ...], Any] = {}
+    results: \
+        Dict[Tuple[Any, ...], Any] = {}
 
     @wraps(func)
-
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         arguments = args + tuple(kwargs.items())
         if arguments in results:
