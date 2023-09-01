3b3b75aafd9e793dedb7110b71ada77fd20900c9
VladKrit-commits <kritvladislav98@gmail.com>
2023-09-01 22:09:45 +0300

    Solution changed

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def inner(*args) -> int:
+    def inner(*args: int) -> int:
         if args in cache_dict:
             print("Getting from cache")
         else:
