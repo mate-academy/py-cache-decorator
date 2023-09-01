ee6f3845becbf26d31551b3656be8ebd623b99c5
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-09-01 15:04:12 +0300

    Very hard task perfect solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    cached = []
+    arg_cache = []
+    kwargs_cache = []
+
+    def wrapper(*args, **kwargs) -> Any:
+        nonlocal cached, arg_cache, kwargs_cache
+
+        if cached is None or args not in arg_cache or \
+                kwargs not in kwargs_cache:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached.append(result)
+            arg_cache.append(args)
+            kwargs_cache.append(kwargs)
+
+            return result
+
+        else:
+            print("Getting from cache")
+
+        return cached[arg_cache.index(args)]
+
+    return wrapper
