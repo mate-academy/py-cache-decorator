ec599d0ec3f4e2aef394301bd4bed97935f5e849
Marina <marina.ua13@gmail.com>
2023-09-02 18:36:18 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,15 +7,14 @@ def cache(func: Callable) -> Callable:
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
-        key = (args, frozenset(kwargs.items()))
+        # key = (args, frozenset(kwargs.items()))
 
-        if key in cache_result:
+        if args in cache_result:
             print("Getting from cache")
-            return cache_result[key]
+            return cache_result[args]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_result[key] = result
-            return result
+            cache_result[args] = func(*args, **kwargs)
+            return cache_result[args]
 
     return wrapper
