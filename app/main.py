818ccb46dea74e6b437f5c46cad3f632b22f8540
Mariia Movchan <mariia.sergeevna.k@gmail.com>
2023-09-04 20:01:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
-def cache(func):
-    # Write your code here
-    pass
+import functools
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Callable:
+    repeating_data = {}
+
+    @functools.wraps(func)
+    def inner(*args) -> Any:
+        if (args,) in repeating_data:
+            print("Getting from cache")
+            return repeating_data[(args,)]
+        print("Calculating new result")
+        repeating_data[(args,)] = func(*args)
+        return repeating_data[(args,)]
+    return inner
