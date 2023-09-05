9d3e5fd551b0973c186abfb906c16d335af582d3
Igor Shapoval <igorshapoval1992@gmail.com>
2023-09-05 20:14:06 +0300

    Edit decorator task

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
+import functools
 from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
+    @functools.wraps(func)
     def wrapper(*args) -> Any:
-        if args not in cache_storage.keys():
-            print("Calculating new result")
-            cache_storage[*args] = func(*args)
-        else:
+        if args in cache_storage:
             print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cache_storage[args] = func(*args)
+        return cache_storage[args]
 
-        return cache_storage[*args]
     return wrapper
