204a8b17a5c9bd8da19935145ed68a3fee536865
Illya Maznitskiy <comercaleuros@gmail.com>
2023-09-07 18:21:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from functools import wraps
 
 
 def cache(func: callable) -> callable:
-    dictionary = {}
+    inner_cache = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> any :
-        if args in dictionary:
+    def wrapper(*args, **kwargs) -> any:
+        if args in inner_cache:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            dictionary[*args] = func(*args)
-        return dictionary[*args]
+            inner_cache[args] = func(*args)
+        return inner_cache[args]
     return wrapper
