ae4f1c8f94e97ab59506d2d611f6592e0f54a021
Anton <antonrozegan1@gmail.com>
2023-09-07 16:13:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: str) -> any:
+    cache = {}
+
+    def wrapper(*args, **kwargs) -> any:
+        args_key = tuple(args)
+        kwargs_key = tuple(kwargs.items())
+        key = (args_key, kwargs_key)
+
+        if key in cache:
+            return cache[key]
+        else:
+            result = func(*args, **kwargs)
+            cache[key] = result
+            return result
+    return wrapper
