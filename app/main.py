fe17b4f470104d712e7aa4e37e5ae61cc13c9a0d
Anton <antonrozegan1@gmail.com>
2023-09-07 19:02:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: str) -> any:
+    cache_decorator = {}
+
+    def wrapper(*args, **kwargs) -> any:
+        args_key = tuple(args)
+        kwargs_key = tuple(kwargs.items())
+        key = (args_key, kwargs_key)
+
+        if key in cache_decorator:
+            print("Getting from cache")
+            return cache_decorator[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_decorator[key] = result
+            return result
+    return wrapper
