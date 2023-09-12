408e8c8f2925c3ff66b31698bb4034b0ede0ec47
Oksana Kozlova <brunhildaaaaa@gmail.com>
2023-09-12 09:16:58 +0300

    'My_solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Any
+
+
+def cache(func: Callable) -> Any:
+    results_long_function = dict()
+
+    def inner(*args, **kwargs) -> Any:
+        args_key = args
+        kwargs_key = frozenset(kwargs.items())
+
+        if (args_key, kwargs_key) not in results_long_function:
+            print("Calculating new result")
+            results_long_function[args_key, kwargs_key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+
+        return results_long_function[args_key, kwargs_key]
+    return inner
