634266940bb40a17e1acbc6101bb6cc71f06f4f2
Many <many.fasy@gmail.com>
2023-09-12 17:16:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable
+result_cache = {}
+
+
+def cache(func: Callable) -> Callable:
+    def new_result(*args, **kwargs) -> None:
+        name_cache = func, args
+        if name_cache in result_cache:
+            print("Getting from cache")
+            return result_cache[name_cache]
+
+        else:
+            result = func(*args, **kwargs)
+            result_cache[name_cache] = result
+            print("Calculating new result")
+            return result
+    return new_result
