f847c12bd81536b2a5310c7ce0beb3a54086153d
Damodar <alaso.se@gmail.com>
2023-09-21 20:53:08 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from typing import Callable
 
+results_list = []
+
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args) -> Callable:
+        global results_list
+        for result in results_list:
+            if (result["arguments"] == [*args]
+                    and result["id_func"] == id(func)):
+                print("Getting from cache")
+                return result["result_func"]
+        results_list += [
+            {
+                "arguments": [*args],
+                "result_func": func(*args),
+                "id_func": id(func)
+            }
+        ]
+        print("Calculating new result")
+        return results_list[-1].get("result_func")
+    return inner
