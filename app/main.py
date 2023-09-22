e8f86dab4067b03fcc2da712d0f3c355f5d244de
Anton Migunov <migunov1au@gmail.com>
2023-09-22 19:55:45 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def cache(func):
-    # Write your code here
-    pass
+from typing import Callable, Union
+
+
+def cache(func: Callable) -> Callable:
+    cache_storage = {}
+
+    def inner(*args) -> Union[int, float, str, tuple, bool]:
+        if func not in cache_storage:
+            cache_storage[func] = {}
+
+        func_storage = cache_storage[func]
+        if args not in func_storage:
+            result = func(*args)
+            func_storage.update({args: result})
+            print("Calculating new result")
+            return result
+        print("Getting from cache")
+        return func_storage[args]
+    return inner
