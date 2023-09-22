c8dcf2d7cea84bd0dd0965b5b58cd116b0df073f
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-09-22 15:57:51 +0300

    Cache decorator solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,24 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    caches = {}
+
+    def cache_checker(*args) -> Callable:
+        if args in caches:
+            print("Getting from cache")
+        else:
+            caches[args] = func(*args)
+            print("Calculating new result")
+        return caches[args]
+
+    return cache_checker
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+long_time_func(1, 2, 3)
+long_time_func(1, 2, 3.2)
+long_time_func(1, 2, 3)
