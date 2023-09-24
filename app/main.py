be1990d66379b4c73753430e784f24be5c286520
MykMazh <mazhm3ua@gmail.com>
2023-09-24 22:51:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    _cache = {}
+
+    def decorator(*args, **kwargs) -> Callable:
+        if args in _cache:
+            print("Getting from cache")
+            return _cache[args]
+        else:
+            res = func(*args)
+            _cache[args] = res
+            print("Calculating new result")
+            return res
+    return decorator
