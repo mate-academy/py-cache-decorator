c68d4de6c31d682d0af97950a8e791319651984a
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-09-24 15:29:35 +0300

    Update main.py
    my commit

--- app/main.py
+++ app/main.py
@@ -10,10 +10,3 @@ def cache(func: Callable) -> Callable:
         if cache_key in cache_dict:
             print("Getting from cache")
             return cache_dict[cache_key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[cache_key] = result
-            return result
-
-    return wrapper
