eb11789cc3b40a21c1a258fce5fbd1f74a445967
Serhii Panchenko <serji13@gmail.com>
2023-09-24 13:53:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,8 +8,7 @@ def cache(func: Callable) -> Callable:
         if args in archive:
             print("Getting from cache")
             return archive.get(args)
-        else:
-            archive[args] = func(*args)
-            print("Calculating new result")
-            return archive[args]
+        archive[args] = func(*args)
+        print("Calculating new result")
+        return archive[args]
     return inner
