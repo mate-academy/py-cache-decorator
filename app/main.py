f0df515bb972325ed32d64bb2dc07a8079bb5e5b
Yuliia Valko <yuliia.valko1@gmail.com>
2023-09-25 18:21:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    archive_functions = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (func, *args)
+        if key in archive_functions:
+            print("Getting from cache")
+        else:
+            archive_functions[key] = func(*args, **kwargs)
+            print("Calculating new result")
+        return archive_functions[key]
+    return wrapper
