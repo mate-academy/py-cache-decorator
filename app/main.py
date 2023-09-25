8ecfc26c6cb5cf08d851a2c7e4b52900eb177866
Viesich <v.s.vesich@gmail.com>
2023-09-25 15:31:04 -0700

    decorator of completed runs

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def inner(*args, **kwargs) -> dict:
+        key = (args, tuple(kwargs.items()))
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            print("Calculating new result")
+            return result
+    return inner
