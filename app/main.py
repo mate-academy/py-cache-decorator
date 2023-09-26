075159015dd15fe60928c344c7d9b17c9e917cfb
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-09-26 18:47:52 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,14 +6,12 @@ def cache(func: Callable) -> Callable:
     cached_results = {}
 
     def wrapper(*args) -> Any:
-        key = args
-        if key in cached_results:
+        if args in cached_results:
             print("Getting from cache")
-            return cached_results[key]
-        else:
-            result = func(*args)
-            cached_results[key] = result
-            print("Calculating new result")
-            return result
+            return cached_results[args]
+        result = func(*args)
+        cached_results[args] = result
+        print("Calculating new result")
+        return result
 
     return wrapper
