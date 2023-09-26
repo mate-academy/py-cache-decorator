495701b86be3476b2a86da7ec48bd0753f21921f
Dmytro Pyrih <dima.purig@gmail.com>
2023-09-26 20:35:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_info = {}
+
+    def inner(*args, **kwargs) -> int:
+        # Щоб унеможливити зміну ключа кешу, довелося перетворити
+        # ключові аргументи у tuple
+        set_key = (args, tuple(sorted(kwargs.items())))
+        if set_key in cached_info:
+            print("Getting from cache")
+            return cached_info[set_key]
+        result = func(*args, **kwargs)
+        cached_info[set_key] = result
+        print("Calculating new result")
+        return result
+    return inner
