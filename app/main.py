d1c4c101eac51fae67266a67bce3b5c4734dd9ab
Lytillis <chemurchem@gmail.com>
2023-09-28 16:27:12 +0300

    Cache decorator solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,13 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_runs = {}
+
+    def wrapper(*args):
+        if args in stored_runs:
+            print("Getting from cache")
+            return stored_runs[args]
+        print("Calculating new result")
+        stored_runs.update({args: func(*args)})
+        return stored_runs[args]
+    return wrapper
