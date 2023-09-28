37589ae1f8d430014ac960f5ce1adc1a1011b37f
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-09-28 19:43:38 +0300

    Removed else and cash_key

--- app/main.py
+++ app/main.py
@@ -5,14 +5,12 @@ def cache(func: Callable) -> Callable:
     caching_results = {}
 
     def wrapper(*args) -> Any:
-        cach_key = args
-        if cach_key in caching_results.keys():
+        if args in caching_results.keys():
             print("Getting from cache")
             return caching_results[args]
-        else:
-            print("Calculating new result")
-            func_result = func(*args)
-            caching_results[args] = func_result
-            return func_result
+        print("Calculating new result")
+        func_result = func(*args)
+        caching_results[args] = func_result
+        return func_result
 
     return wrapper
