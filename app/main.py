521080e64da5b641d5efe10c63c9b36007829faf
Mykola Kokoiev <veokoknik@gmail.com>
2023-09-29 10:39:17 +0200

    SOL

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    database = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        if f"{args}, {kwargs}" in database.keys():
+            print("Getting from cache")
+            return database[f"{args}, {kwargs}"]
+        else:
+            print("Calculating new result")
+            database[f"{args}, {kwargs}"] = func(*args, **kwargs)
+            return database[f"{args}, {kwargs}"]
+    return wrapper
