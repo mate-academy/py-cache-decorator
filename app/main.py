27bbc9606c028fcc76decdd6dd937920f60caaf2
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-09-29 16:22:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ def cache(func: Callable) -> Callable:
     cached_data = {}
 
     def inner(*args, **kwargs):
-        #print(cached_data)
         if args in cached_data.keys():
             print("Getting from cache")
         else:
-            new_cach = func(*args)
-            cached_data[args] = new_cach
+            new_cache = func(*args)
+            cached_data[args] = new_cache
             print("Calculating new result")
 
         return func(*args) if args not in cached_data.keys() else cached_data[args]
