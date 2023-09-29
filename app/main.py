1164a84456b301ca8f05e030e7042e93b59fe142
Severyn Krok <sevkrok@gmail.com>
2023-09-29 21:00:04 +0300

    v_2.1

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> int:
         if args in cached_results:
             print("Getting from cache")
-        else:
-            cached_results[args] = func(*args)
-            print("Calculating new result")
+            return cached_results[args]
+        cached_results[args] = func(*args)
+        print("Calculating new result")
         return cached_results[args]
 
     return wrapper
