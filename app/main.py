bf55fcf7fc63510719c186af30b538c1503ce445
naiv <nazarivankiv1@gmail.com>
2023-09-29 13:09:23 +0300

    push repository from local

--- app/main.py
+++ app/main.py
@@ -2,16 +2,5 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-
-    cached_results_storage = {}
-
-    def inner(*args, **kvargs) -> int:
-        if args in cached_results_storage:
-            print("Getting from cache")
-            return cached_results_storage.get(args)
-        else:
-            print("Calculating new result")
-            result = func(*args, **kvargs)
-            cached_results_storage[args] = result
-            return result
-    return inner
+    # Write your code here
+    pass
