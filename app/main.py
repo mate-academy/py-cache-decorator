bc7308a3c0065c755002c87ced31dd89860aeb6e
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-09-29 16:33:25 +0300

    Solution]

--- app/main.py
+++ app/main.py
@@ -15,20 +15,3 @@ def cache(func: Callable) -> Callable:
         return func(*args) if args not in cached_data.keys() else cached_data[args]
 
     return inner
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
