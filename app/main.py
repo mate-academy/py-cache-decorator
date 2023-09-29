c234964f77bda40ea5d5b5513c3ca2bfb77c5908
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-09-29 23:49:33 +0300

    'Improving'

--- app/main.py
+++ app/main.py
@@ -4,17 +4,15 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def inner(*args, **kwargs) -> None:
-        key = args
+    def inner(*args, **kwargs) -> [list, int]:
 
-        if key in cache_dict:
+        if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
+            return cache_dict[args]
 
-            return result
+        print("Calculating new result")
+        cache_dict[args] = func(*args, **kwargs)
+
+        return cache_dict[args]
 
     return inner
