fefb61d0c272bbebd5b9e4031c7bd41696390506
Kostia <kostia3507@gmail.com>
2023-09-29 18:07:19 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     stored_values = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
-        if args in stored_values.keys():
+    def wrapper(*args) -> int | list:
+
+        if args in stored_values:
             print("Getting from cache")
-            return stored_values[args]
         else:
             print("Calculating new result")
-            result_of_function = func(*args)
-            stored_values[args] = result_of_function
-            return result_of_function
+            stored_values[args] = func(*args)
+
+        return stored_values[args]
 
     return wrapper
