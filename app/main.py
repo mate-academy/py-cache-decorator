f6f87c4f72575efe0f2c8fd3b3430ace6361c9f3
Denys <den.prislipskyi@gmail.com>
2023-09-30 09:44:06 +0200

    fix else and add type annotation Any

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args: Callable) -> Callable:
+    def wrapper(*args) -> Any:
         if args not in cache_dict:
             cache_dict[args] = func(*args)
             print("Calculating new result")
-        else:
-            print("Getting from cache")
+            return cache_dict[args]
+        print("Getting from cache")
         return cache_dict[args]
 
     return wrapper
