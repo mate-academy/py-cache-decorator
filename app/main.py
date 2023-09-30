7a4b857a0765490599dae89bbe1a55406e43f973
Rost <rv4@ukr.net>
2023-09-30 18:23:12 +0300

    create ceche decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    def wrapper(*args) -> Any:
+        nonlocal cache_storage
+        if args in cache_storage:
+            print("Getting from cache")
+            return cache_storage[args]
+        else:
+            print("Calculating new result")
+            cache_storage[args] = func(*args)
+            return cache_storage[args]
+    return wrapper
