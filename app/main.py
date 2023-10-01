7d8a34b15e5d8cf8a14e4ca0f5a476d71d336d01
Danylo Sheremeta <danylo3002@gmail.com>
2023-10-01 16:09:33 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        nonlocal cache_data
+        name = func.__name__
+
+        if name in cache_data:
+            if args in cache_data[name]:
+                print("Getting from cache")
+                return cache_data[name][args]
+            else:
+                result = func(*args)
+                cache_data[name][args] = result
+                print("Calculating new result")
+                return result
+        else:
+            result = func(*args)
+            cache_data[name] = {args: result}
+            print("Calculating new result")
+            return result
+    return wrapper
