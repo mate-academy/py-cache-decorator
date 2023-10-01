769c4e27ab9bad7cfd05fa7e6c764825486a33ad
Eugene Rabenko <qwer09846@gmail.com>
2023-10-01 13:14:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_dictionary = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        args_key = (args, tuple(kwargs.items()))
+        if args_key in cash_dictionary:
+            print("Getting from cache")
+            return cash_dictionary[args_key]
+        else:
+            result = func(*args, **kwargs)
+            cash_dictionary[args_key] = result
+            print("Calculating new result")
+            return result
+    return wrapper
