6a6310857d6881a07194175a80868255d2c89938
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-10-01 15:02:01 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results_of_function_runs = {}
+
+    def inner(*args) -> Any:
+        if args not in results_of_function_runs:
+            result = func(*args)
+            results_of_function_runs[args] = result
+            print("Calculating new result")
+            return result
+        else:
+            print("Getting from cache")
+            return results_of_function_runs[args]
+    return inner
