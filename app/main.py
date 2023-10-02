5af7fee5a53705475ab9a064bccffc15d7cfe337
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-10-02 15:31:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    arh_cache = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        if args in arh_cache.keys():
+            print("Getting from cache")
+            return arh_cache[args]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            arh_cache[args] = result
+            return result
+    return inner
