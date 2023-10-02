20e527b40de39b269243df9421e30a327fc86408
Dmytro Petrykiv <111526221+petrykivd@users.noreply.github.com>
2023-10-02 18:01:36 +0300

    Fixed type annotation [int -> Any]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args) -> int:
+    def wrapper(*args) -> Any:
         if args in cache_dict:
             print("Getting from cache")
         else:
