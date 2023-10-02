8def2493fd1a6c42a7f89ac55a4a4cdcadd2fc4f
Alex Moon <moon.ra.lex@gmail.com>
2023-10-02 08:17:40 +0200

    Removed redundant code.

--- app/main.py
+++ app/main.py
@@ -3,21 +3,15 @@ from typing import Callable, Any
 
 def cache(func: Callable) -> Callable:
     cached_data = {}
-    immutable_type = (int, float, str, tuple, bool, type(None))
 
     def argument_based_cacher(*args) -> Any:
-        nonlocal cached_data
-        if all([isinstance(variable, immutable_type) for variable in args]):
-            func_key = tuple(args)
-            if func.__name__ not in cached_data.keys():
-                cached_data[func.__name__] = {}
-            if func_key in cached_data[func.__name__]:
-                print("Getting from cache")
-                return cached_data[func.__name__][func_key]
-            print("Calculating new result")
-            func_result = func(*args)
-            cached_data[func.__name__][func_key] = func_result
-            return func_result
+        if func.__name__ not in cached_data.keys():
+            cached_data[func.__name__] = {}
+        if args in cached_data[func.__name__]:
+            print("Getting from cache")
+            return cached_data[func.__name__][args]
         print("Calculating new result")
-        return func(*args)
+        func_result = func(*args)
+        cached_data[func.__name__][args] = func_result
+        return func_result
     return argument_based_cacher
