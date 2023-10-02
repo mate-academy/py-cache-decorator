61c0b5dd88f81ef60ea8b6e07c32ac8369778806
sakhaline <alexandra.ivva@gmail.com>
2023-10-02 19:23:47 +0300

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,19 @@
 from typing import Callable
-import functools
 
 
 def cache(func: Callable) -> Callable:
-    @functools.wraps(func)
-    def wraper(*args, **kwargs) -> int:
-        cache_key = func.__name__, args, tuple(kwargs.items())
 
-        if cache_key not in wraper.execution_cache:
-            func_res = func(*args, **kwargs)
-            wraper.execution_cache[cache_key] = func_res
+    execution_cache = {}
 
+    def wrapper(*args, **kwargs) -> any:
+        cache_key = args, tuple(kwargs.items())
+
+        if cache_key not in execution_cache:
+            execution_cache[cache_key] = func(*args, **kwargs)
             print("Calculating new result")
-            return func_res
+        else:
+            print("Getting from cache")
 
-        print("Getting from cache")
-        return wraper.execution_cache[cache_key]
+        return execution_cache[cache_key]
 
-    wraper.execution_cache = {}
-
-    return wraper
+    return wrapper
