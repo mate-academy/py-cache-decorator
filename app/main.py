6a3f6166f0bbc7f0bda247bbb7c9b26efdbe6fe2
Serhii <martinenko.sergiy@gmail.com>
2023-10-02 20:25:56 +0300

    Callable to Any

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_result = {}
 
-    def inner(*args, **kwargs) -> Callable:
+    def inner(*args, **kwargs) -> Any:
         if args in cached_result:
             print("Getting from cache")
         else:
