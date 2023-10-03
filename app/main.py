1fcdaee862d70f92db7d6cd07b65ad051b278b11
Oleg Shumov <145033206+OlehShumov@users.noreply.github.com>
2023-10-03 19:49:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cache_results = {}
-
     def wrapper(*args, **kwargs) -> None:
+        if not hasattr(wrapper, "cache_results"):
+            wrapper.cache_results = {}
         arg_key = (args, frozenset(kwargs.items()))
-        if arg_key in cache_results:
+        if arg_key in wrapper.cache_results:
             print("Getting from cache")
-            result = cache_results[arg_key]
+            result = wrapper.cache_results[arg_key]
         else:
             print("Calculating and caching result")
             result = func(*args, **kwargs)
-            cache_results[arg_key] = result
+            wrapper.cache_results[arg_key] = result
         return result
 
     return wrapper
