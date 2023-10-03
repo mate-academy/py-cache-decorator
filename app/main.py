fc7f05652a85418318bd566ebd41cb7ab5a9bca4
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-03 23:16:34 +0300

    Update main.py
    add type

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def cache(func: int) -> int:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> int:
+    def wrapper(*args: int, **kwargs: int) -> int:
         cache_key = (args, frozenset(kwargs.items()))
 
         if cache_key in cache_dict:
@@ -9,7 +9,7 @@ def cache(func: int) -> int:
             return cache_dict[cache_key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
+            result = func(*args: int, **kwargs: int)
             cache_dict[cache_key] = result
             return result
 
