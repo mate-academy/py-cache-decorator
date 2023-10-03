3229e4ece673a93c15cc32804c7f2381f13d32df
Oleg Shumov <145033206+OlehShumov@users.noreply.github.com>
2023-10-03 19:45:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def cache(func: Callable) -> Callable:
         arg_key = (args, frozenset(kwargs.items()))
         if arg_key in cache_results:
             print("Getting from cache")
-            return cache_results[arg_key]
+            result = cache_results[arg_key]
         else:
+            print("Calculating and caching result")
             result = func(*args, **kwargs)
             cache_results[arg_key] = result
-            print("Calculating new result")
-            return result
+        return result
 
     return wrapper
