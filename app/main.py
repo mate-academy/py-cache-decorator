34523a0d9b506faa3653dd772e82c962fbb32c87
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-10-04 22:57:45 +0300

    Update main.py
    add new type annotation

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-def cache(func: int) -> int:
+from typing import Callable, Any
+
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
     cache_dict = {}
 
-    def wrapper(*args: int, **kwargs: int) -> int:
+    def wrapper(*args, **kwargs) -> Any:
         cache_key = (args, frozenset(kwargs.items()))
 
         if cache_key in cache_dict:
             print("Getting from cache")
             return cache_dict[cache_key]
-        else:
-            print("Calculating new result")
-            result = func(*args: int, **kwargs: int)
-            cache_dict[cache_key] = result
-            return result
+        result = func(*args, **kwargs)
+        cache_dict[cache_key] = result
+        return result
 
     return wrapper
