8bffe8cfe7f4781bbd1d645ffe01db1e58e2d1cb
unknown <pashkova.yuliana@gmail.com>
2023-10-04 15:02:17 +0300

    Fixed py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_archive = {}
 
-    def inner(*args, **kwargs) -> Any:
+    def inner(*args) -> Any:
         if args in cache_archive:
             print("Getting from cache")
         else:
-            cache_archive[args] = func(*args, **kwargs)
+            cache_archive[args] = func(*args)
             print("Calculating new result")
         return cache_archive[args]
     return inner
