5ab8527ff4ba8ee89e38335fd1fa3b24d691db0e
Сергей <kovalenkoserhii33@gmail.com>
2023-10-06 20:16:04 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -7,22 +7,10 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
-        if key in cache_dict:
-            print("Getting from cache")
-            return cache_dict[key]
-        else:
+        if key not in cache_dict:
             print("Calculating new result")
             cache_dict[key] = func(*args, **kwargs)
-            return cache_dict[key]
+        else:
+            print("Getting from cache")
+        return cache_dict[key]
     return wrapper
-
-
-# I wrote a new function because I deleted the old one
-@cache
-def expensive_function(param1: int, param2: int) -> int:
-    return param1 + param2
-
-
-@cache
-def another_expensive_function(arg1: int, arg2: int, arg3: int) -> int:
-    return arg1 * arg2 * arg3
