adab3c3ca7b73a2076a331edf30a416ead3996fe
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2023-10-07 10:06:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from typing import Callable
+from typing import Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    args_dict = {}
+
+    def saver(*args) -> Any:
+        if args in args_dict:
+            print("Getting from cache")
+            return args_dict[args]
+        else:
+            args_dict[args] = func(*args)
+            print("Calculating new result")
+            return args_dict[args]
+    return saver
