cd5740ecfe5666d6bacc1c2563cd9078a44f605c
naiv <nazarivankiv1@gmail.com>
2023-10-07 09:32:42 +0300

    Updated solution

--- app/main.py
+++ app/main.py
@@ -4,20 +4,18 @@ import functools
 
 def cache(func: Callable) -> Callable:
 
-    cached_results_storage = {}
+    cached_results = {}
 
     @functools.wraps(func)
     def inner(*args, **kwargs) -> any:
         cache_key = func.__name__, args, tuple(kwargs.items())
 
-        if cache_key not in cached_results_storage:
-            func_res = func(*args, **kwargs)
-            cached_results_storage[cache_key] = func_res
-
+        if cache_key not in cached_results:
+            cached_results[cache_key] = func(*args, **kwargs)
             print("Calculating new result")
-            return func_res
+            return cached_results[cache_key]
 
         print("Getting from cache")
-        return cached_results_storage[cache_key]
+        return cached_results[cache_key]
 
     return inner
