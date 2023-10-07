e057209b16937042d9e80fcda5727e54a1c004be
Olga Dolnyk <dolnik2104@gmail.com>
2023-10-07 08:31:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,13 +8,15 @@ def cache(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
-        if key in cached_results:
-            result = cached_results[key]
-            print("Getting from cache")
-        else:
+        result = cached_results.get(key, None)
+
+        if result is None:
             result = func(*args, **kwargs)
             cached_results[key] = result
             print("Calculating new result")
+        else:
+            print("Getting from cache")
+
         return result
 
     return wrapper
