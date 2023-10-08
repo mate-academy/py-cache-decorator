2018e12b628a3e1d888785bec000eb3384a74064
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2023-10-08 16:55:25 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     args_dict = {}
 
+    @wraps(func)
     def saver(*args) -> Any:
         if args in args_dict:
             print("Getting from cache")
