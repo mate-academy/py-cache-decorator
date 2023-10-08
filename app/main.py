d22c80133b8a111e26565c0ea36db09edf7d499b
Aleksandr Paslawski <aleksandr.paslawski@gmail.com>
2023-10-08 22:01:32 +0200

    Task done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from typing import Callable
 
 
+cache_history = dict()
+
+
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def wrapper(*args) -> str:
+        func_name = f"{func}({args})"
+
+        if func_name in cache_history:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cache_history[func_name] = func(*args)
+
+        return cache_history[func_name]
+
+    return wrapper
