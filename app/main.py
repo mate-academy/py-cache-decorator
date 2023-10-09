c2e13cb0e4fa7766e306a8dd5bd973c1ac0442e5
Yuliia Dudych <juliadudych@gmail.com>
2023-10-09 12:58:37 +0300

    Add missing print for new result

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     results = {}
+
     def inner(*args) -> None:
         nonlocal results
         if args in results:
             print("Getting from cache")
             print(results[args])
         else:
+            print("Calculating new result")
             results.update({args: func(*args)})
     return inner
 
