9f629403bd8993b970d30d9914fcb900601d2b3b
Yuliia Dudych <juliadudych@gmail.com>
2023-10-09 12:30:57 +0300

    Fill in decorator function

--- app/main.py
+++ app/main.py
@@ -2,10 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    def inner(*args, **kwargs) -> None:
-
+    def inner(data: list) -> None:
+        data_w_result = {}
+        for elem in data:
+            if elem in data_w_result:
+                print("Getting from cache")
+                print(data_w_result[elem])
+            else:
+                data_w_result.update({elem: func(data)})
     return inner
 
 
 @cache
-def long_time_func(*args, **kwargs) -> None:
+def long_time_func(data: list) -> None:
