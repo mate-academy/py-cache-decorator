a8ed367139dff17330626db6db1d0b1a9fb5ed8c
Halyna Baklanova <novabalka@gmail.com>
2023-10-13 18:37:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> None:
+    def wrapper(*args, **kwargs) -> str:
         key = (args, frozenset(kwargs.items()))
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
-        else:
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
-            print("Calculating new result")
-            return result
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        print("Calculating new result")
+        return result
 
     return wrapper
