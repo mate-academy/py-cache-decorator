96356309638fd8b358ae3f8d6e018dd951c6d6a4
Halyna Baklanova <novabalka@gmail.com>
2023-10-14 13:40:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> str:
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cache_dict:
             print("Getting from cache")
