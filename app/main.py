2f6d04a83d31df9fc4ad6425f1a1612a8a5d4fbb
Denis Dymerlii <dum.denya15@gmail.com>
2023-10-17 20:30:02 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cached_result = {}
 
+    @wraps(func)
     def wrapper(*args) -> Any:
         if args not in cached_result.keys():
             cached_result[args] = func(*args)
