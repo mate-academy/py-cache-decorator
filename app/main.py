93b72a6a4fd4ec2f109233f8ebcfbcc22de55c56
jinjo <vitaly.tyagunov@gmail.com>
2023-10-19 20:00:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def cache(func):
-    # Write your code here
-    pass
+def cache(func: callable) -> callable:
+    cached_results = dict()
+
+    def wrapper(*args, **qwargs) -> callable:
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cached_results[args] = result
+            return result
+    return wrapper
