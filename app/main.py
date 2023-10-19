b39dc1d4461a5d0f11b30359aff4a4c8b6ef6556
Andrey1104 <malovaniy.an@.com>
2023-10-19 16:19:29 +0300

    remove else, add functools, change anotation function 'wrapper'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
 
-        cache_key = args + tuple(sorted(kwargs.items()))
+        cache_key = args + tuple(kwargs.items())
         if cache_key in cache_dict:
             print("Getting from cache")
             return cache_dict[cache_key]
-        else:
-            result = func(*args, **kwargs)
-            cache_dict[cache_key] = result
-            print("Calculating new result")
-            return result
+
+        result = func(*args, **kwargs)
+        cache_dict[cache_key] = result
+        print("Calculating new result")
+        return result
 
     return wrapper
