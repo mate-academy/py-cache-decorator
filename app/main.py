9c9a71680f0070bd0c628f0374a9aba364cee157
Dmytro Karaniaga <ddikey1@gmail.com>
2023-10-19 23:00:43 +0300

    Implemented cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (func, tuple(args), frozenset(kwargs.items()))
+
+        if key in storage:
+            print("Getting from cache")
+            return storage[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            storage[key] = result
+            return result
+
+    return wrapper
