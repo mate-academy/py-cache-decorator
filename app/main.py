28e5a474d5f2f00f122732e24ccbbad98c36f2bd
Liliya <liliyakhu@ukr.net>
2023-10-20 15:46:02 +0300

    Remove redundant code

--- app/main.py
+++ app/main.py
@@ -15,21 +15,3 @@ def cache(func: Callable) -> Callable:
         return completed_runs[args]
 
     return wrapper
-
-
-@cache
-def long_time_func(first: int, second: int, third: int) -> int:
-    return (first ** second ** third) % (first * third)
-
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list:
-    return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
