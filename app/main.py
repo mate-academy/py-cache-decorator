d94d2824bf0211baf9c31416c408c452fabfb3c6
lyaskov <lyaskov1@gmail.com>
2023-10-22 18:15:39 +0300

    added implementation of cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,20 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dictionary = {}
+
+    def inner(*args, **kwargs) -> None:
+        key = (args, tuple(kwargs.items()))
+
+        if key in cache_dictionary.keys():
+            print("Getting from cache")
+            result = cache_dictionary[key]
+            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dictionary[key] = result
+
+        return result
+
+    return inner
