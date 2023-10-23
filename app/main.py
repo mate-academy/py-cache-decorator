b548ffda6c5d6cff3dabdaf660a148a9f68f9740
Anton Konyk <antonkonyk.com>
2023-10-23 20:52:39 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,10 +5,9 @@ def cache(func: Callable) -> Callable:
     result_dict = {}
 
     def inner(*args) -> Any:
-        for key in result_dict:
-            if args in result_dict:
-                print("Getting from cache")
-                return result_dict[args]
+        if args in result_dict:
+            print("Getting from cache")
+            return result_dict[args]
 
         res_fun = func(*args)
         result_dict[args] = res_fun
