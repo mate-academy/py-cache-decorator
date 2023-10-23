178f724b9d4c53e5d9362467c8074a6558afaaed
Anton Konyk <antonkonyk.com>
2023-10-23 18:25:40 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_dict = {}
+
+    def inner(*args) -> Any:
+        for key, value in result_dict.items():
+            if key == args:
+                print("Getting from cache")
+                return value
+
+        res_fun = func(*args)
+        result_dict[args] = res_fun
+        print("Calculating new result")
+        return res_fun
+
+    return inner
