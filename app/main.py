79e0cab4d76713c019a24cb630961b8a5d8df16b
Dmytro Karaniaga <ddikey1@gmail.com>
2023-10-23 15:23:08 +0300

    Removed duplicated returns

--- app/main.py
+++ app/main.py
@@ -6,14 +6,16 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Any:
         key = (func, tuple(args), frozenset(kwargs.items()))
+        result: Any
 
         if key in storage:
             print("Getting from cache")
-            return storage[key]
+            result = storage[key]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
             storage[key] = result
-            return result
+
+        return result
 
     return wrapper
