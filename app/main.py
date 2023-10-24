de7774f65065dd85e006f967bccc30607ef3be59
Oleksandr <gubar3113@gmail.com>
2023-10-24 21:46:17 +0300

    solution_1

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    collected_dictionary = {}
+
+    def wrapper(*args, **kwargs) -> dict:
+        dict_key = (func, args, frozenset(kwargs.items()))
+        if dict_key in collected_dictionary:
+            print("Getting from cache")
+            return collected_dictionary[dict_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            collected_dictionary[dict_key] = result
+            return result
+    return wrapper
