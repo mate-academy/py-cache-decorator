07567b96efa8d31fd1df45783a63e75eeda5c6e0
Marinel444 <380978907444m@gmail.com>
2023-10-26 17:04:55 +0300

    I removed it

--- app/main.py
+++ app/main.py
@@ -5,17 +5,13 @@ def cache(func: Callable) -> Callable:
     result_dict = {}
 
     def inner(*args) -> int:
-        func_name = func.__name__
-        args_str = str(args)
-        if func_name not in result_dict:
-            result_dict[func_name] = {}
-        if args_str not in result_dict[func_name]:
+        if str(args) not in result_dict:
             result = func(*args)
-            result_dict[func_name][args_str] = result
+            result_dict[str(args)] = result
             print("Calculating new result")
             return result
         else:
             print("Getting from cache")
-            return result_dict[func_name][args_str]
+            return result_dict[str(args)]
 
     return inner
