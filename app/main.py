999b83abda47f957296851cd073e63dd6f79b2bc
Sergii Pryima <spryima@gmail.com>
2023-10-28 15:16:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+
+
+cached_func = {}
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, **kwargs) -> Any:
+        key = (func, args, frozenset(kwargs.items()))
+        if key in cached_func:
+            print("Getting from cache")
+            return cached_func[key]
+        else:
+            print("Calculating new result")
+            res = func(*args, **kwargs)
+            cached_func[key] = res
+        return res
+    return inner
