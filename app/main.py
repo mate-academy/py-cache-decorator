8e00f0b14db7e7d2c5843cf7d8c1ce0ba37fcfc3
Vitalii <ananievvitalii10@gmail.com>
2023-10-29 20:14:43 +0200

    Solution to the Cache decorator problem

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(kwargs.items()))
+
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+
+        result = func(*args, **kwargs)
+        cache_store[key] = result
+        print("Calculating new result")
+        return result
+
+    return wrapper
