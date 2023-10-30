b88d23d74797f8518ba703c8da1bd883e7f75486
Volodymyr Yablonskyi <wowa2807@gmail.com>
2023-10-30 22:14:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_results = {}
+
+    def decorator(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+
+        if key in cached_results:
+            print("Getting from cache")
+            return cached_results[key]
+        else:
+            result = func(*args, **kwargs)
+            cached_results[key] = result
+            print("Calculating new result")
+            return result
+
+    return decorator
