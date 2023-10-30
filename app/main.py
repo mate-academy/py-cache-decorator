18f1115d8ceb00f59e3c77a5c0f17325084d4d6d
Mikola Rozhansky <bymikola@gmail.com>
2023-10-30 23:37:34 +0200

    work nothing

--- app/main.py
+++ app/main.py
@@ -3,4 +3,25 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     # Write your code here
-    pass
+    cache_data = {}
+    def inner(*arg, **kwarg) -> Callable:
+        nonlocal cache_data
+
+        print(arg)
+        print(cache_data)
+        if arg in list(cache_data.keys()):
+            print(f"Getting from cache: {arg} result {cache_data[arg]}")
+            print("Getting from cache")
+            return cache_data[arg]
+        else:
+            cache_data[arg] = func(*arg, **kwarg)
+            print("Calculating new result")
+            return func(*arg, **kwarg)
+
+        #     return cache_data[arg]
+        # else:
+        #     print("Calculating new result")
+        return func(*arg, **kwarg)
+    return inner
+
+
