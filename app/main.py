9b86478ddac1c34daced4ddd402561d6b2cea861
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-10-31 10:21:38 +0100

    Delete not needed part

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     used_arguments = {}
 
     def wrapper(*args) -> Callable:
-        if args not in used_arguments.keys():
+        if args not in used_arguments:
             print("Calculating new result")
             result = func(*args)
             used_arguments[args] = result
