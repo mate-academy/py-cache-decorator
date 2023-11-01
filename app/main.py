8e4842681c567eccf3be37b2a37b1fd296edd2da
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-11-01 16:53:17 +0200

    Add functools.wraps

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cached_result = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> None:
         key = (args, frozenset(kwargs))
         if key in cached_result:
