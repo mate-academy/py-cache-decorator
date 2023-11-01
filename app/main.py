89c4629ede28a78ab909241b0be1894ef78fa46c
cyb1ove <cyb1ove.ua@gmail.com>
2023-11-01 19:17:20 +0300

    impoved codestyle

--- app/main.py
+++ app/main.py
@@ -7,10 +7,9 @@ def cache(func: Callable) -> Callable:
 	def inner(*args):
 		if args in cached_values:
 			print("Getting from cache")
-			return cached_values[args]
 		else:
 			print("Calculating new result")
 			cached_values[args] = func(*args)
-			return cached_values[args]
+		return cached_values[args]
 
 	return inner
