c8abfe5f14ba06ee9f32ec5a945de12819bc6592
Kyryl Zhelizko <kyryl.zhelizko@gmail.com>
2023-11-01 23:13:39 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+
+
+data = []
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args: Any) -> Callable:
+        for el in data:
+            if el["args"] == args and el["func"] == func:
+                print("Getting from cache")
+                return el["result"]
+        result = func(*args)
+        print("Calculating new result")
+        data.append({"args": args, "func": func, "result": result})
+        return result
+
+    return inner
