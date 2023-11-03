b9b9ef444e53803a2e91fe5812478e42b07e5f5b
Oleksandr Klymyk <klymyksasha@gmail.com>
2023-11-04 01:55:46 +0200

    Cache decorator Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    func_cache = {}
+
+    @functools.wraps(func)
+    def wrapper(*args) -> any:
+
+        if args in func_cache.get(func.__name__, ()):
+            print("Getting from cache")
+            return func_cache[func.__name__][args]
+
+        print("Calculating new result")
+        result = func(*args)
+        if func_cache.get(func.__name__):
+            func_cache[func.__name__].update({args: result})
+        else:
+            func_cache[func.__name__] = {args: result}
+            return result
+
+    return wrapper
