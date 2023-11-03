e11866ac0a20000474982cb25617eaf041ca5de4
Viktor Shcherbak <dev.psychologist@gmail.com>
2023-11-03 22:45:13 +0200

    cache function created

--- app/main.py
+++ app/main.py
@@ -2,5 +2,22 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache = []
+
+    def wrapper(*args) -> None:
+        for data in cache:
+            if data["arguments"] == args:
+                print("Getting from cache")
+                return data.get("result")
+
+        results = func(*args)
+        add_cache = {
+            "arguments": args,
+            "result": results
+        }
+        cache.append(add_cache)
+        print("Calculating new result")
+
+        return results
+
+    return wrapper
