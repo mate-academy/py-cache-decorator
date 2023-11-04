14904b67a458096034b5443dabb8cb218d569f7b
Viktor Shcherbak <dev.psychologist@gmail.com>
2023-11-04 21:43:58 +0200

    cache function created with dictionary, deleted loop, add functools

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args) -> None:
+    @wraps(func)
+    def wrapper(*args) -> int:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict.get(args)
