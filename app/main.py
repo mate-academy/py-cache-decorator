b2fa55a6f769475b880b1d6d7feb7680b7bdea62
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-11-07 23:01:46 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    items_dic = {}
+
+    def wrapper(*args) -> int:
+        nonlocal items_dic
+        if args not in items_dic.keys():
+            print("Calculating new result")
+            res = func(*args)
+            items_dic[args] = res
+            return res
+        else:
+            print("Getting from cache")
+            return items_dic.get(args)
+    return wrapper
