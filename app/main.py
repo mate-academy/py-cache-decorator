f4a7fecd4c4a616a56a8b4b3212ea6857318e679
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-11-08 18:59:50 +0200

    Provided requested changes

--- app/main.py
+++ app/main.py
@@ -5,13 +5,11 @@ def cache(func: Callable) -> Callable:
     items_dic = {}
 
     def wrapper(*args) -> int:
-        nonlocal items_dic
-        if args not in items_dic.keys():
+        if args not in items_dic:
             print("Calculating new result")
             res = func(*args)
             items_dic[args] = res
             return res
-        else:
-            print("Getting from cache")
-            return items_dic.get(args)
+        print("Getting from cache")
+        return items_dic.get(args)
     return wrapper
