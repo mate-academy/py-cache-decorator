8a005b9c3db1561167ce9e2884f7e9bbd06f2df4
Oleksandr Klymyk <klymyksasha@gmail.com>
2023-11-10 02:00:44 +0200

    Updated Solution

--- app/main.py
+++ app/main.py
@@ -8,16 +8,13 @@ def cache(func: Callable) -> Callable:
     @functools.wraps(func)
     def wrapper(*args) -> any:
 
-        if args in func_cache.get(func.__name__, ()):
+        if args in func_cache:
             print("Getting from cache")
-            return func_cache[func.__name__][args]
+            return func_cache[args]
 
         print("Calculating new result")
         result = func(*args)
-        if func_cache.get(func.__name__):
-            func_cache[func.__name__].update({args: result})
-        else:
-            func_cache[func.__name__] = {args: result}
-            return result
+        func_cache[args] = result
+        return result
 
     return wrapper
