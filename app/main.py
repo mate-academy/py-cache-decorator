664bf439b722139d7367790a8de92bf2f6febfa4
artem.puhach <artem.puhach@cyberpollice.gov.ua>
2023-11-12 10:32:09 +0200

    fixed: decorator accepting both args and kwargs now & fixed blank lines

--- app/main.py
+++ app/main.py
@@ -4,15 +4,14 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     inner_cache = {}
 
-    def inner(*args) -> Any:
-
+    def inner(*args, **kwargs) -> Any:
         if args in inner_cache.keys():
             print("Getting from cache")
-            return inner_cache[args]
-        else:
-            print("Calculating new result")
-            result = func(*args)
-            inner_cache[args] = result
-            return result
-
+            return inner_cache[args + tuple(kwargs.values())]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        inner_cache[args + tuple(kwargs.values())] = result
+        return result
     return inner
+
+
