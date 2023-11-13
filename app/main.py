86ad1b5848ba942d7b47f7b12ed826b2d5aa05b1
Misha Purh <mishapurh@Mishas-MacBook-Air.local>
2023-11-12 17:47:57 -0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable) -> None:
+    cache_memory = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        cache_key = (args, frozenset(kwargs.items()))
+        if cache_key in cache_memory:
+            print("Getting from cache")
+            return cache_memory[cache_key]
+        else:
+            result = func(*args, **kwargs)
+            cache_memory[cache_key] = result
+            print("Calculating new result")
+            return result
+    return wrapper
