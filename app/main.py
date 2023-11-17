89f8de7cd17a4b7e34492793680e52cd26dae49d
Nataliia Povashevych <interchanal@ukr.net>
2023-11-17 22:08:57 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,28 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_for_funk = {}
+    cache_for_funk2 = {}
+
+    def inner(*arg) -> Callable:
+        if len(arg) == 3:
+            a, b, c = arg
+            if (a, b, c) in cache_for_funk:
+                print("Getting from cache")
+                result = cache_for_funk[(a, b, c)]
+            else:
+                print("Calculating new result")
+                result = func(a, b, c)
+                cache_for_funk[(a, b, c)] = result
+            return result
+        else:
+            n_tuple, power = arg
+            if (n_tuple, power) in cache_for_funk2:
+                print("Getting from cache")
+                result = cache_for_funk2[(n_tuple, power)]
+            else:
+                print("Calculating new result")
+                result = func(n_tuple, power)
+                cache_for_funk2[n_tuple, power] = result
+            return result
+    return inner
