5572631f0f4b0ede2b2455446b28dc61af44f836
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-11-18 22:27:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_func_names = []
+    cached_func_args = []
+    cached_func_res = []
+
+    @wraps(func)
+    def wrapper(*args) -> int:
+        func_name = func.__name__
+        for i in range(len(cached_func_names)):
+            if func_name == cached_func_names[i]:
+                if args == cached_func_args[i]:
+                    print("Getting from cache")
+                    return cached_func_res[i]
+
+        print("Calculating new result")
+        res = func(*args)
+        cached_func_names.append(func_name)
+        cached_func_args.append(args)
+        cached_func_res.append(res)
+
+        return res
+
+    return wrapper
