b7c2671b125705348493e9a770733037bc69cbe2
Nataliia Povashevych <interchanal@ukr.net>
2023-11-20 22:10:07 +0200

    'Update_Solution'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import Callable, Any
+from functools import wraps
 
 
-def cache(func: Callable) -> Any:
+def cache(func: Callable) -> Callable:
     cache_for_func = {}
 
-    def inner(*args: Any) -> int:
+    @wraps(func)
+    def inner(*args: Any, **kwargs: Any) -> int:
         key_cache = args
         if key_cache in cache_for_func:
             print("Getting from cache")
             result = cache_for_func[key_cache]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cache_for_func[key_cache] = result
         return result
     return inner
