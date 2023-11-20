fcb8f07d7bb9acc9d0b4c9af1df2d846b6e82286
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-11-20 12:42:37 +0200

    аÑRemoving return

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args: Any) -> Any:
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
+            result = cache_dict[args]
         else:
             print("Calculating new result")
             result = func(*args)
             cache_dict[args] = result
-            return result
+        return result
 
     return wrapper
     pass
