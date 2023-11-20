568863c3636d81795b4f6037bb02d57c24005a44
Roman Sokolov <gnonasis@gmail.com>
2023-11-20 14:21:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,19 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args) -> int:
+
+        for item in cash:
+            if item[0] == func and item[1] == args:
+                print("Getting from cache")
+                return item[2]
+
+        print("Calculating new result")
+        result = func(*args)
+        cash.append([func, args, result])
+        return result
+
+    return inner
+
+
+cash = [[None, None, None]]
