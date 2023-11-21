ca20a5e61039782c52ef1ba292b3d33399b0fed3
Nataliia Povashevych <interchanal@ukr.net>
2023-11-21 19:52:06 +0200

    'Solution_number_10'

--- app/main.py
+++ app/main.py
@@ -7,12 +7,11 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def inner(*args: Any, **kwargs: Any) -> int:
-        key_cache = (args, frozenset(kwargs.items()))
+        key_cache = (args, tuple(kwargs.items()))
         if key_cache in cache_for_func:
             print("Getting from cache")
-            return cache_for_func[key_cache]
         else:
             print("Calculating new result")
             cache_for_func[key_cache] = func(*args, **kwargs)
-            return func(*args, **kwargs)
+        return cache_for_func[key_cache]
     return inner
