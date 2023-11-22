75cd18cf1b51dbb6a013ce438725e1cf2ca06eda
angelzagniy <angelzagniy@gmail.com>
2023-11-22 14:11:20 +0200

    make decorator with two lists (I can`t understand how to create ditionary wiht values of function and results)

--- app/main.py
+++ app/main.py
@@ -2,5 +2,21 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    list_of_functions = []
+    list_of_results = []
+
+    def wrapper(*args: object, **kwargs: object) -> object:
+        if [func.__name__, args, kwargs] in list_of_functions:
+            print("Getting from cache")
+            return list_of_results[
+                list_of_functions.index([func.__name__, args, kwargs])
+            ]
+        else:
+            print("Calculating new result")
+            list_of_functions.append([func.__name__, args, kwargs])
+            list_of_results.append(func(*args, **kwargs))
+            return list_of_results[
+                list_of_functions.index([func.__name__, args, kwargs])
+            ]
+
+    return wrapper
