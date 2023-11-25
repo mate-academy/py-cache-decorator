9910d84c68d8de4bc1c72cb0efd0b4f7a33ba4da
angelzagniy <angelzagniy@gmail.com>
2023-11-25 21:06:19 +0200

    make variable key and add wraps

--- app/main.py
+++ app/main.py
@@ -1,21 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     dict_of_functions = {}
 
+    @wraps(func)
     def wrapper(*args: object, **kwargs: object) -> object:
-        if ((func.__name__, args, frozenset(kwargs.items()))
-                in dict_of_functions):
+        key = (func.__name__, args, frozenset(kwargs.items()))
+
+        if key in dict_of_functions:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            dict_of_functions[
-                (func.__name__, args, frozenset(kwargs.items()))
-            ] = func(*args, **kwargs)
+            dict_of_functions[key] = func(*args, **kwargs)
 
-        return dict_of_functions[
-            (func.__name__, args, frozenset(kwargs.items()))
-        ]
+        return dict_of_functions[key]
 
     return wrapper
