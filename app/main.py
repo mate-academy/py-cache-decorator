4887312c4da4eb1bde5b81e534ec2526d5a7a155
Oleksandr <binsasha97@gmail.com>
2023-11-27 09:14:32 +0200

    'compleated'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_result = {}
+
+    def inner(*args) -> None:
+        if args not in saved_result:
+            print("Calculating new result")
+            saved_result[args] = func(*args)
+        else:
+            print("Getting from cache")
+        return saved_result[args]
+
+    return inner
