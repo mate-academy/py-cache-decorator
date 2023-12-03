14748285e5c1e1ad492ada1d302338a73acdf830
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-03 22:55:17 +0100

    implement solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    store_result = {}
+
+    def inner(*args) -> str:
+        if args in store_result.keys():
+            print("Getting from cache")
+            return store_result[args]
+        else:
+            func_result = func(*args)
+            store_result.update({args: func_result})
+            print("Calculating new result")
+            return func_result
+    return inner
