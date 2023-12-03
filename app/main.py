1bd119c4fd7e980f9f1b740aad142bbfd26e6c2c
Futilite <marketesh@gmail.com>
2023-12-03 20:46:42 +0200

    SFA9

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from functools import wraps
 from typing import Any, Callable, Dict, Tuple
 
-def cache(func: Callable[..., Any], key_func: Callable[..., Tuple[Any, ...]] = None) -> Callable[..., Any]:
+def cache(func: Callable[..., Any]) -> Callable[..., Any]:
     results: Dict[Tuple[Any, ...], Any] = {}
 
     @wraps(func)
-    def wrapper(*args: Any) -> Any:
-        cache_key = key_func(*args) if key_func else args
-        print("Getting from cache" if cache_key in results else "Calculating new result")
-        return results.setdefault(cache_key, func(*args))
+    def wrapper(*args: Any, **kwargs) -> Any:
+        print("Getting from cache" if args in results else "Calculating new result")
+        return results.setdefault(args, func(*args))
 
     return wrapper
