7e4282fbd360db0aded8c3debe52d61a69898396
Max <bidun.max@gmail.com>
2023-12-04 21:38:40 +0200

    Solution_4

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cache_memory = {}
 
+    @wraps(func)
     def inner(*args, **kwargs) -> Callable:
         key = args + tuple(kwargs.values())
         if key in cache_memory:
