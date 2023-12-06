400a0a3c0bc2651dbfa088cf967c11640af3801b
Maksym Symonovych <maksym.symonovych@gmail.com>
2023-12-06 19:42:01 +0200

    add solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @wraps(func)
+    def inner(*args) -> int:
+        if cache_dict.get(args) is not None:
+            print("Getting from cache")
+            return cache_dict[args]
+        print("Calculating new result")
+        result = func(*args)
+        cache_dict[args] = result
+        return result
+
+    return inner
