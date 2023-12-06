f15e8aaf2ea744179f44190efcb65bdc452743d2
xrkxdd <nazar.mc28@gmail.com>
2023-12-06 18:58:58 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     result = {}
 
-    def wrapper(*args) -> Any:
+    def wrapper(*args, **kwargs) -> Any:
         if args not in result:
             print("Calculating new result")
             result[args] = func(*args)
