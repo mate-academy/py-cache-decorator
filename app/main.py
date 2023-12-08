b6556999467d199785e1d7fc5261e09008abb704
Felix Poslushnyi <felixp4@gmail.com>
2023-12-08 05:56:59 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,28 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_dict = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        immutable = True
+        for arg in args:
+            if (
+                isinstance(arg, list)
+                or isinstance(arg, dict)
+                or isinstance(arg, set)
+            ):
+                immutable = False
+
+        if not immutable:
+            return func(*args, **kwargs)
+
+        if args not in result_dict:
+            print("Calculating new result")
+            result_value = func(*args, **kwargs)
+            result_dict[*args] = result_value
+            return result_value
+        else:
+            print("Getting from cache")
+            return result_dict[args]
+
+    return wrapper
