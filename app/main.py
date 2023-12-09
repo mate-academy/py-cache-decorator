98ba31cf98355ed6d6e2da5cf4239c14653eab36
Stepan Stelmakhovych <126243562+StepanSTY@users.noreply.github.com>
2023-12-09 15:11:14 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> dict:
-        key = (args, frozenset(kwargs.items()))
-        if key in cache_dict:
-            print("Getting from cache")
-            return cache_dict[key]
-        else:
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
+       def wrapper(*args, **kwargs) -> dict:
+        key = (args, frozenset(kwargs.items())) if kwargs else args
+        if key not in cache_dict:
+            cache_dict[key] = func(*args, **kwargs)
             print("Calculating new result")
-            return result
-
+        else:
+            print("Getting from cache")
+        return cache_dict[key]
+           
     return wrapper
     pass
