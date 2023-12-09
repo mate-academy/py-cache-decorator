0b5fbbeab47ac6abf02e34fe8ffd060b13a922d9
Felix Poslushnyi <felixp4@gmail.com>
2023-12-09 18:49:48 +0200

    Add @wrap(func)

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     result_dict = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
+        # check for mutable arguments
         for arg in args:
             if (
                 isinstance(arg, list)
                 or isinstance(arg, dict)
                 or isinstance(arg, set)
             ):
+                # if mutable, just return function
                 return func(*args, **kwargs)
 
         if args not in result_dict:
