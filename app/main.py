dfd65be6c872cc989d4d30abcb303ce78c9c8660
Arsen Rudnytskyy <acikz.33@google.com>
2023-12-11 12:20:34 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,20 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+
+    result_history = {}
+    input_history = []
+
+    def wrapper(*args) -> Callable:
+        arguments = ", ".join(str(arg) for arg in args)
+
+        if arguments not in input_history:
+            result = func(*args)
+            input_history.append(arguments)
+            result_history[arguments] = result
+            print("Calculating new result")
+            return result
+        else:
+            print("Getting from cache")
+            return result_history[arguments]
+    return wrapper
