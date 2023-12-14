49a360069f9065f46b8c4ff3a26b559775007e2e
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-14 15:02:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = dict()
+
+    def decorator(*data: Any) -> Any:
+        if not len(data) == 1 and not isinstance(data[0], (list, set, dict)):
+            if data in cache_data:
+                print("Getting from cache")
+                return cache_data[data]
+            new_data = func(*data)
+            cache_data[data] = new_data
+            print("Calculating new result")
+            return new_data
+
+    return decorator
