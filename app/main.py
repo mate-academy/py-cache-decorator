0adfd7192a9d5be43b510cea97f9177f38cd8f91
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-14 18:26:03 +0200

    created decorator "def cache"

--- app/main.py
+++ app/main.py
@@ -17,19 +17,3 @@ def cache(func: Callable) -> Callable:
         return result
 
     return wrapper
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
