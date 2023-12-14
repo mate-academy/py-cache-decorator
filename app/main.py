0e6f3b610b442f2a509d80e9a7d3a7225b814c53
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-14 18:20:49 +0200

    created decorator "def cache"

--- app/main.py
+++ app/main.py
@@ -2,5 +2,34 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_results = {}
+
+    def wrapper(*args, **kwargs):
+        key = (func, args, frozenset(kwargs.items()))
+        
+        if key in cached_results:
+            print("Getting from cache")
+            return cached_results[key]
+        
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cached_results[key] = result
+        return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
