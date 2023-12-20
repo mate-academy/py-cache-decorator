ec4eb1d22a0dd7b162c8ef9d9d57dcf76c5a64ea
D Petetskyi <4477617@gmail.com>
2023-12-20 17:56:27 +0200

    Completed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 from typing import Callable
 
+memory = {}
+
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, **kwargs) -> dict:
+        key = args + tuple(kwargs.items()), func
+        if key not in memory:
+            memory[key] = func(*args, **kwargs)
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+        return memory[key]
+
+    return inner
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
