6f785dd05d2263910bb6d1102a829fe978943dab
D Petetskyi <4477617@gmail.com>
2023-12-20 21:00:59 +0200

    added functools.wraps

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from typing import Callable
-
-memory = {}
+import functools
 
 
 def cache(func: Callable) -> Callable:
+    @functools.wraps(func)
     def inner(*args, **kwargs) -> dict:
         key = args + tuple(kwargs.items()), func
-        if key not in memory:
-            memory[key] = func(*args, **kwargs)
+        if key not in inner.cache:
+            inner.cache[key] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return memory[key]
-
+        return inner.cache[key]
+    inner.cache = dict()
     return inner
