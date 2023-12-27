315c98b0da920047c1efc04e6e79c40558155199
Ivan Romanov <i.romanov.dn@gmail.com>
2023-12-27 03:41:23 +0200

    fix solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from typing import Callable, Any
 
+
 def cache(func: Callable) -> Callable:
     cache_data = {}
+
     def inner(*args) -> Any:
         if not cache_data.get(func.__name__):
             result = func(*args)
@@ -18,32 +20,3 @@ def cache(func: Callable) -> Callable:
             print("Getting from cache")
             return cache_data[func.__name__][args]
     return inner
-
-#
-# @cache
-# def long_time_func(a, b, c):
-#     return (a ** b ** c) % (a * c)
-#
-#
-# @cache
-# def long_time_func_2(text_1, text_2):
-#     return f"{text_1.upper()}, {text_2.lower()}"
-#
-#
-# @cache
-# def long_time_func_3(n_list, text):
-#     return f"{[i ** 2 for i in n_list]}, {text}"
-#
-# long_time_func(1, 2, 3)
-# long_time_func(1, 2, 3)
-# long_time_func(1, 2, 3)
-# long_time_func_3((10, 20, 30), "wow, numbers!")
-# long_time_func(2, 2, 3)
-# long_time_func_2("Hello", "world")
-# long_time_func(1, 2, 3)
-# long_time_func_2("Hello", "Mark")
-# long_time_func_2("Hello", "Mark")
-# long_time_func_3((10, 20, 30), "wow, numbers!")
-# long_time_func_3((10, 20, 30), "egh, numbers...")
-#
-
