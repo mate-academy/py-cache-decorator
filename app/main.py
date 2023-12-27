51d45bf1e6775505eb14bfeba527cd665410fce6
DenZatolokin <denzatolokin@gmail.com>
2023-12-28 00:17:23 +0200

    Result in cache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_archive = {}
+
+    def inner(*args, **kwargs) -> Any:
+        key = args + tuple(kwargs.items())
+        if key not in result_archive:
+            print("Calculating new result")
+            result_archive[args] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return result_archive[key]
+    return inner
