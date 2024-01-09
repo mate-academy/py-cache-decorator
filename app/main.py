7a7360aed13c0af8fa4aa362e9464cd9a2c9e288
Kate <katrusja.ko@gmail.com>
2024-01-09 18:46:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,32 +3,13 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     memory = {}
-    def inner(*args):
+
+    def inner(*args) -> dict:
         if args in memory.keys():
             print("Getting from cache")
-            print(memory[args])
             return memory[args]
         else:
             print("Calculating new result")
-            t = func(*args)
-            memory[args] = t
-            print(t)
-            return t
+            memory[args] = func(*args)
+            return memory[args]
     return inner
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func(1, 2, 3)
-long_time_func(3, 4, 5)
-long_time_func(3, 4, 5)
-long_time_func_2((1, 2, 3), 5)
-long_time_func_2((4, 5, 6), 5)
-long_time_func_2((1, 2, 3), 5)
