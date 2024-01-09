766abc0fd7cb21ab946df650068764436edd062c
ScienceUA <o@upman.group>
2024-01-09 20:24:39 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,21 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_result = {}
+
+    def wrapper(*args, **kwargs) -> str:
+        cache_key = func(args, tuple(sorted(kwargs.items())))
+        if cache_key in cached_result:
+            print("Getting from cache")
+            return cached_result[cache_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached_result[cache_key] = result
+            return result
+    return wrapper
+
+
+@cache
+def pass_through_function(*args, **kwargs) -> tuple:
+    return args, kwargs
