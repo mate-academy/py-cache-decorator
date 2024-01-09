76e9330af90b8e34ff57dbd9a86ad008d9ee6e2b
kabust <tulyulyuk91@gmail.com>
2024-01-09 16:55:11 +0100

    Fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     data = {}
 
-    def wrapper(*args, **kwargs) -> int:
-        if args in data:
+    def wrapper(*args, **kwargs) -> Any:
+        key = args + tuple(kwargs.items())
+
+        if key in data:
             print("Getting from cache")
-            return data[args]
+            return data[key]
+
         print("Calculating new result")
         result = func(*args, **kwargs)
-        data.update({args: result})
+        data.update({key: result})
         return result
+
     return wrapper
