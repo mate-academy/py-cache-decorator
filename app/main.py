1c39bcdab6b6097e65f351f3a6aec1b7fd4c5539
Polonnikova Anna <xxxuko@gmail.com>
2024-01-10 09:12:51 +0200

    fixed mistakes

--- app/main.py
+++ app/main.py
@@ -3,22 +3,18 @@ from typing import Callable, Any
 
 def cache(func: Callable) -> Callable:
     def wrapper(*args, **kwargs) -> Any:
-        func_name = func.__name__
-        key_args = args + tuple(kwargs.items())
+        key = args + tuple(kwargs.items())
 
-        if func_name not in wrapper.results:
-            wrapper.results[func_name] = {}
-
-        if key_args not in wrapper.results[func_name]:
+        if key not in wrapper.results:
             value_result = func(*args, **kwargs)
-            new_dict = {key_args: value_result}
-            wrapper.results[func_name].update(new_dict)
+            new_dict = {key: value_result}
+            wrapper.results.update(new_dict)
             print("Calculating new result")
 
         else:
             print("Getting from cache")
 
-        return wrapper.results[func_name][key_args]
+        return wrapper.results[key]
 
     wrapper.results = {}
     return wrapper
