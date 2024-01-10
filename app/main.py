3a0f5705a3c3be83e7477211b245c42a80c14e2c
Ant <u123@ua.fm>
2024-01-10 16:47:09 +0200

    Solution & fixing unnecessary "else"

--- app/main.py
+++ app/main.py
@@ -8,9 +8,8 @@ def cache(func: Callable) -> Callable:
         if args in cache_dict :
             print("Getting from cache")
             return cache_dict[args]
-        else :
-            print("Calculating new result")
-            result = func(*args)
-            cache_dict[args] = result
+        print("Calculating new result")
+        result = func(*args)
+        cache_dict[args] = result
         return result
     return wrapper
