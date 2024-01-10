4c5bc5478c8d1917851feaec80005149cfe6f115
Polonnikova Anna <xxxuko@gmail.com>
2024-01-10 11:55:09 +0200

    fixed wrapper.results -> results

--- app/main.py
+++ app/main.py
@@ -2,18 +2,19 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
+    results = {}
+
     def wrapper(*args, **kwargs) -> Any:
         key = args + tuple(kwargs.items())
 
-        if key not in wrapper.results:
+        if key not in results:
             value_result = func(*args, **kwargs)
-            wrapper.results[key] = value_result
+            results[key] = value_result
             print("Calculating new result")
 
         else:
             print("Getting from cache")
 
-        return wrapper.results[key]
+        return results[key]
 
-    wrapper.results = {}
     return wrapper
