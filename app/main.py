8cc9a2d5c46bc1b124189ba76eb86e75445aed18
Kyrylo Chepenko <chip22835ya@gmail.com>
2024-01-10 21:25:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,30 +1,18 @@
-from typing import List, Tuple, Union, Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    results_cache = {}
+    cache_storage = {}
 
-    def wrapper(*args, **kwargs) -> Union[int, List[int], Tuple[int, int]]:
-        key = (args, frozenset(kwargs.items()))
-        if key in results_cache:
+    @wraps(func)
+    def inner(*args) -> Any:
+        if args in cache_storage:
             print("Getting from cache")
-            return results_cache[key]
-        else:
-            result = func(*args, **kwargs)
-            results_cache[key] = result
-            print("Calculating new result")
-            return result
+            return cache_storage[args]
+        result = func(*args)
+        cache_storage[args] = result
+        print("Calculating new result")
+        return result
 
-    return wrapper
-
-
-@cache
-def long_runtime_function(data: List[int]) -> List[int]:
-    print(f"Running long runtime function with data: {data}")
-    return [element * 2 for element in data]
-
-
-@cache
-def another_function(arg1: int, arg2: int) -> int:
-    print(f"Running another function with arguments: {arg1}, {arg2}")
-    return arg1 + arg2
+    return inner
