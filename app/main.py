73a6d32474b04c52b7d2ac4ab2fe34dc8df34c59
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-10 09:45:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,19 @@ def cache(func: Callable) -> Callable:
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
-            return result
-
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_dict[key] = result
+        return result
     return inner
+
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+print(
+long_time_func(1, 2, 3),
+long_time_func(2, 2, 3)
+)
