4c8c57e309f5505c206092199d24002ff6471ef3
Ant <u123@ua.fm>
2024-01-10 12:26:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}  # dict: args -> result
+
+    def wrapper(*args) -> Any:
+        if args in cache_dict :
+            print("Getting from cache")
+            return cache_dict[args]
+        else :
+            print("Calculating new result")
+            result = func(*args)
+            cache_dict[args] = result
+        return result
+    return wrapper
