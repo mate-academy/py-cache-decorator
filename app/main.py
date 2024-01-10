7b056f5a1bf9d29536f6957d291baa959918eead
Kyrylo Storozhenko <busterakel@gmail.com>
2024-01-10 09:46:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -14,14 +14,3 @@ def cache(func: Callable) -> Callable:
         cache_dict[key] = result
         return result
     return inner
-
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-print(
-long_time_func(1, 2, 3),
-long_time_func(2, 2, 3)
-)
