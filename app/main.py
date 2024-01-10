5048d110b4a4cb3de1d9f539a4e490a3957bac4e
Kate <katrusja.ko@gmail.com>
2024-01-10 20:47:11 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -7,9 +7,8 @@ def cache(func: Callable) -> Callable:
     def inner(*args) -> dict:
         if args in memory.keys():
             print("Getting from cache")
-            return memory[args]
         else:
             print("Calculating new result")
             memory[args] = func(*args)
-            return memory[args]
+        return memory[args]
     return inner
