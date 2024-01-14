db51cad13a81c695c98cee0f5570c7239b19efce
volry <v.v.ryzhuk@gmail.com>
2024-01-14 10:59:36 +0200

    make first block

--- app/main.py
+++ app/main.py
@@ -2,5 +2,8 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {} # створимо словник
+
+    def wrapper (*args):
+
+    return wrapper
