e5857d4e08b01180572659aee4beb9bc6e531013
volry <v.v.ryzhuk@gmail.com>
2024-01-15 08:49:25 +0200

    create function to store results

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cache_dict = {} # створимо словник
+    cache_dict = {} # I want to create dict
 
     def wrapper (*args):
+
+        key = (func.__name__, args)
+
+        # Check if the result is already in the cache
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+
+        # If not in cache, calculate the result and store it
+        print("Calculating new result")
+        result = func(*args)
+        cache_dict[key] = result
+        return result
 
     return wrapper
