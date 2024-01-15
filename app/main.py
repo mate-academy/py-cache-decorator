76ca99f7875091eadbb42bf2d657c339bbe4409c
volry <v.v.ryzhuk@gmail.com>
2024-01-15 08:54:20 +0200

    clean my comments

--- app/main.py
+++ app/main.py
@@ -8,12 +8,10 @@ def cache(func: Callable) -> Callable:
 
         key = (func.__name__, args)
 
-        # Check if the result is already in the cache
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
 
-        # If not in cache, calculate the result and store it
         print("Calculating new result")
         result = func(*args)
         cache_dict[key] = result
