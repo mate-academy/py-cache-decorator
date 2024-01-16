aac65d00668f83723e0c8542c381080e46d88e78
Viktar Chaunavy <paca.vaca.pachvara@gmail.com>
2024-01-16 22:12:30 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results = {}
+
+    def repeat_function(*args) -> Any:
+        if args in results:
+            print("Getting from cache")
+            return results[args]
+        else:
+            print("Calculating new result")
+            new_result = func(*args)
+            results[args] = new_result
+            return new_result
+    return repeat_function
