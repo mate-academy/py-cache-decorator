f8e903d4ca7322e93626244fb9d97e568aa876a7
voronvasia <voronvasia@gmail.com>
2024-01-19 23:21:16 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    saved_result = {}
+
+    def wrapper_cache(*args) -> dict:
+        if args in saved_result:
+            print("Getting from cache")
+            return saved_result[args]
+        else:
+            print("Calculating new result")
+            saved_result[args] = func(*args)
+            return saved_result[args]
+
+    return wrapper_cache
