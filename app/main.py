c2bbc5beb8ba9570774f28f00657abd58be34921
Voronessko <voronessko@gmail.com>
2024-01-20 16:22:27 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    data_base = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        combined_arguments = (args, frozenset(kwargs.items()))
+        if combined_arguments not in data_base:
+            result = func(*args, **kwargs)
+            data_base[combined_arguments] = result
+            print("Calculating new result")
+        else:
+            print("Getting from cache")
+            result = data_base[combined_arguments]
+        return result
+    return inner
