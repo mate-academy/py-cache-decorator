31e609fe0e9d032c53183f16bac00f39e3933599
voronvasia <voronvasia@gmail.com>
2024-01-20 14:12:25 +0200

    refactored code

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     saved_result = {}
 
+    @wraps(func)
     def wrapper_cache(*args) -> dict:
         if args in saved_result:
             print("Getting from cache")
             return saved_result[args]
-        else:
-            print("Calculating new result")
-            saved_result[args] = func(*args)
-            return saved_result[args]
+
+        print("Calculating new result")
+        saved_result[args] = func(*args)
+        return saved_result[args]
 
     return wrapper_cache
