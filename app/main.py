cad1f56299569b8da9153aa0bbef4df1e9a9f618
Vitalii Burkalo <vitaliyburkalo81@gmail.com>
2024-01-22 22:09:14 +0200

    Rewritten the function with using return statement one time

--- app/main.py
+++ app/main.py
@@ -7,13 +7,14 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
 
-        if key in cache_store:
+        result = cache_store.get(key)
+
+        if result is not None:
             print("Getting from cache")
-            return cache_store[key]
         else:
             result = func(*args, **kwargs)
             cache_store[key] = result
             print("Calculating new result")
-            return result
+        return result
 
     return wrapper
