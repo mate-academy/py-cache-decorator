68560677aa964938a1ef6632396ee7195930e2d8
Bodan Tereshchenkov <jonieblack1488@gmail.com>
2024-01-23 17:18:13 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    dict_cache = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        item = (args, frozenset(kwargs.items()))
+        if item in dict_cache:
+            print("Getting from cache")
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        dict_cache[item] = result
+        return dict_cache[item]
+
+    return wrapper
