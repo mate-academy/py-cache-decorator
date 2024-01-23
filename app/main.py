1fee85a6fc5485282e120a2c48811054085e10fd
dap-glitch <andruha.ept@gmail.com>
2024-01-23 14:55:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args, **kwargs) -> int:
+        piece_of_data = (args, frozenset(kwargs.items()))
+        if piece_of_data in cache_dict:
+            print("Getting from cache")
+            return cache_dict[piece_of_data]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[piece_of_data] = result
+            return result
+
+    return wrapper
