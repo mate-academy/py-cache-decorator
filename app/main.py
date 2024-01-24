08d8f1bae192920f62e7f849cf853d586eb43eac
Maksym Hekalo <maximgekalo@gmail.com>
2024-01-24 22:04:55 +0200

    make logic implementation easier than was

--- app/main.py
+++ app/main.py
@@ -4,16 +4,13 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args_1: tuple) -> Any:
-        def save_result_in_cache(*args_2: tuple) -> bool:
-            if args_2 not in cache_dict.keys():
-                cache_dict[args_2] = func(*args_2)
-                return True
-            return False
-        was_calculating = save_result_in_cache(*args_1)
-        print(
-            "Calculating new result" if
-            was_calculating else "Getting from cache"
-        )
-        return cache_dict[*args_1]
+    def wrapper(*args: tuple) -> Any:
+        func_was_calculating = args not in cache_dict.keys()
+        if func_was_calculating:
+            cache_dict[args] = func(*args)
+
+        print(["Getting from cache",
+               "Calculating new result"][func_was_calculating])
+
+        return cache_dict[args]
     return wrapper
