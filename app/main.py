3f99b7f1056c183bc5534fa273f534f15261d7f8
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-01-24 11:55:43 +0200

    implement-py-cache-decorator with requested changes

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    cache2 = {}
+    cache_dict = {}
 
     def wrapper(*args, **kwargs) -> any:
         key = (args, tuple(kwargs.items()))
-        if key in cache2:
+        result = cache_dict.get(key, None)
+
+        if result is not None:
             print("Getting from cache")
-            return cache2[key]
         else:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache2[key] = result
-            return result
+            cache_dict[key] = func(*args, **kwargs)
+        return cache_dict[key]
 
     return wrapper
