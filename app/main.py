8c670f6a2498f8c1b8adfa91d1c4276288a588a2
Maksym Hekalo <maximgekalo@gmail.com>
2024-01-24 09:26:20 +0200

    create cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args_1: tuple) -> Any:
+        def save_result_in_cache(*args_2: tuple) -> bool:
+            if args_2 not in cache_dict.keys():
+                cache_dict[args_2] = func(*args_2)
+                return True
+            return False
+        was_calculating = save_result_in_cache(*args_1)
+        print(
+            "Calculating new result" if
+            was_calculating else "Getting from cache"
+        )
+        return cache_dict[*args_1]
+    return wrapper
