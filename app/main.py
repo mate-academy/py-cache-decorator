05a0dd79e600e4c0062878a71c1ca4435c85252b
Erik <erikgriniov1@gmail.com>
2024-01-25 13:48:47 +0200

    Fix_Solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from typing import Callable, Any
 
 from typing import Callable, Any
 
+
 def cache(func: Callable) -> Callable:
     cache_values = []
     result_values = []
+
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cache_values:
@@ -13,7 +15,6 @@ def cache(func: Callable) -> Callable:
             for i in range(len(cache_values)):
                 if key == cache_values[i]:
                     return result_values[i]
-
         else:
             result = func(*args, **kwargs)
             cache_values.append(key)
