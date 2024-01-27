355447e0e7c01a36715935324cbba2817b29559f
Artem Milchenko <pl301078map2@gmail.com>
2024-01-27 17:30:49 +0200

    Changed the dictionary, do not save the name functions

--- app/main.py
+++ app/main.py
@@ -2,19 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    result_dict = {}
+    caching_result = {}
 
     def wrapper(*args) -> Callable:
-        name_func = func.__name__
-        if name_func in result_dict:
-            if args in [arg[0] for arg in result_dict[name_func]]:
-                print("Getting from cache")
-                result_num = [num for num in result_dict[name_func]
-                              if num[0] == args][0][-1]
-                return result_num
+        if args in caching_result:
+            print("Getting from cache")
+            return caching_result[args][0]
         print("Calculating new result")
         result_func = func(*args)
-        result_dict[name_func] = (result_dict.get(name_func, [])
-                                  + [[args] + [result_func]])
+        caching_result[args] = (caching_result.get(args, []) + [result_func])
         return result_func
     return wrapper
