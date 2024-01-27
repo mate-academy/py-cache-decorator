f595efd7747ae7ec330cbed4fe1cfe28c40f3314
Artem Milchenko <pl301078map2@gmail.com>
2024-01-27 19:49:54 +0200

    implement use functools.wraps and comment on lines +10 to +14

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from typing import Callable
-
+from functools import wraps
 
 def cache(func: Callable) -> Callable:
     caching_result = {}
-
+    @wraps(func)
     def wrapper(*args) -> Callable:
         if args in caching_result:
             print("Getting from cache")
-            return caching_result[args][0]
+            return caching_result[args]
         print("Calculating new result")
-        result_func = func(*args)
-        caching_result[args] = (caching_result.get(args, []) + [result_func])
-        return result_func
+        caching_result[args] = func(*args)
+        return caching_result[args]
     return wrapper
