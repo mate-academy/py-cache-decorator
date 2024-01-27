4211c4c533283e4c38fb5a8b696fc5eb08a01af8
Artem Milchenko <pl301078map2@gmail.com>
2024-01-27 11:51:15 +0200

    Solution py-cache-decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,26 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_dict = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        name_func = func.__name__
+        if name_func in result_dict:
+            if args in [arg[0] for arg in result_dict[name_func]]:
+                print("Getting from cache")
+                result_num = [num for num in result_dict[name_func]
+                              if num[0] == args][0][-1]
+                return result_num
+            else:
+                print("Calculating new result")
+                result_func = func(*args, **kwargs)
+                result_dict[name_func] = (result_dict.get(name_func, [])
+                                          + [[args] + [result_func]])
+                return result_func
+        else:
+            print("Calculating new result")
+            result_func = func(*args, **kwargs)
+            result_dict[name_func] = (result_dict.get(name_func, [])
+                                      + [[args] + [result_func]])
+            return result_func
+    return wrapper
