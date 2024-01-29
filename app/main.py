f3f9944c1d166ac40bf566ce44511447bde81896
dzhyranov <v.dzhyranov@gmail.com>
2024-01-29 23:29:56 +0200

    add wraps

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
+    @wraps(func)
     def wrapper(*args) -> Callable:
         if args in cache_storage:
             print("Getting from cache")
