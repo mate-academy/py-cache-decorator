ecd44d2009c411f9bee055f7078076dfd53b0485
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-01-31 11:28:01 +0100

    Rewrited with using one return statement

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ def cache(func: Callable) -> Callable:
     def wrapper(*args) -> int:
         if args in cache_dict:
             print("Getting from cache")
-            return cache_dict[args]
         else:
             print("Calculating new result")
-            result = func(*args)
-            cache_dict[args] = result
-            return result
+            cache_dict[args] = func(*args)
+        return cache_dict[args]
     return wrapper
