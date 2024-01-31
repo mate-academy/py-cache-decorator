a171145108c04e24ba0f4745548242824f2b977e
Molchanov Ihor <=>
2024-01-31 23:03:50 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_data = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        params = (args, frozenset(kwargs))
+        if params in cache_data:
+            print("Getting from cache")
+            return cache_data[params]
+        else:
+            cache_data[params] = func(*args, **kwargs)
+            print("Calculating new result")
+            return cache_data[params]
+
+    return inner
