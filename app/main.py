c5a9e8f45bb3668d4e99044b5f0884cac2db172b
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-02-01 21:29:36 +0200

    cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_results = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> None:  # Not sure what return type annotation to use here
+        key = (func.__name__, args, tuple(kwargs.items()))
+
+        if key in cached_results:
+            print("Getting from cache")
+            return cached_results[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cached_results[key] = result
+            return result
+
+    return wrapper
