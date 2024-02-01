4849ba4c7b53df7b944ec5f39aa8a777ab76825e
Maksym Shramko <shramkomax@gmail.com>
2024-02-01 11:58:50 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,27 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    results_cache = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+        if key in results_cache:
+            print("Getting from cache")
+            return results_cache[key]
+        else:
+            result = func(*args, **kwargs)
+            results_cache[key] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(aa: int, bb: int, cc: int) -> int:
+    return (aa ** bb ** cc) % (aa * cc)
+
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
