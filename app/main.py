31a39b3d62e8e524fe27820a370cc8cbc9506aeb
Molchanov Ihor <=>
2024-02-01 15:51:10 +0200

    - change inner annotation - removed "else" (according to a comment from the review)

--- app/main.py
+++ app/main.py
@@ -4,14 +4,13 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cache_data = {}
 
-    def inner(*args, **kwargs) -> Callable:
+    def inner(*args, **kwargs) -> object:
         params = (args, frozenset(kwargs))
         if params in cache_data:
             print("Getting from cache")
             return cache_data[params]
-        else:
-            cache_data[params] = func(*args, **kwargs)
-            print("Calculating new result")
-            return cache_data[params]
+        cache_data[params] = func(*args, **kwargs)
+        print("Calculating new result")
+        return cache_data[params]
 
     return inner
