59ca26d02b85c5139bb2b946849e68de8f917b71
Roman <fb.momot.r@gmail.com>
2024-02-03 15:31:48 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -8,13 +8,15 @@ def cache(func: Callable) -> Callable:
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
         key = (args, frozenset(kwargs.items()))
-        if key in cache_dict:
+        result = cache_dict.get(key, None)
+
+        if result is not None:
             print("Getting from cache")
-            return cache_dict[key]
         else:
             result = func(*args, **kwargs)
             cache_dict[key] = result
             print("Calculating new result")
-            return result
+
+        return result
 
     return wrapper
