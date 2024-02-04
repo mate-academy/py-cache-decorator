1803f9b9b8798f3e58560acab25babb48f8105c8
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-04 16:22:49 +0200

    Implement cache-decorator -> replace counter with else

--- app/main.py
+++ app/main.py
@@ -6,12 +6,11 @@ cash = []
 
 def cache(func: Callable) -> Callable:
     def wrapper(*args) -> int:
-        count = 0
         for item in cash:
             if item[0] == func and item[1] == args:
                 print("Getting from cache")
                 return item[2]
-        if count == 0:
+        else:
             print("Calculating new result")
             result = func(*args)
             cash.append([func, args, result])
