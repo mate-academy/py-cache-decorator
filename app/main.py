ee3c47c89450745f6ca463c1c15d4348f4b248d6
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-04 16:04:20 +0200

    Implement cache-decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from typing import Callable
 
 
+cash = []
+
+
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def wrapper(*args) -> int:
+        count = 0
+        for item in cash:
+            if item[0] == func and item[1] == args:
+                print("Getting from cache")
+                return item[2]
+        if count == 0:
+            print("Calculating new result")
+            result = func(*args)
+            cash.append([func, args, result])
+            return result
+    return wrapper
