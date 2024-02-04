0095ef4026c75971b0effadca6a60a4ff76f3175
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-04 18:52:40 +0200

    Add **kwargs

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cash = {}
 
-    def wrapper(*args) -> int:
+    def wrapper(*args, **kwargs) -> int:
         for item in cash:
             if f"{func}, {args}" in cash:
                 print("Getting from cache")
                 return cash[f"{func}, {args}"]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cash[f"{func}, {args}"] = result
             return result
     return wrapper
