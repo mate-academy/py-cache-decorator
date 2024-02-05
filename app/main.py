6e1258bcd2ea55dfde7458200ee8ba42f508afcf
OKochubei <alexkochubey13@gmail.com>
2024-02-05 21:39:06 +0200

    Changed solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_storage = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
 
