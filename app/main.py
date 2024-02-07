d283b7425f3697c45add25aa911f5a9fd6c3fef2
Andrii <jakodestroys@gmail.com>
2024-02-07 14:43:10 +0200

    add @wraps, remove unnecessary 'else'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     dict_cache = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(kwargs.items()))
         if key in dict_cache:
             print("Getting from cache")
             return dict_cache[key]
-        else:
-            result = func(*args, **kwargs)
-            dict_cache[key] = result
-            print("Calculating new result")
-            return result
+
+        result = func(*args, **kwargs)
+        dict_cache[key] = result
+        print("Calculating new result")
+        return result
 
     return wrapper
