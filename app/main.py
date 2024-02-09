5e95304fbc91474626f56c084d38e5edf00b74b5
= <=>
2024-02-09 09:04:14 +0200

    Wrote cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @wraps(func)
+    def inner(*args) -> None:
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        print("Calculating new result")
+        add = True
+        for arg in args:
+            if isinstance(arg, (list, set, dict)):
+                add = False
+        if add:
+            cache_dict[args] = func(*args)
+        return func(*args)
+
+    return inner
