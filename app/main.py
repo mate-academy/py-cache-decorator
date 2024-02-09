f1e4a7f9eb29f469901b756051093fe0477de4f6
Rost Bonchuk <rostbonchuk@gmail.com>
2024-02-09 16:48:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    def wrap(*args) -> Callable:
+        key = (func.__name__, args)
+        if key not in storage:
+            print("Calculating new result")
+            storage[key] = func(*args)
+        else:
+            print("Getting from cache")
+        return storage[key]
+
+    return wrap
