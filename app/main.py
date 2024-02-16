d200ae025a85d7c4b10136d836ae286282ebc633
AlexNechval <oleksandrnechval@gmail.com>
2024-02-16 10:05:19 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Tuple, Dict, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    tempe_big: Dict[Tuple] = {}
+
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        tempe_small = args + tuple(sorted(kwargs.items()))
+        if tempe_small not in tempe_big:
+            print("Calculating new result")
+            tempe_big[tempe_small] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+        return tempe_big[tempe_small]
+
+    return wrapper
