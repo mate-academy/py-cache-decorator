b870bfc495bb25ba51d32123aba9e0174fcf630c
IRYNA LYAKHOVA <ipppk@outlook.com>
2024-02-16 17:11:52 +0200

    Add an annotation

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
+
     results_value_cashe = {}
 
+    @functools.wraps(func)
     def wrapper_function(*args, **kwargs) -> Callable:
         key = (args, frozenset(kwargs.items()))
         if key in results_value_cashe:
