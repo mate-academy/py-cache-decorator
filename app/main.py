aa15df26aff30be8d7358803d88597938cb7bb48
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-02-17 13:04:27 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 import functools
 
 
@@ -6,7 +6,7 @@ def cache(func: Callable) -> Callable:
     storage = {}
 
     @functools.wraps(func)
-    def wrapper(*args, **kwargs) -> Callable:
+    def wrapper(*args, **kwargs) -> Any:
         key = (func.__name__, args, frozenset(kwargs.items()))
 
         if key not in storage:
