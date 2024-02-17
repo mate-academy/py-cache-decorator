d327d8c2c6e55313234fba76f82812ef2973f65a
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-02-17 15:42:54 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -7,12 +7,10 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def inner(*args) -> any:
-        if args in cached_data:
-            print("Getting from cache")
-            return cached_data[args]
-        else:
+        if args not in cached_data:
             print("Calculating new result")
             cached_data[args] = func(*args)
             return cached_data[args]
-
+        print("Getting from cache")
+        return cached_data[args]
     return inner
