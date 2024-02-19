4f9de7ed3ca616edb72e70f936c7f51c9e36ad52
Mikita Havryliuk <chinter.games1@gmail.com>
2024-02-19 18:18:01 +0200

    feature: implement solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cached_results = {}
+
+    def wrapper(*args, **kwargs):
+        cache_key = f"{args=}{kwargs=}"
+        nonlocal cached_results
+        if cache_key in cached_results:
+            print("Getting from cache")
+            return cached_results[cache_key]
+
+        result = func(*args, **kwargs)
+        cached_results[cache_key] = result
+        print("Calculating new result")
+
+        return result
+    return wrapper
