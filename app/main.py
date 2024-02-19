9a06a53a0343ea59fcdb9ba12f94f7b9a318aaf7
Mikita Havryliuk <chinter.games1@gmail.com>
2024-02-19 18:24:58 +0200

    fixed: use flake8 rules for formatting

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
-    def wrapper(*args, **kwargs):
-        cache_key = f"{args=}{kwargs=}"
+    def wrapper(*args, **kwargs) -> Any:  # any because unknown return type
+        cache_key = f"args={args} kwargs={kwargs}"
         nonlocal cached_results
         if cache_key in cached_results:
             print("Getting from cache")
@@ -16,4 +16,5 @@ def cache(func: Callable) -> Callable:
         print("Calculating new result")
 
         return result
+
     return wrapper
