a02491854e0134c57dafb3d5f2c83e620cfd203a
Mikita Havryliuk <chinter.games1@gmail.com>
2024-02-19 22:00:24 +0200

    fixes: remove duplicates and unusable variable

--- app/main.py
+++ app/main.py
@@ -6,15 +6,13 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Any:  # any because unknown return type
         cache_key = f"args={args} kwargs={kwargs}"
-        nonlocal cached_results
         if cache_key in cached_results:
             print("Getting from cache")
             return cached_results[cache_key]
 
-        result = func(*args, **kwargs)
-        cached_results[cache_key] = result
+        cached_results[cache_key] = func(*args, **kwargs)
         print("Calculating new result")
 
-        return result
+        return cached_results[cache_key]
 
     return wrapper
