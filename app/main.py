570ca895ed3959f587373935a73f3de173007929
Alex Plugin <plugin0785@gmail.com>
2024-02-23 16:54:24 +0200

    Solution cache task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage_dict = {}
+
+    @wraps(func)
+    def wrapper(*args: tuple) -> Any:
+        if args in storage_dict:
+            print("Getting from cache")
+            return storage_dict[args]
+        else:
+            result = func(*args)
+            storage_dict[*args] = result
+            print("Calculating new result")
+            return result
+    return wrapper
