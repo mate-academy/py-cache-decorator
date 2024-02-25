4892effca6282aaf713d506d5104475d868c78fe
Dmytro Bilan <126859750+Lumideus@users.noreply.github.com>
2024-02-25 21:05:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def searcher(*args) -> Callable:
+    def wrapper(*args) -> Callable:
         if args in cached_data:
             print("Getting from cache")
             return cached_data[args]
-        result = func(*args)
-        cached_data[args] = result
+        cached_data[args] = func(*args)
         print("Calculating new result")
-        return result
+        return cached_data[args]
 
     return searcher
