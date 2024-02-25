433f8df9beea22774d129822afd9fe1b3703a24d
Serhii Sierkov <sserkov84@gmail.com>
2024-02-25 20:50:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from typing import Callable
+import functools
+
+
+cached = {}
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @functools.wraps(func)
+    def wrapper(*args) -> int:
+        global cached
+        key = str(id(func)) + "(" + f"{args}" + ")"
+        # print(key)
+        if key not in cached:
+            print("Calculating new result")
+            result = func(*args)
+            cached[key] = result
+        else:
+            print("Getting from cache")
+            result = cached[key]
+        return result
+    return wrapper
