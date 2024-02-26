2eab0e6aceacfea0ae3aa179f17f1b168e344a8b
Kyrylo <kbuchinskiy1@gmail.com>
2024-02-26 17:26:40 +0200

    Fixed fix of solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,18 @@
-import functools
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    @functools.wraps(func)
+    @wraps(func)
     def wrapper(*args, **kwargs) -> None:
         key = args + tuple(kwargs.items())
         if key in cache_dict:
             print("Getting from cache")
             return cache_dict[key]
-        else:
-            print("Calculating new result")
-            cache_dict[key] = func(*args, **kwargs)
-            return cache_dict[key]
+        print("Calculating new result")
+        cache_dict[key] = func(*args, **kwargs)
+        return cache_dict[key]
 
     return wrapper
