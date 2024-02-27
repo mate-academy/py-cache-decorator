3dc8d866f1feb01825362b04583d27c1dc760165
Bludger None <bludger404@gmail.com>
2024-02-27 21:57:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
         if key in cached_results:
             print("Getting from cache")
             return cached_results[key]
-        else:
-            print("Calculating new result")
-            result = func(*args, **kwargs)
-            cached_results[key] = result
-            return result
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cached_results[key] = result
+        return result
 
     return wrapper
