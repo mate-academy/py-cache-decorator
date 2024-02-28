3870364921395ad83ec5c22eca72e4b6b8066196
Shafran Andriy <shafran.andriy@gmail.com>
2024-02-28 05:06:06 -0800

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,12 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args: list) -> list:
+        
+        old_cach = args
+        list_of_old_cache = []
+        if old_cach not in list_of_old_cache:
+            list_of_old_cache.append(old_cach)
+            return "Calculating new result"
+        return "Getting from cache"
+    return inner
