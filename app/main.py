0ed497274e993fc1557bf3ef493e8672eaa524ac
Oksana Dudok <oksanaklw@gmail.com>
2024-03-01 09:04:01 +0200

    Solution after fix bug

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
-        key = (args, frozenset(kwargs.items()))
-        if key in cache_dict:
-            print("Getting from cache")
-            return cache_dict[key]
-        else:
-            result = cache_dict[key] if key in cache_dict \
-                else cache_dict.setdefault(key, func(*args, **kwargs))
-            print("Calculating new result")
-        return result
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(kwargs.items()))
+        result = cache_dict.get(key, None)
 
+        if result is None:
+            cache_dict[key] = func(*args, **kwargs)
+            print("Calculating new result")
+
+        else:
+            print("Getting from cache")
+
+        return cache_dict[key]
     return wrapper
