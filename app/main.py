746c2f81be6f0b6e400f8ca5b522bfda4a20a2bc
Ruslan CHavus <chruslan4@gmail.com>
2024-03-02 17:47:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    # count = 0
+    result_cache = dict()
+
+    @wraps(func)
+    def inner(*args) -> None:
+        item_func = args
+        if item_func in result_cache:
+            print("Getting from cache")
+            return result_cache[item_func]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            result_cache[item_func] = result
+            return result
+    return inner
