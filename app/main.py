2f1e270103e5bc2a48c22b6cbf3347cf7cae49c8
Kolya <nikomail2023@gmail.com>
2024-03-02 18:31:26 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,16 @@
 from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
+def cache(func: Callable) -> [Callable]:
     cache_dict = {}
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        key = (func, args, frozenset(kwargs.items()))
+        key = (func.__name__, args, frozenset(kwargs.items()))
 
         if key in cache_dict:
             print("Getting from cache")
-            return cache_dict[key]
         else:
-            result = func(*args, **kwargs)
+            cache_dict[key] = func(*args, **kwargs)
             print("Calculating new result")
-            cache_dict[key] = result
-            return result
-
+        return cache_dict[key]
     return wrapper
