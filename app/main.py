4cd0c16bdb52c464f805916a1853f2359647b649
Kate Chernyavska <ccarts.kpi@gmail.com>
2024-03-02 16:41:00 +0200

    added frozenset(kwargs.items()) when defined key and added @wraps decorator to inner function

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
     result = {}
 
+    @wraps(func)
     def wrapper(*args, **kwargs) -> str:
-        key = str(args)
+        key = (args, frozenset(kwargs.items()))
         if key not in result:
             print("Calculating new result")
             new_result = func(*args, **kwargs)
