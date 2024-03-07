ae20a699c3f0813e94a571f4e108c2bb2f031f7d
bujhm <Bujhm99@gmail.com>
2024-03-07 11:48:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    all_cash = {}
+
+    def inner_func(*args) -> Any:
+        if args in all_cash:
+            print("Getting from cache")
+            return all_cash[args]
+        # not in cash
+        print("Calculating new result")
+        res_of_func = func(*args)
+        all_cash[args] = res_of_func
+        return res_of_func
+    return inner_func
