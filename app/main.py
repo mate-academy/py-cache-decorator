6edef8dccfea696dbeeaa56808b58afb62bd6d1a
Olena Poplias <poplyasitasony@gmail.com>
2024-03-09 12:30:39 -0500

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    result_dict = {}
+
+    @functools.wraps(func)
+    def inner(*args: Any) -> Any:
+
+        if args in result_dict:
+            print("Getting from cache")
+            result = result_dict[args]
+            return result
+
+        print("Calculating new result")
+        result = func(*args)
+        result_dict[args] = result
+        return result
+
+    return inner
