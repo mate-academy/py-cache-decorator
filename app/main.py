3de1bc56242d05dbb8aabbf5693e30ad6ddadba4
pochtalon <pochtalon1010@gmail.com>
2024-03-11 16:07:37 +0200

    Added annotation, kwargs

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
-from typing import Callable
+import functools
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
-    def wrapper(*args) -> int:
-        key = args
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        key = args, tuple(kwargs.items())
         if key in cached_results:
             print("Getting from cache")
             return cached_results[key]
         else:
             print("Calculating new result")
-            result = func(*args)
+            result = func(*args, **kwargs)
             cached_results[key] = result
             return result
     return wrapper
