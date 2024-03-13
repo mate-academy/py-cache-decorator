7e1f3ed7621491649c22bccee8d2394130291cbc
kekW <panicua12@gmail.com>
2024-03-13 16:26:03 +0200

    Solution (took about 4 hours)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
-    def inner(*args, hash_storage=None):
-        if hash_storage is None:
-            hash_storage = {}
-        if args not in hash_storage:
+def cache(func: Callable, hash_storage: dict = None) -> Callable:
+    if hash_storage is None:
+        hash_storage = {}
+
+    def inner(*args) -> Any:
+        storage_key = (func.__name__, args)
+        if storage_key not in hash_storage:
             print("Calculating new result")
-            hash_storage[args] = func(*args)
+            hash_storage[storage_key] = func(*args)
         else:
             print("Getting from cache")
-        return hash_storage[args]
+        return hash_storage[storage_key]
 
     return inner
