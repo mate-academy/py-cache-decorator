0fdc73abacc2c97878a2dad6914e5555365239e0
kekW <panicua12@gmail.com>
2024-03-13 15:13:52 +0200

    error due to cached value return

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    def inner(*args, hash_storage=None):
+        if hash_storage is None:
+            hash_storage = {}
+        if args not in hash_storage:
+            print("Calculating new result")
+            hash_storage[args] = func(*args)
+        else:
+            print("Getting from cache")
+        return hash_storage[args]
+
+    return inner
