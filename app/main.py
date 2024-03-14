9f925d9d4b550c8f6fd405ea9d13913b7de6b6b9
ForestGanst <maxim133102@gmail.com>
2024-03-14 21:08:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    diсtant = {}
+    cache_results = {}
 
     def inner(*args, **kwargs) -> Callable:
         tuple_of_numbers = (args, tuple(kwargs.items()))
-        if tuple_of_numbers in diсtant:
+        if tuple_of_numbers in cache_results:
             print("Getting from cache")
-            return diсtant[tuple_of_numbers]
-        else:
-            print("Calculating new result")
-            diсtant[tuple_of_numbers] = func(*args, **kwargs)
-            return diсtant[tuple_of_numbers]
+            return cache_results[tuple_of_numbers]
+
+        print("Calculating new result")
+        cache_results[tuple_of_numbers] = func(*args, **kwargs)
+        return cache_results[tuple_of_numbers]
 
     return inner
