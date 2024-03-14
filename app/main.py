05f11698fe8d35122ff1e6cc42ec448f296aa2b5
Dmytro Hlazyrin <dmytro.hlazyrin@gmail.com>
2024-03-14 18:06:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-from typing import Callable
+from typing import Callable, Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    @wraps(func)
+    def wrapper(*args) -> Any:
+
+        if args not in storage:
+            print("Calculating new result")
+            result_of_func = func(*args)
+            storage[args] = result_of_func
+            return result_of_func
+
+        else:
+            print("Getting from cache")
+            return storage[args]
+
+    return wrapper
