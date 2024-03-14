96e0f8d857342043d25870d6698051b2a43a000e
Volodymyr Shutov <forshutov@gmail.com>
2024-03-15 00:55:06 +0400

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,5 +2,19 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args) -> Callable:
+        key = (func.__name__, args)
+
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict.get(key)
+
+        else:
+            print("Calculating new result")
+            function_result = func(*args)
+            cache_dict.setdefault(key, function_result)
+            return function_result
+
+    return wrapper
