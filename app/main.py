9f4322947843b56aff5340ec7289f6cb731d8e01
Lebid Dmytro <lebiddmytriy@gmail.com>
2024-03-14 21:41:55 +0200

    Solution 4.0

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
+    @wraps(func)
     def wrapper(*args) -> None:
         if args in cached_results:
             print("Getting from cache")
             return cached_results[args]
-        else:
-            print("Calculating new result")
-            result = cached_results[args] = func(*args)
-            return result
+
+        print("Calculating new result")
+        result = cached_results[args] = func(*args)
+        return result
     return wrapper
