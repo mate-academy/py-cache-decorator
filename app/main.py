d600a90437674e2105b7eddda33d79160140bf24
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-15 20:16:55 +0200

    Deleted unneeded key variable

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        key = (func.__name__, args, tuple(kwargs.items()))
-        if key not in cache_store:
+        if args not in cache_store:
             print("Calculating new result")
-            cache_store[key] = func(*args, **kwargs)
+            cache_store[args] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-        return cache_store[key]
+        return cache_store[args]
 
     return wrapper
