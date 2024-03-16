9cee5bd7a1e7caf3d3a68c840107ba66b1ad895b
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-03-16 16:37:05 +0200

    Solution (Cache decorator)

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ def cache(func: Callable) -> Callable:
     results_dict = {}
 
     @functools.wraps(func)
-    def wrapper(*args) -> None:
-        func_args = args
+    def wrapper(*args, **kwargs) -> None:
+        func_args = args, tuple(kwargs.items())
         if func_args in results_dict:
             print("Getting from cache")
             return results_dict[func_args]
 
-        results_dict[func_args] = func(*args)
+        results_dict[func_args] = func(*args, **kwargs)
         print("Calculating new result")
         return results_dict[func_args]
     return wrapper
