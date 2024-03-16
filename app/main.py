9567e342daabeacc4a2735d1f0cd93a933e9497b
Dede <specialized8393@gmail.com>
2024-03-16 02:49:37 +0000

    added decorator Functools

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from typing import Callable, Any
-
+import functools
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
 
+    @functools.wraps(func)
     def inner(*args) -> Any:
         nonlocal cache_store
         if args not in cache_store:
