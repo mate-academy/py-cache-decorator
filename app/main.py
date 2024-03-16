f473cdb9b6a7bf40364fd6c326e0f67916ab67da
bwaah1 <chykivan@gmail.com>
2024-03-16 20:27:23 +0200

    Implement cache function

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    closure_list = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        key = (args, frozenset(kwargs.items()))
+        if key not in closure_list.keys():
+            result = func(*args, **kwargs)
+            closure_list[key] = result
+            print("Calculating new result")
+            return result
+        else:
+            print("Getting from cache")
+            return closure_list[key]
+
+    return inner
