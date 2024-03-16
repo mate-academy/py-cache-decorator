bd5ea08db4a9e3f10b8fbc6fff93a1dc79cba9ed
Dmytro K <32574679+SpecializedBaby@users.noreply.github.com>
2024-03-16 01:34:14 +0000

    Nice Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    def inner(*args) -> Any:
+        nonlocal cache_store
+        if args not in cache_store:
+            print("Calculating new result")
+            result = func(*args)
+            cache_store[args] = result
+            return result
+        else:
+            print("Getting from cache")
+        return cache_store[args]
+    return inner
