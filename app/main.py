9a8dfce435f7d9904dc4e75b6a86ee3bb28324ce
Lebid Dmytro <lebiddmytriy@gmail.com>
2024-03-17 15:12:36 +0200

    Solution 5.0

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_results = {}
 
     @wraps(func)
-    def wrapper(*args) -> None:
+    def wrapper(*args) -> Any:
         if args in cached_results:
             print("Getting from cache")
             return cached_results[args]
