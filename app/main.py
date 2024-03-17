692ce71374eaaddb16c845b0f6397c0bc35f166d
Serhii Marynin <serhii.marynin@gmail.com>
2024-03-17 19:58:50 +0200

    make a tuple from function name and args as key

--- app/main.py
+++ app/main.py
@@ -7,14 +7,12 @@ def cache(func: Callable) -> Callable:
 
     @wraps(func)
     def wrapper(*args) -> Any:
-        name_of_function = func.__name__
-        if name_of_function not in cache_dict:
-            cache_dict[name_of_function] = dict()
-        if args not in cache_dict[name_of_function]:
-            cache_dict[name_of_function][args] = func(*args)
+        key = (func.__name__, args)
+        if key not in cache_dict:
+            cache_dict[key] = func(*args)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return cache_dict[name_of_function][args]
+        return cache_dict[key]
 
     return wrapper
