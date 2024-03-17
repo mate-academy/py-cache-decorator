90155c0160f1b35a19dfb3fbd7ae1d72851d6b6a
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-03-17 15:22:48 +0200

    Added data variable

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     def wrapper(*args, **kwargs) -> Any:
-        if args not in cache_store:
+        data = (func.__name__, args, tuple(kwargs.items()))
+        if data not in cache_store:
             print("Calculating new result")
-            cache_store[args] = func(*args, **kwargs)
+            cache_store[data] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-        return cache_store[args]
+        return cache_store[data]
 
     return wrapper
