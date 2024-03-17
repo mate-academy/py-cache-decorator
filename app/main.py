290c24d21c41d82d401a02e1203e39474d8bb41e
Dmytro K <32574679+SpecializedBaby@users.noreply.github.com>
2024-03-17 15:55:16 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     @functools.wraps(func)
-    def inner(*args) -> Any:
-        nonlocal cache_store
+    def inner(*args, **kwargs) -> Any:
+        cache_store
         if args not in cache_store:
             print("Calculating new result")
-            cache_store[args] = func(*args)
+            cache_store[args] = func(*args, **kwargs)
             return cache_store[args]
         else:
             print("Getting from cache")
