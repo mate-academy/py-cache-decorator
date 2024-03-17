6afb9bf8ebf415e3b2947011501a7480cb4627c3
siaserb <assiya.serb@gmail.com>
2024-03-17 23:47:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,18 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_res = {}
+
+    def inner(*args, **kwargs) -> Callable:
+        key = (args, tuple(kwargs.items()))
+
+        if key in cache_res:
+            print("Getting from cache")
+            return cache_res[key]
+
+        print("Calculating new result")
+        res = func(*args, **kwargs)
+        cache_res[key] = res
+        return res
+
+    return inner
