bb22ccb728d379fd9cfecabb2a174304773b6a4b
Dede <specialized8393@gmail.com>
2024-03-17 22:30:23 +0000

    add sol. with kwargs and deletrd rest - code is cleaner

--- app/main.py
+++ app/main.py
@@ -6,13 +6,19 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     @functools.wraps(func)
-    def inner(*args) -> Any:
-        nonlocal cache_store
-        if args not in cache_store:
+    def inner(*args, **kwargs) -> Any:
+        if args and args not in cache_store:
             print("Calculating new result")
             cache_store[args] = func(*args)
             return cache_store[args]
-        else:
+        elif kwargs and kwargs not in cache_store:
+            print("Calculating new result")
+            cache_store[kwargs] = func(*args)
+            return cache_store[kwargs]
+        elif args:
             print("Getting from cache")
-        return cache_store[args]
+            return cache_store[args]
+        elif kwargs:
+            print("Getting from cache")
+            return cache_store[kwargs]
     return inner
