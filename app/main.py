f132336865980661cf5a183325044db5d3f9dd5b
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-03-18 16:57:20 +0200

    Cache decorator solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    temp_dict = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        if args not in temp_dict:
+            result = func(*args, **kwargs)
+            temp_dict[args] = result
+            print("Calculating new result")
+            return result
+
+        else:
+            print("Getting from cache")
+            return temp_dict[args]
+
+    return wrapper
