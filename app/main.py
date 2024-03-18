39b897eb5e3e5e242fbae3a4055515e83e22c055
VR <vladrymarchuk@gmail.com>
2024-03-18 16:40:50 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
     @wraps(func)
-    def inner(*args, **kwargs) -> None:
+    def inner(*args, **kwargs) -> Any:
         key = (args, tuple(kwargs.items()))
         if key not in cache_dict:
             print("Calculating new result")
-            result = func(*args, **kwargs)
-            cache_dict[key] = result
-            return result
-        print("Getting from cache")
+            cache_dict[key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
         return cache_dict[key]
     return inner
