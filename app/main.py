de990fe214de85c9185b7db456251256093ef910
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-03-19 10:11:16 +0200

    added wraps, deleted redundant else, added processing named arguments

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     temp_dict = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
-        if args not in temp_dict:
+        key = func.__name__, *args + tuple(kwargs.keys())
+        if key not in temp_dict:
             result = func(*args, **kwargs)
-            temp_dict[args] = result
+            temp_dict[key] = result
             print("Calculating new result")
             return result
-
-        else:
-            print("Getting from cache")
-            return temp_dict[args]
+        print("Getting from cache")
+        return temp_dict[key]
 
     return wrapper
