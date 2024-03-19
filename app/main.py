cf69663d00b336dd336e628a07c8502174146d15
hhorhe <horhehorda@gmail.com>
2024-03-19 22:56:00 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from typing import Callable
+from functools import wraps
+from typing import Any, Dict, Tuple
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store: Dict[Tuple, Any] = {}
+
+    @wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_store[key] = result
+            return result
+
+    return wrapper
