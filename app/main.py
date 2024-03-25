f5f781cedb50908d73576823d98ec71511680ea4
Volkov <volkovyegorandriiovich@gmail.com>
2024-03-25 16:22:15 +0200

    cache_decrtr_v1.0

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from typing import Callable
+# from typing import Callable
+#
+#
+# def cache(func: Callable[[], int]) -> Callable[[], int]:
+def cache(func: callable) -> callable:
+    data = {}
 
+    def action(*args, **kwargs) -> int:
+        if (f"{func}{args}{kwargs}") in data:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            data[f"{func}{args}{kwargs}"] = func(*args, **kwargs)
+        return data[f"{func}{args}{kwargs}"]
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    return action
