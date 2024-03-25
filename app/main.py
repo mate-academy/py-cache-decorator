5db5ea93679afe4013c30a8deadb6ad5ab6a6a43
igorchapy <159249038+igorchapy@users.noreply.github.com>
2024-03-25 13:19:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     result_cashe = {}
 
-
     def wrapper(*args,**kwargs):
         nonlocal result_cashe
         key = (args, frozenset(kwargs.items()))
