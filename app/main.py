e9c648430cd4b54e805abeae9df2eaffc923bd8e
Igor Chapy <misha@mate.acedemy>
2024-03-25 19:34:21 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    result_cashe = {}
+    result_cache = {}
 
-
-    def wrapper(*args,**kwargs):
-        nonlocal result_cashe
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
-        if key in result_cashe:
-            print("Getting from cashe")
-            return result_cashe[key]
+        if key in result_cache:
+            print("Getting from cache")
+            return result_cache[key]
         else:
             print("Calculating new result")
-            result = func(*args,**kwargs)
-            result_cashe[key] = result
+            result = func(*args, **kwargs)
+            result_cache[key] = result
             return result
 
     return wrapper
