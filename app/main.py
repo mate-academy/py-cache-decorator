a182c8bc9d38a0946309b15117508c81723026d0
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-03-26 18:56:52 +0100

    The solution of the task

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+import functools
 from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     result = {}
 
+    @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Any:
         key = (func.__name__, args, frozenset(kwargs))
         if key in result:
