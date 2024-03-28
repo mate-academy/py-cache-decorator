172c5b8a49797613789dd20b3757b976616ea59b
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-03-28 13:31:13 +0200

    ÐSimplifying the wrapper function

--- app/main.py
+++ app/main.py
@@ -5,10 +5,9 @@ def cache(func: Callable) -> Callable:
     cached_results = {}
 
     def wrapper(*args) -> int:
-        for key in cached_results:
-            if key == args:
-                print("Getting from cache")
-                return cached_results[key]
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
         result = func(*args)
         cached_results[args] = result
         print("Calculating new result")
